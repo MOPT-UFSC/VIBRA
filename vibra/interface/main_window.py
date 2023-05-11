21d4b9b150c5082362e26c634bfebcf0b337a557
Danilo <daniloespindola.grad@gmail.com>
2023-05-11 14:07:39 -0300

    Icons of Views and new menu "mode view"

--- vibra/interface/main_window.py
+++ vibra/interface/main_window.py
@@ -26,6 +26,19 @@ class MainWindow(QMainWindow):
         self.exit_import_icon = QIcon(str(Path("data/door-exit.png")))
         self.save_icon = QIcon(str(Path("data/save-solid.png")))
         self.save_as_icon = QIcon(str(Path("data/save_blue.png")))
+        self.view_up_icon = QIcon(str(Path("data/top.png")))
+        self.view_down_icon = QIcon(str(Path("data/bottom.png")))
+        self.view_right_icon = QIcon(str(Path("data/right.png")))
+        self.view_left_icon = QIcon(str(Path("data/left.png")))
+        self.view_back_icon = QIcon(str(Path("data/back.png")))
+        self.view_front_icon =QIcon(str(Path("data/front.png")))
+        self.view_orthogonal_icon = QIcon(str(Path("data/orthogonal.png")))
+        self.view_mode_line_icon = QIcon(str(Path("data/lines.png")))
+        self.view_mode_nodes_icon = QIcon(str(Path("data/nodes.png")))
+        self.view_mode_face_icon = QIcon(str(Path("data/faces.png")))
+        
+
+        
 
     def config(self):
         self.setMinimumSize(800, 600)
@@ -40,13 +53,16 @@ class MainWindow(QMainWindow):
         self.save_action = QAction(self.save_icon, "Save", self)
         self.save_as_action = QAction(self.save_as_icon, "Save as", self)
         self.help_action = QAction(self.help_icon, "About Vibra", self)
-        self.view_up = QAction("Up View", self)
-        self.view_down = QAction("Down View", self)
-        self.view_left = QAction("Left View", self)
-        self.view_right = QAction("Right View", self)
-        self.view_orthogonal = QAction("Orthogonal View", self)
-        self.view_front = QAction("Front View", self)
-        self.view_back = QAction("Back View", self)
+        self.view_up_action = QAction(self.view_up_icon, "Up View", self)
+        self.view_down_action = QAction(self.view_down_icon, "Down View", self)
+        self.view_left_action = QAction(self.view_left_icon, "Left View", self)
+        self.view_right_action = QAction(self.view_right_icon, "Right View", self)
+        self.view_front_action = QAction(self.view_front_icon, "Front View", self)
+        self.view_back_action = QAction(self.view_back_icon, "Back View", self)
+        self.view_orthogonal_action = QAction(self.view_orthogonal_icon, "Orthogonal View", self)
+        self.view_mode_face_icon = QAction(self.view_mode_face_icon, "Face View", self)
+        self.view_mode_line_icon = QAction(self.view_mode_line_icon, "Line View", self)
+        self.view_mode_nodes_icon = QAction(self.view_mode_nodes_icon, "Node View", self)
         self.save_action.triggered.connect(self.project.save)
         self.help_action.triggered.connect(self.show_help_window)
 
@@ -56,11 +72,14 @@ class MainWindow(QMainWindow):
     def create_menu_bar(self):
         menu_bar = self.menuBar()
         self.project_menu = menu_bar.addMenu("Project")
-        self.help_menu = menu_bar.addMenu("Help")
         self.views_menu = menu_bar.addMenu("Views")
+        self.views_mode_menu = menu_bar.addMenu("View Mode")
+        self.help_menu = menu_bar.addMenu("Help")
+        
         self.load_project_menu()
         self.load_views_menu()
         self.load_help_menu()
+        self.load_views_mode_menu()
 
     def load_project_menu(self):
         self.project_menu.clear()
@@ -71,13 +90,18 @@ class MainWindow(QMainWindow):
         self.project_menu.addAction(self.exit_import_action)
 
     def load_views_menu(self):
-        self.views_menu.addAction(self.view_up)
-        self.views_menu.addAction(self.view_down)
-        self.views_menu.addAction(self.view_left)
-        self.views_menu.addAction(self.view_right)
-        self.views_menu.addAction(self.view_front)
-        self.views_menu.addAction(self.view_back)
-        self.views_menu.addAction(self.view_orthogonal)
+        self.views_menu.addAction(self.view_up_action)
+        self.views_menu.addAction(self.view_down_action)
+        self.views_menu.addAction(self.view_left_action)
+        self.views_menu.addAction(self.view_right_action)
+        self.views_menu.addAction(self.view_front_action)
+        self.views_menu.addAction(self.view_back_action)
+        self.views_menu.addAction(self.view_orthogonal_action)
+
+    def load_views_mode_menu(self):
+        self.views_mode_menu.addAction(self.view_mode_nodes_icon)
+        self.views_mode_menu.addAction(self.view_mode_line_icon)
+        self.views_mode_menu.addAction(self.view_mode_face_icon)
 
     def load_help_menu(self):
         self.help_menu.addAction(self.help_action)
