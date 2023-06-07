ca22b537efffa8e9528ad3a2cef51a53dda7bbf0
André Fernandes <fpf.andre@gmail.com>
2023-06-07 13:22:01 -0300

    WIP: connecting cutting plane with sliders

--- vibra/interface/clip_plane_widget.py
+++ vibra/interface/clip_plane_widget.py
@@ -18,56 +18,54 @@ class ClipPlaneWidget(QWidget):
         )
     
     def create_sliders(self):
-        self.angle_label = [QLabel() for _ in range(3)]
+        # 
+        self.v_angle_title_label = QLabel("Vertical Angle")
+        self.h_angle_title_label = QLabel("Horizontal Angle")
+        self.position_title_label = QLabel("Percentage")
 
-        self.text1 = QLabel("Vertical Angle")
-        self.text1.setFont(QFont("Decorative", 12))
-        self.text2 = QLabel("Horizontal Angle")
-        self.text2.setFont(QFont("Decorative", 12))
-        self.text3 = QLabel("Percentage")
-        self.text3.setFont(QFont("Decorative", 12))
+        self.v_angle_value_label = QLabel("0 °")
+        self.h_angle_value_label = QLabel("0 °")
+        self.position_value_label = QLabel("0 %")
 
-        for label in self.angle_label:
-            label.setFont(QFont("Decorative", 12))
+        # 
+        self.v_angle_slider = QSlider(Qt.Orientation.Horizontal)
+        self.v_angle_slider.setMaximum(360)
+        self.v_angle_slider.setMinimum(0)
+        self.v_angle_slider.valueChanged.connect(self.value_change)
 
-        self.angle_label[0].setText("0 °")
-        self.angle_label[1].setText("0 °")
-        self.angle_label[2].setText("0 %")
+        self.h_angle_slider = QSlider(Qt.Orientation.Horizontal)
+        self.h_angle_slider.setMaximum(360)
+        self.h_angle_slider.setMinimum(0)
+        self.h_angle_slider.valueChanged.connect(self.value_change)
 
-        self.angle_slider = [QSlider(Qt.Orientation.Horizontal) for x in range(3)]
+        self.position_slider = QSlider(Qt.Orientation.Horizontal)
+        self.position_slider.setMaximum(100)
+        self.position_slider.setMinimum(0)
+        self.position_slider.valueChanged.connect(self.value_change) 
 
-        self.angle_slider[0].setMaximum(360)
-        self.angle_slider[0].setMinimum(0)
-        self.angle_slider[0].valueChanged.connect(self.value_change)
-
-        self.angle_slider[1].setMaximum(360)
-        self.angle_slider[1].setMinimum(0)
-        self.angle_slider[1].valueChanged.connect(self.value_change)
-
-        self.angle_slider[2].setMaximum(100)
-        self.angle_slider[2].setMinimum(0)
-        self.angle_slider[2].valueChanged.connect(self.value_change)
-
+        # 
         grid_layout = QGridLayout()
-        grid_layout.addWidget(self.text1, 0, 0)
-        grid_layout.addWidget(self.angle_slider[0], 0, 1)
-        grid_layout.addWidget(self.angle_label[0], 0, 2)
-        grid_layout.addWidget(self.text2, 1, 0)
-        grid_layout.addWidget(self.angle_slider[1], 1, 1)
-        grid_layout.addWidget(self.angle_label[1], 1, 2)
-        grid_layout.addWidget(self.text3, 2, 0)
-        grid_layout.addWidget(self.angle_slider[2], 2, 1)
-        grid_layout.addWidget(self.angle_label[2], 2, 2)
+        grid_layout.addWidget(self.v_angle_title_label, 0, 0)
+        grid_layout.addWidget(self.v_angle_slider, 0, 1)
+        grid_layout.addWidget(self.v_angle_value_label, 0, 2)
+
+        grid_layout.addWidget(self.h_angle_title_label, 1, 0)
+        grid_layout.addWidget(self.h_angle_slider, 1, 1)
+        grid_layout.addWidget(self.h_angle_value_label, 1, 2)
+        
+        grid_layout.addWidget(self.position_title_label, 2, 0)
+        grid_layout.addWidget(self.position_slider, 2, 1)
+        grid_layout.addWidget(self.position_value_label, 2, 2)
 
         self.setLayout(grid_layout)
         self.setGeometry(1450, 150, 450, 200)
 
     def value_change(self):
-        self.angle_label[0].setText(f"{self.angle_slider[0].value()} °")
-        self.angle_label[1].setText(f"{self.angle_slider[1].value()} °")
-        self.angle_label[2].setText(f"{self.angle_slider[2].value()} %")
+        self.v_angle_value_label.setText(f"{self.v_angle_slider.value()} °")
+        self.h_angle_value_label.setText(f"{self.h_angle_slider.value()} °")
+        self.position_value_label.setText(f"{self.position_slider.value()} %")
 
-        print(f"{self.angle_slider[2].value()}")
+        # print(f"{self.angle_slider[2].value()}")
 
         
     
