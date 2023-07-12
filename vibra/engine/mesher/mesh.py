3333e7c32c4951b3066b812dcd9d0789fb0b62c2
André Fernandes <fpf.andre@gmail.com>
2023-07-12 15:03:05 -0300

    Remove unecessary long messages from status bar

--- vibra/engine/mesher/mesh.py
+++ vibra/engine/mesher/mesh.py
@@ -77,7 +77,7 @@ class Mesh:
 
         path = Path(path)
         gmsh.initialize("", False)
-        logging.info(f"Generating mesh from {path}" + ProgressStatus(0, 100))
+        logging.info(f"Generating mesh from {path}")
 
         logging.info("Configuring Mesh" + ProgressStatus(5, 100))
         self._configure_mesh(
@@ -118,7 +118,7 @@ class Mesh:
             f"Mesh generated with {len(self.nodal_coordinates)} nodes"
             f", {len(self.lines_connectivity)} dim 1"
             f", {len(self.faces_connectivity)} dim 2"
-            f"and {len(self.solids_connectivity)} dim 3 elements" + ProgressStatus(100, 100)
+            f"and {len(self.solids_connectivity)} dim 3 elements"
         )
 
     def export_nodes_coordinates(self, filename):
