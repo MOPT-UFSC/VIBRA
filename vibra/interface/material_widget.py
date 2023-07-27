04c960ddfd8fe2804fd93dff9abdf001d6e72f9b
dan-espd <daniloespindola.grad@gmail.com>
2023-07-27 18:32:40 -0300

    WIP

--- vibra/interface/material_widget.py
+++ vibra/interface/material_widget.py
@@ -1,46 +1,54 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QGridLayout, QScrollArea, QPushButton, QDialog, QHBoxLayout
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QGridLayout, QScrollArea, QPushButton, QDialog, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
 
 class MaterialWidget(QDialog):
     def __init__(self):
         super().__init__()
 
-        # Create the toolbar
+
         toolbar_layout = QHBoxLayout()
         icon1_button = QPushButton("Icon 1")
-        icon1_button.setFixedSize(50, 50)  # Definir tamanho quadrado
+        icon1_button.setFixedSize(50, 50)  
         icon1_button.clicked.connect(self.open_widget1)
         toolbar_layout.addWidget(icon1_button)
 
         icon2_button = QPushButton("Icon 2")
-        icon2_button.setFixedSize(50, 50)  # Definir tamanho quadrado
+        icon2_button.setFixedSize(50, 50)  
         icon2_button.clicked.connect(self.open_widget2)
         toolbar_layout.addWidget(icon2_button)
 
-        icon3_button = QPushButton("Icon 3")
-        icon3_button.setFixedSize(50, 50)  # Definir tamanho quadrado
-        icon3_button.clicked.connect(self.open_widget3)
-        toolbar_layout.addWidget(icon3_button)
+        toolbar_layout.addStretch(1)
+        reset_button = QPushButton("Reset")
+        reset_button.setFixedSize(50, 50)
+        reset_button.clicked.connect(self.reset_widgets)
+        toolbar_layout.addWidget(reset_button)
 
         toolbar_layout.setAlignment(Qt.AlignTop)
 
-        # Create the main layout
+        no_list = ["Name", "Id", "Density[kg/m3]", "Young Modulus[GPa]", "Poisson", "Expansion cofficient[m/K]", "Color"]
+        self.table = QTableWidget()
+        self.table.setColumnCount(len(no_list))
+        self.table.setRowCount(1)  
+        self.table.setHorizontalHeaderLabels(no_list)
+        self.table.setItem(0,0,QTableWidgetItem(1))
+        self.table.resizeColumnsToContents()
         main_layout = QVBoxLayout()
         main_layout.addLayout(toolbar_layout)
+        main_layout.addWidget(self.table)
         self.setLayout(main_layout)
-        self.setFixedSize(500, 500)
+        self.setMinimumSize(610,500)
+          
 
         self.exec_()
 
     def open_widget1(self):
-        widget1 = QDialog()
-        widget1.exec_()
-
+        self.table.setRowCount(2)
+        
     def open_widget2(self):
-        widget2 = QDialog()
-        widget2.exec_()
+        self.table.setRowCount(3)
 
-    def open_widget3(self):
-        widget3 = QDialog()
-        widget3.exec_()
+    def reset_widgets(self):
+        for widget in self.findChildren(QWidget):
+            if isinstance(widget, QDialog):
+                widget.reject()
