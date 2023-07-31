3122ede9cf47ccc8c2355ab3c05ef48e3447d9fb
dan-espd <daniloespindola.grad@gmail.com>
2023-07-31 18:31:51 -0300

    add delete, plus icons and resize code

--- vibra/interface/material_widget.py
+++ vibra/interface/material_widget.py
@@ -1,28 +1,42 @@
-from PyQt5.QtCore import Qt
-from PyQt5 import QtGui
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QLineEdit, QColorDialog, QLabel, QVBoxLayout, QWidget, QGridLayout, QScrollArea, QPushButton, QDialog, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
+from pathlib import Path
+from vibra.utils.icons import load_icon
 
 class MaterialWidget(QDialog):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Set Material")
+        color = QColor("#0055DD")
 
 
         toolbar_layout = QHBoxLayout()
-        icon1_button = QPushButton("Icon 1")
-        icon1_button.setFixedSize(50, 50)  
-        icon1_button.clicked.connect(self.open_widget1)
-        toolbar_layout.addWidget(icon1_button)
+        add_material_button = QPushButton()
+        add_material_button.setFocusPolicy(Qt.NoFocus)
+        add_material_icon = load_icon(Path("data/icons/plus-thick.png"), color)
+        add_material_button.setIconSize(QSize(25, 25))
+        add_material_button.setIcon(add_material_icon)
+    
+        
+        add_material_button.setFixedSize(25, 25)  
+        add_material_button.clicked.connect(self.add_material)
+        toolbar_layout.addWidget(add_material_button)
 
-        icon2_button = QPushButton("Icon 2")
-        icon2_button.setFixedSize(50, 50)  
-        icon2_button.clicked.connect(self.open_widget2)
-        toolbar_layout.addWidget(icon2_button)
+        trash_button = QPushButton()
+        trash_button.setFocusPolicy(Qt.NoFocus)
+        trash_icon = load_icon(Path("data/icons/delete.png"), color)
+        trash_button.setIconSize(QSize(25, 25))
+        trash_button.setIcon(trash_icon)
+        trash_button.setFixedSize(25, 25)  
+        trash_button.clicked.connect(self.open_widget2)
+        toolbar_layout.addWidget(trash_button)
 
         toolbar_layout.addStretch(1)
         reset_button = QPushButton("Reset")
-        reset_button.setFixedSize(50, 50)
+        reset_button.setFocusPolicy(Qt.NoFocus)
+        reset_button.setFixedSize(50, 25)
         reset_button.clicked.connect(self.reset_widgets)
         toolbar_layout.addWidget(reset_button)
 
@@ -30,10 +44,11 @@ class MaterialWidget(QDialog):
 
         self.no_list = ["Name", "Density[kg/m3]", "Young Modulus[GPa]", "Poisson", "Expansion cofficient[m/K]", "Color"]
         self.table = QTableWidget()
-        self.table.setColumnCount(len(self.no_list))
-        self.table.setRowCount(1)  
+        self.table.setColumnCount(len(self.no_list)) 
+        self.table.setRowCount(1) 
         self.table.setHorizontalHeaderLabels(self.no_list)
         self.table.setItem(0,0,QTableWidgetItem(1))
+        self.table.setSelectionBehavior(1)
         self.table.resizeColumnsToContents()
         main_layout = QVBoxLayout()
         main_layout.addLayout(toolbar_layout)
@@ -43,14 +58,30 @@ class MaterialWidget(QDialog):
 
         self.exec_()
 
-    def open_widget1(self):
-        widget = QDialog()
-        widget.setWindowTitle("New Material")
+
+    def add_material(self):
+        instance = MaterialAdd()
+
+    def open_widget2(self):
+        self.table.setRowCount(3)
+
+    def reset_widgets(self):
+        for widget in self.findChildren(QWidget):
+            if isinstance(widget, QDialog):
+                widget.reject()
+
+
+class MaterialAdd(QDialog):
+
+    def __init__(self):
+        super().__init__()
+    
+        self.setWindowTitle("New Material")
         layout1 = QGridLayout()
         layout1.setAlignment(Qt.AlignTop)
 
         material_name_label = QLabel("Material Name")
-        line_edit_material_name =  QLineEdit()
+        self.line_edit_material_name =  QLineEdit()
         density_label = QLabel("Density[kg/m3]")
         line_edit_density =  QLineEdit()
         poisson_label = QLabel("Poisson")
@@ -70,11 +101,11 @@ class MaterialWidget(QDialog):
         self.color_button.clicked.connect(self.color_button_callback)
         
 
-        widget.setLayout(layout1)
-        widget.setMinimumSize(500,100)
+        self.setLayout(layout1)
+        self.setMinimumSize(500,100)
 
         layout1.addWidget(material_name_label, 0, 0)
-        layout1.addWidget(line_edit_material_name, 1, 0)
+        layout1.addWidget(self.line_edit_material_name, 1, 0)
         layout1.addWidget(density_label, 0, 1)
         layout1.addWidget(line_edit_density, 1, 1)
         layout1.addWidget(poisson_label, 0, 2)
@@ -87,21 +118,14 @@ class MaterialWidget(QDialog):
         layout1.addWidget(self.color_button, 3, 2)
         layout1.addWidget(add_new_material_button, 6, 2)
         layout1.addWidget(cancel_button, 6, 0)
-
-
-        widget.exec_()
         
+
+        self.exec_()
+
     def color_button_callback(self):
         color = QColorDialog.getColor()
         pick_color = color.name()
         self.color_button.setStyleSheet(f"background-color: {pick_color};")
-        
+
         
     
-    def open_widget2(self):
-        self.table.setRowCount(3)
-
-    def reset_widgets(self):
-        for widget in self.findChildren(QWidget):
-            if isinstance(widget, QDialog):
-                widget.reject()
