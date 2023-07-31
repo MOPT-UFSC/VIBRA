18d22d5cc8945a9b0b4209714af344c8dc4765f3
dan-espd <daniloespindola.grad@gmail.com>
2023-07-31 12:21:36 -0300

    WIP: material inputs widget1

--- vibra/interface/material_widget.py
+++ vibra/interface/material_widget.py
@@ -1,10 +1,12 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QGridLayout, QScrollArea, QPushButton, QDialog, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QLineEdit, QColorDialog, QLabel, QVBoxLayout, QWidget, QGridLayout, QScrollArea, QPushButton, QDialog, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
 
 class MaterialWidget(QDialog):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Set Material")
 
 
         toolbar_layout = QHBoxLayout()
@@ -26,25 +28,76 @@ class MaterialWidget(QDialog):
 
         toolbar_layout.setAlignment(Qt.AlignTop)
 
-        no_list = ["Name", "Id", "Density[kg/m3]", "Young Modulus[GPa]", "Poisson", "Expansion cofficient[m/K]", "Color"]
+        self.no_list = ["Name", "Density[kg/m3]", "Young Modulus[GPa]", "Poisson", "Expansion cofficient[m/K]", "Color"]
         self.table = QTableWidget()
-        self.table.setColumnCount(len(no_list))
+        self.table.setColumnCount(len(self.no_list))
         self.table.setRowCount(1)  
-        self.table.setHorizontalHeaderLabels(no_list)
+        self.table.setHorizontalHeaderLabels(self.no_list)
         self.table.setItem(0,0,QTableWidgetItem(1))
         self.table.resizeColumnsToContents()
         main_layout = QVBoxLayout()
         main_layout.addLayout(toolbar_layout)
         main_layout.addWidget(self.table)
         self.setLayout(main_layout)
-        self.setMinimumSize(610,500)
-          
+        self.setMinimumSize(571,500)
 
         self.exec_()
 
     def open_widget1(self):
-        self.table.setRowCount(2)
+        widget = QDialog()
+        widget.setWindowTitle("New Material")
+        layout1 = QGridLayout()
+        layout1.setAlignment(Qt.AlignTop)
+
+        material_name_label = QLabel("Material Name")
+        line_edit_material_name =  QLineEdit()
+        density_label = QLabel("Density[kg/m3]")
+        line_edit_density =  QLineEdit()
+        poisson_label = QLabel("Poisson")
+        line_edit_poisson =  QLineEdit()
+        expansion_label = QLabel("Expansion cofficient[m/K]")
+        line_edit_expansion_cofficient =  QLineEdit()
+        young_label = QLabel("Young Modulus[GPa]")
+        line_edit_young_modulus =  QLineEdit()
+        color_label = QLabel("Color")
+        self.color_button =  QPushButton("")
+        add_new_material_button = QPushButton("Add New Material")
+        cancel_button = QPushButton("Cancel")
+        add_new_material_button.setMinimumSize(40,40)
+        cancel_button.setMinimumSize(40,40)
+
+
+        self.color_button.clicked.connect(self.color_button_callback)
         
+
+        widget.setLayout(layout1)
+        widget.setMinimumSize(500,100)
+
+        layout1.addWidget(material_name_label, 0, 0)
+        layout1.addWidget(line_edit_material_name, 1, 0)
+        layout1.addWidget(density_label, 0, 1)
+        layout1.addWidget(line_edit_density, 1, 1)
+        layout1.addWidget(poisson_label, 0, 2)
+        layout1.addWidget(line_edit_poisson, 1, 2)
+        layout1.addWidget(young_label, 2, 1)
+        layout1.addWidget(line_edit_young_modulus, 3, 1)
+        layout1.addWidget(expansion_label, 2, 0)
+        layout1.addWidget(line_edit_expansion_cofficient, 3, 0)
+        layout1.addWidget(color_label, 2, 2)
+        layout1.addWidget(self.color_button, 3, 2)
+        layout1.addWidget(add_new_material_button, 6, 2)
+        layout1.addWidget(cancel_button, 6, 0)
+
+
+        widget.exec_()
+        
+    def color_button_callback(self):
+        color = QColorDialog.getColor()
+        pick_color = color.name()
+        self.color_button.setStyleSheet(f"background-color: {pick_color};")
+        
+        
+    
     def open_widget2(self):
         self.table.setRowCount(3)
 
