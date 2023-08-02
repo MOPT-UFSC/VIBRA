a1d9b99d09400ebbda34c7eee6cc9f81b476e6fc
dan-espd <daniloespindola.grad@gmail.com>
2023-08-02 18:09:33 -0300

    add error message in add material widget

--- vibra/interface/material_widget.py
+++ vibra/interface/material_widget.py
@@ -9,13 +9,13 @@ class MaterialWidget(QDialog):
         super().__init__()
 
         self.setWindowTitle("Set Material")
-        color = QColor("#0055DD")
+        self.color = QColor("#0055DD")
         self.instance = None
 
         toolbar_layout = QHBoxLayout()
         add_material_button = QPushButton()
         add_material_button.setFocusPolicy(Qt.NoFocus)
-        add_material_icon = load_icon(Path("data/icons/plus-thick.png"), color)
+        add_material_icon = load_icon(Path("data/icons/plus-thick.png"), self.color)
         add_material_button.setIconSize(QSize(25, 25))
         add_material_button.setIcon(add_material_icon)
     
@@ -25,7 +25,7 @@ class MaterialWidget(QDialog):
 
         trash_button = QPushButton()
         trash_button.setFocusPolicy(Qt.NoFocus)
-        trash_icon = load_icon(Path("data/icons/delete.png"), color)
+        trash_icon = load_icon(Path("data/icons/delete.png"), self.color)
         trash_button.setIconSize(QSize(25, 25))
         trash_button.setIcon(trash_icon)
         trash_button.setFixedSize(25, 25)  
@@ -72,8 +72,6 @@ class MaterialWidget(QDialog):
 
         self.setLayout(main_layout)
         self.setMinimumSize(526,500)
-
-
 
         self.exec_()
 
@@ -152,24 +150,32 @@ class MaterialAdd(QDialog):
         self.setLayout(layout1)
         self.setMinimumSize(500,100)
 
-        self.error_label = QLabel("")
+        self.error_label_material_name = QLabel("")
+        self.error_label_density = QLabel("")
+        self.error_label_young = QLabel("")
+        self.error_label_poisson = QLabel("")
+        self.error_label_expansion = QLabel("")
 
         layout1.addWidget(material_name_label, 0, 0)
         layout1.addWidget(self.line_edit_material_name, 1, 0)
-        
         layout1.addWidget(density_label, 0, 1)
         layout1.addWidget(self.line_edit_density, 1, 1)
-        layout1.addWidget(self.error_label, 2, 0)
-        layout1.addWidget(self.line_edit_poisson, 3, 0)
-        layout1.addWidget(young_label, 0, 2)
+        layout1.addWidget(self.error_label_material_name, 2, 0)   
+        layout1.addWidget(poisson_label, 3, 0)  
+        layout1.addWidget(self.line_edit_poisson, 4, 0)
+        layout1.addWidget(self.error_label_poisson, 5, 0)  
+        layout1.addWidget(young_label, 0, 2)   
         layout1.addWidget(self.line_edit_young_modulus, 1, 2)
-        layout1.addWidget(expansion_label, 2, 1)
-        layout1.addWidget(self.line_edit_expansion_cofficient, 3, 1)
-        layout1.addWidget(color_label, 2, 2)
-        layout1.addWidget(self.color_button, 3, 2)
-        layout1.addWidget(self.add_new_material_button, 6, 2)
-        layout1.addWidget(self.cancel_button, 6, 0)
-
+        layout1.addWidget(self.error_label_density, 2, 1)  
+        layout1.addWidget(expansion_label, 3, 1)   
+        layout1.addWidget(self.line_edit_expansion_cofficient, 4, 1)
+        layout1.addWidget(self.error_label_expansion, 5, 1)  
+        layout1.addWidget(self.error_label_young, 2, 2) 
+        layout1.addWidget(color_label, 3, 2)   
+        layout1.addWidget(self.color_button, 4, 2)  
+        layout1.addWidget(self.add_new_material_button, 8, 2)
+        layout1.addWidget(self.cancel_button, 8, 0)
+    
         self.completed = False
         
         self.exec_()
@@ -181,16 +187,57 @@ class MaterialAdd(QDialog):
 
     def verify_error(self):
         error = False
+        red_color = QColor(224, 73, 70)
+        border_color = f"border: 0.5px solid {red_color.name()};"
+        none_color = f"border: 0px solid {None}"
 
         if self.line_edit_material_name.text():
-            self.error_label.setText("")
-
+            self.error_label_material_name.setText("")
+            self.line_edit_material_name.setStyleSheet(border_color)
+            self.line_edit_material_name.setStyleSheet(none_color)
         else:
             error = True
-            self.error_label.setText("deu erro andre")
-        return error
+            self.error_label_material_name.setText("Please put a material name")
+            self.error_label_material_name.setStyleSheet(f'color: {red_color.name()};')
+            self.line_edit_material_name.setStyleSheet(border_color)
 
-            
+        if self.line_edit_density.text() and self.line_edit_density.text().isnumeric():
+            self.error_label_density.setText("")
+            self.line_edit_density.setStyleSheet(none_color)
+        else:
+            error = True
+            self.error_label_density.setText("Please put a density")
+            self.error_label_density.setStyleSheet(f'color: {red_color.name()};')
+            self.line_edit_density.setStyleSheet(border_color)
+        
+        if self.line_edit_young_modulus.text() and self.line_edit_young_modulus.text().isnumeric():
+            self.error_label_young.setText("")
+            self.line_edit_young_modulus.setStyleSheet(none_color)
+        else:
+            error = True
+            self.error_label_young.setText("Please put a number")
+            self.error_label_young.setStyleSheet(f'color: {red_color.name()};')
+            self.line_edit_young_modulus.setStyleSheet(border_color)
+        
+        if self.line_edit_expansion_cofficient.text() and self.line_edit_expansion_cofficient.text().isnumeric():
+            self.error_label_expansion.setText("")
+            self.line_edit_expansion_cofficient.setStyleSheet(none_color)
+        else:
+            error = True
+            self.error_label_expansion.setText("Please put a number")
+            self.error_label_expansion.setStyleSheet(f'color: {red_color.name()};')
+            self.line_edit_expansion_cofficient.setStyleSheet(border_color)
+
+        if self.line_edit_poisson.text() and self.line_edit_poisson.text().isnumeric():
+            self.error_label_poisson.setText("")
+            self.line_edit_poisson.setStyleSheet(none_color)
+        else:
+            error = True
+            self.error_label_poisson.setText("Please put a number")
+            self.error_label_poisson.setStyleSheet(f'color: {red_color.name()};')
+            self.line_edit_poisson.setStyleSheet(border_color)
+        
+        return error
 
     def cancel_button_callback(self):
         self.close()
