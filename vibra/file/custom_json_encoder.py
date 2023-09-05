42f8ed9ee0442d1f93ef2d4cdaa3392c895655b0
André Fernandes <fpf.andre@gmail.com>
2023-09-05 10:11:12 -0300

    fix custom json encoder tuple key conversion

--- vibra/file/custom_json_encoder.py
+++ vibra/file/custom_json_encoder.py
@@ -28,12 +28,34 @@ class CustomJsonEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
     def transform_key_tuples(self, obj):
-        """
-        Transform tuple keys into strings.
-        """
+        '''
+        If obj is a dict removes recursivelly all keys that
+        are tuples and replaces by a sequence string. 
+
+        Example:
+            {(0,0):0, (0,1):1, (1,0):2, (1,1):3}
+
+            is transformed to
+            
+            {"0,0":0, "0,1":1, "1,0":2, "1,1":3}
+        '''
+        if isinstance(obj, dict):
+            obj = self.transform_key_tuples_dict(obj)
+        elif isinstance(obj, (tuple, list)):
+            obj = self.transform_key_tuples_list(obj)
+        return obj
+
+    def transform_key_tuples_dict(self, obj: dict):
         new_obj = dict()
         for key, val in obj.items():
             if isinstance(key, tuple):
                 key = ", ".join(str(i) for i in key)
-            new_obj[key] = val
+            new_obj[key] = self.transform_key_tuples(val)
         return new_obj
+
+    def transform_key_tuples_list(self, obj: list | tuple):
+        new_list = []
+        for i in obj:
+            new_obj = self.transform_key_tuples(i)
+            new_list.append(new_obj)
+        return new_list
