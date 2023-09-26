efd11524911d1d3fa52b6d6e99e8b6bfdeede456
vitorslongo <v.v.slongo@gmail.com>
2023-09-26 11:53:10 -0300

    WIP widget

--- vibra/interface/local_refine_widget.py
+++ vibra/interface/local_refine_widget.py
@@ -7,11 +7,40 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
+    QLineEdit,
 )
+
 
 
 class LocalRefineWidget(QDialog):
     def __init__(self):
         super().__init__()
 
+        self.banana = QLabel("banana")
+        self.maca = QLabel("maçã")
+        self.botao_mesh_size = QPushButton("texto")
+        self.textbox = QLineEdit()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.banana)
+        layout.addWidget(self.maca)
+        layout.addWidget(self.botao_mesh_size)
+        layout.addWidget(self.textbox)
+        self.setLayout(layout)
+        self.resize(500,500)
+
+
+
+        self.botao_mesh_size.clicked.connect(self.botao_mesh_size_callback)
+
+
+
+
+
+
         self.exec_()
+
+    def botao_mesh_size_callback(self):
+        # print("banana")
+        self.banana.setText("alala")
+    
