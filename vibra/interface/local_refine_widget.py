a41e75063dd493f060c49aeb405eace5194e3fab
vitorslongo <v.v.slongo@gmail.com>
2023-11-05 20:34:01 -0300

    WIP widget

--- vibra/interface/local_refine_widget.py
+++ vibra/interface/local_refine_widget.py
@@ -1,13 +1,18 @@
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import (
+    QColorDialog,
     QDialog,
     QGridLayout,
+    QHBoxLayout,
     QLabel,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
     QLineEdit,
+    QPushButton,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
 )
 
 
@@ -18,29 +23,53 @@ class LocalRefineWidget(QDialog):
 
         self.banana = QLabel("banana")
         self.maca = QLabel("maçã")
-        self.botao_mesh_size = QPushButton("texto")
-        self.textbox = QLineEdit()
+        self.apply_button = QPushButton("Apply")
+        self.global_mesh_size_textbox = QLineEdit()
+        
+        # apply_button
+        
+        self.apply_button.resize
+
+
+
+
+
+        # header
+        header = ["Refining mesh size", "Faces list"]
+        self.table = QTableWidget()
+        self.table.setColumnCount(len(header))
+        self.table.setHorizontalHeaderLabels(header)
+        self.table.resizeColumnsToContents()
+        # self.table.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         layout = QVBoxLayout()
         layout.addWidget(self.banana)
         layout.addWidget(self.maca)
-        layout.addWidget(self.botao_mesh_size)
-        layout.addWidget(self.textbox)
+        layout.addWidget(self.apply_button)
+        layout.addWidget(self.global_mesh_size_textbox)
+        layout.addWidget(self.table)
+
         self.setLayout(layout)
+        self.setWindowTitle("Mesh parameters")
         self.resize(500,500)
 
-
-
-        self.botao_mesh_size.clicked.connect(self.botao_mesh_size_callback)
-
-
-
-
-
+        self.apply_button.clicked.connect(self.apply_button_callback)
 
         self.exec_()
 
-    def botao_mesh_size_callback(self):
-        # print("banana")
-        self.banana.setText("alala")
+    def apply_button_callback(self):
+        self.banana.setText("Changes applied")
     
