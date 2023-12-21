fc5a592029f7e0861f8306be8e4b0f8ac3a22f85
JacsonGilVargas <jgvargasemc@gmail.com>
2023-12-20 23:29:19 -0300

    Minor adjustments - mesh data export

--- vibra/interface/data_handler/export_mesh_data.py
+++ vibra/interface/data_handler/export_mesh_data.py
@@ -27,13 +27,17 @@ class ExportMeshData(QDialog):
 
         self.main_window = get_main_window()
         self.main_window.set_input_widget(self)
-        self.main_window.viewer_tabs.show_mesh()
+        
         self.project = self.main_window.project
         self.model = self.project.model
         self.mesh = self.project.model.mesh
         self.properties = self.model.properties
 
-        self.temp_path = os.path.expanduser('~')
+        if self.mesh is None:
+            return
+        else:
+            self.main_window.viewer_tabs.show_mesh()
+
         self._load_icons()
         self._reset_variables()
         self._define_qt_variables()
@@ -60,6 +64,10 @@ class ExportMeshData(QDialog):
         self.checkBox_solid_elements_connectivity = self.findChild(QCheckBox, 'checkBox_solid_elements_connectivity')
         self.checkBox_face_elements_connectivity = self.findChild(QCheckBox, 'checkBox_face_elements_connectivity')
         self.checkBox_export_vtu_file = self.findChild(QCheckBox, 'checkBox_export_vtu_file')
+        self.checkBox_nodal_coordinates.setChecked(True)
+        self.checkBox_face_elements_connectivity.setChecked(True)
+        self.checkBox_solid_elements_connectivity.setChecked(True)
+        self.checkBox_export_vtu_file.setChecked(True)
         # QLineEdit
         self.lineEdit_folder_path = self.findChild(QLineEdit, 'lineEdit_folder_path')
         # QPushButton
@@ -69,19 +77,29 @@ class ExportMeshData(QDialog):
         self.pushButton_search_folder.setIcon(self.search_icon)
     
     def _create_connections(self):
-        self.pushButton_export_mesh.clicked.connect(self.export_mesh)
+        self.pushButton_export_mesh.clicked.connect(self.export_mesh_data)
         self.pushButton_search_folder.clicked.connect(self.search_folder)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return:
+            self.export_mesh_data()
+        if event.key() == Qt.Key_Escape:
+            self.close()
 
     def search_folder(self):
         self.folder_path = QFileDialog.getExistingDirectory(None, 'Choose a folder to export the mesh data', self.temp_path)
         self.lineEdit_folder_path.setText(str(self.folder_path))
-        if self.folder_path != "":
+        if self.folder_path == "":
+            return True
+        else:
             self.temp_path = self.folder_path
+            return False
 
-    def export_mesh(self):
+    def export_mesh_data(self):
 
         if not os.path.exists(self.folder_path):
-            self.search_folder()
+            if self.search_folder():
+                return
 
         if self.generate_mesh():
             return
@@ -102,12 +120,14 @@ class ExportMeshData(QDialog):
             _path = Path(f"{self.folder_path}/mesh_data.vtu")
             self.mesh.export_vtu_file(_path)
 
-        self.close()
-
-        title = "Exporting mesh data"
-        message = "Exportation data completed."
-        window_title = "Info"
-        PrintMessageInput([window_title, title, message], auto_close=True)
+        if self.check_data_to_export():
+            self.close()
+            title = "Exporting mesh data"
+            message = "The selected mesh data has been exported."
+            window_title = "Information"
+            PrintMessageInput([window_title, title, message], auto_close=True)
+        else:
+            return
 
     def generate_mesh(self):
         if not self.main_window.project.model.generated_mesh:
@@ -115,3 +135,19 @@ class ExportMeshData(QDialog):
             if not self.mesher.complete:
                 self.mesher = None
                 return True
+            
+    def check_data_to_export(self):
+        if self.checkBox_nodal_coordinates.isChecked():
+            return True
+        elif self.checkBox_face_elements_connectivity.isChecked():
+            return True
+        elif self.checkBox_solid_elements_connectivity.isChecked():
+            return True
+        elif self.checkBox_export_vtu_file.isChecked():
+            return True
+        else:
+            title = "Empty mesh data selection"
+            message = "Select a mesh data to proceed with the data exportation."
+            window_title = "Warning"
+            PrintMessageInput([window_title, title, message], auto_close=False)
+            return False
