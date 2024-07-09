cc60b4a3664c44a274f87682ce6cc49b652320d8
André Fernandes <fpf.andre@gmail.com>
2024-07-08 21:21:23 -0300

    Update CommonRenderWidget import

--- vibra/interface/renderer_toolbar.py
+++ vibra/interface/renderer_toolbar.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QAction, QToolBar
 
-from vibra.interface.mesh.mesher_inputs import MesherInputs
-from vibra.interface.viewer_3d.render_widgets.common_render_widget import CommonRenderWidget
+from molde.render_widgets import CommonRenderWidget
+
 from vibra.utils.icons import load_icon
 
 
