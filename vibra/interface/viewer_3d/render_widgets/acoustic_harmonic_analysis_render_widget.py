3fef589722d6e48ec32933a82597cff599618f08
André Fernandes <fpf.andre@gmail.com>
2024-10-23 19:00:05 -0300

    add scale bar in every renderer

--- vibra/interface/viewer_3d/render_widgets/acoustic_harmonic_analysis_render_widget.py
+++ vibra/interface/viewer_3d/render_widgets/acoustic_harmonic_analysis_render_widget.py
@@ -57,6 +57,7 @@ class AcousticHarmonicAnalysisRenderWidget(AnimatedRenderWidget):
 
         self.create_axes()
         self.create_color_bar()
+        self.create_scale_bar()
         self.update_frequencies()
         self.update_plot()
 
