5dde8470e818a7f26741466efb6a6bd0b6d4a846
André Fernandes <fpf.andre@gmail.com>
2025-01-28 13:27:32 -0300

    WIP: experiments solids optimization

--- vibra/interface/viewer_3d/render_widgets/mesh_render_widget.py
+++ vibra/interface/viewer_3d/render_widgets/mesh_render_widget.py
@@ -2,8 +2,11 @@ import numpy as np
 from molde.render_widgets import CommonRenderWidget
 from molde.utils import TreeInfo
 from molde.utils.format_sequences import format_long_sequence
+from molde.interactor_styles import BoxSelectionInteractorStyle
+
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+
 from vtkmodules.vtkCommonCore import vtkIntArray
 from vtkmodules.vtkCommonDataModel import vtkPolyData
 from vtkmodules.vtkRenderingCore import vtkActor, vtkCellPicker
@@ -23,10 +26,10 @@ from ..actors.symbols.symbols_actor import SymbolsActor
 from ..actors.ghost_actor import GhostActor
 
 
-SHOW_POINTS = 0
-SHOW_LINES = 1
-SHOW_FACES = 2
-SHOW_VOLUMES = 3
+# SHOW_POINTS = 0
+# SHOW_LINES = 1
+# SHOW_FACES = 2
+# SHOW_VOLUMES = 3
 
 
 class MeshRenderWidget(CommonRenderWidget):
@@ -34,10 +37,11 @@ class MeshRenderWidget(CommonRenderWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.set_interactor_style(BoxSelectionInteractorStyle())
         self.mouse_click = (0, 0)
 
         self.main_window = app().main_window
-        self.view_mode = SHOW_FACES
+        # self.view_mode = SHOW_FACES
         self.selection_color = (20, 106, 245)
 
         self.left_clicked.connect(self.click_callback)
@@ -72,7 +76,8 @@ class MeshRenderWidget(CommonRenderWidget):
         self.nodes_actor = NodesActor(mesh)
         self.faces_actor = FacesActor(mesh)
         self.edges_actor = EdgesActor(self.faces_actor.data)
-        self.solids_actor = SolidsActor(mesh)
+        self.solids_actor: SolidsActor | FacesActor = FacesActor(mesh)
+        # self.solids_actor: SolidsActor | FacesActor = SolidsActor(mesh)
         self.symbols_actor = SymbolsActor(self.renderer)
         self.selection_spheres_actor = SelectionSpheres()
 
@@ -86,7 +91,8 @@ class MeshRenderWidget(CommonRenderWidget):
         self.add_actors(
             self.nodes_actor,
             self.edges_actor,
-            self.faces_actor,
+            # self.faces_actor,
+            self.solids_actor,
             self.ghost_actor,
             self.plane_actor,
             self.symbols_actor,
@@ -108,16 +114,16 @@ class MeshRenderWidget(CommonRenderWidget):
 
         self.nodes_actor.SetVisibility(visualization.points)
         self.edges_actor.SetVisibility(visualization.lines)
-        self.faces_actor.SetVisibility(visualization.faces)
-        self.solids_actor.SetVisibility(visualization.solids)
+        self.faces_actor.SetVisibility(False)
         self.ghost_actor.SetVisibility(has_hidden_part)
+        self.solids_actor.SetVisibility(visualization.solids)
 
         self.update()
 
     def update_symbols(self):
-        self.renderer.RemoveActor(self.symbols_actor)
+        self.remove_actors(self.symbols_actor)
         self.symbols_actor = SymbolsActor(self.renderer)
-        self.renderer.AddActor(self.symbols_actor)
+        self.add_actors(self.symbols_actor)
 
     def update_hidden_plot(self):
         return 
@@ -233,9 +239,9 @@ class MeshRenderWidget(CommonRenderWidget):
         if not self._actors_exists():
             return
 
-        print("hello world")
+        x0, y0 = self.mouse_click
+        mouse_moved = (abs(x0 - x) > 10) or (abs(y0 - y) > 10)
 
-        mouse_moved = False
         if mouse_moved:
             picked_nodes, picked_faces, picked_solids = self._get_area_picked_cell_id(x, y)
         else:
@@ -297,8 +303,8 @@ class MeshRenderWidget(CommonRenderWidget):
         if closest == node_distance:
             picked_nodes.append(node_id)
 
-        elif closest == face_distance:
-            picked_faces.append(face_id)
+        # elif closest == face_distance:
+        #     picked_faces.append(face_id)
 
         elif closest == solid_distance:
             picked_solids.append(solid_id)
@@ -306,7 +312,7 @@ class MeshRenderWidget(CommonRenderWidget):
         return picked_nodes, picked_faces, picked_solids
 
     def _get_area_picked_cell_id(self, x, y):
-        # Not implemented
+        print("Area selection not implemented yet")
         picked_nodes = []
         picked_faces = []
         picked_solids = []
@@ -367,7 +373,12 @@ class MeshRenderWidget(CommonRenderWidget):
 
         self.nodes_actor.clear_colors((0, 0, 0, 0))
         self.faces_actor.clear_colors((255, 255, 255, 255))
-        self.solids_actor.clear_colors()
+        # self.solids_actor.clear_colors()
+
+        if isinstance(self.solids_actor, FacesActor):
+            self.solids_actor.set_color((0, 255, 255))
+        else:
+            self.solids_actor.set_color((255, 255, 0))
 
         nodes = self.main_window.selected_mesh_nodes
         faces = self.main_window.selected_mesh_faces
@@ -383,8 +394,8 @@ class MeshRenderWidget(CommonRenderWidget):
             return
         self.nodes_actor.paint_cells([255, 0, 0], new_nodes)
         self.update()
-        if self.view_mode != SHOW_FACES:
-            self.show_points()
+        # if self.view_mode != SHOW_FACES:
+        #     self.show_points()
 
     def select_multiple_faces(self, new_faces, *, join=False, remove=False):
         if not self._actors_exists():
@@ -426,13 +437,6 @@ class MeshRenderWidget(CommonRenderWidget):
 
     def _actors_exists(self):
         return len(self._widget_actors) > 0
-        actors = [
-            self.solids_actor,
-            self.faces_actor,
-            self.edges_actor,
-            self.selection_spheres_actor,
-        ]
-        return all([actor is not None for actor in actors])
 
     def _get_info_tab(self):
         pass
