8bc1d515904a58b5e49b2f5eed1be7c4fb29bb23
André Fernandes <fpf.andre@gmail.com>
2025-01-29 12:31:41 -0300

    isolate element mappings into a dedicated function

--- vibra/engine/mesher/mesh.py
+++ vibra/engine/mesher/mesh.py
@@ -482,7 +482,10 @@ class Mesh:
         # for i, values in enumerate(self.solids_connectivity[:,4:]):
         #     if (aux_zeros == values).all():
         #         print(f"The solid element #{i} doesn't have valid connectivity")
+        
+        self.create_element_mappings()
 
+    def create_element_mappings(self):
         self._maps_lines_by_elements()
         self._maps_surfaces_by_elements()
         self._maps_volumes_by_elements()
@@ -523,6 +526,7 @@ class Mesh:
 
 
     def _maps_volumes_by_elements(self):
+        print("hi", len(self.solids_connectivity))
         self.volume_from_element.clear()
         self.elements_from_volume.clear()
         for tag, gmsh_indexes in self.gmsh_elements_from_volumes.items():
@@ -588,6 +592,7 @@ class Mesh:
         self.solid_to_face_elements = defaultdict(list)
 
         if len(self.solids_connectivity) == 0:
+            print("no solids")
             return
 
         nodes_per_face_element = len(self.faces_connectivity[0, 4:])
@@ -601,6 +606,9 @@ class Mesh:
             els_id = filtered_data[mask_1, 0][0]
             self.face_to_solid_element[elf_id] = els_id
             self.solid_to_face_elements[els_id].append(elf_id)
+        
+        from pprint import pprint
+        pprint(self.solid_to_face_elements)
 
 
     def get_face_elements_connected_to_nodes(self, node_ids, surface_id=None):
