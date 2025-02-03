12ff4e29d4e59bd02d239fe35be4421796e094e8
André Fernandes <fpf.andre@gmail.com>
2025-02-03 14:22:07 -0300

    check if tube is hollow before replacing it with a solid one

--- vibra/interface/viewer_3d/render_widgets/structural_modal_analysis_render_widget.py
+++ vibra/interface/viewer_3d/render_widgets/structural_modal_analysis_render_widget.py
@@ -256,9 +256,10 @@ class StructuralModalAnalysisRenderWidget(AnimatedRenderWidget):
             if mesh is None:
                 return
 
-            self.remove_actors(self.analysis_actor)
-            self.analysis_actor = AnalysisActor(mesh)
-            self.add_actors(self.analysis_actor)
+            if mesh.solids_connectivity.size > 0:
+                self.remove_actors(self.analysis_actor)
+                self.analysis_actor = AnalysisActor(mesh)
+                self.add_actors(self.analysis_actor)
 
         self.plane_actor.configure_section_plane(position, rotation)
         xyz = self.plane_actor.calculate_xyz_position(position)
