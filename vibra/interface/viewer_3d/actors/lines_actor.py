13a82af374296c585ff644ff1cba79f5a02c8794
André Fernandes <fpf.andre@gmail.com>
2025-02-14 20:28:21 -0300

    refactor lines actor

--- vibra/interface/viewer_3d/actors/lines_actor.py
+++ vibra/interface/viewer_3d/actors/lines_actor.py
@@ -13,53 +13,47 @@ from vtkmodules.util.numpy_support import numpy_to_vtk
 
 
 class LinesActor(vtkActor):
+    NODES_TO_VTK_CELL = {
+        2: VTK_LINE,
+        3: VTK_QUADRATIC_EDGE
+    }
+
     def __init__(self, mesh):
         self.mesh = mesh
         self.create_geometry()
         self.configure_appearance()
 
     def create_geometry(self):
+        nodes_per_line = self.mesh.lines_connectivity.shape[1] - 4
+        number_of_lines = self.mesh.lines_connectivity.shape[0]
+        
         data = vtkPolyData()
         points = vtkPoints()
         mapper = vtkPolyDataMapper()
-        cell_colors = vtkUnsignedCharArray()
 
         line_indexes = vtkIntArray()
         line_indexes.SetName("line_indexes")
-        # volume_indexes.Allocate(number_of_elements)
+        line_indexes.Allocate(number_of_lines)
 
-
-        data.Allocate(len(self.mesh.lines_connectivity))
+        cell_colors = vtkUnsignedCharArray()
         cell_colors.SetNumberOfComponents(3)
-        cell_colors.SetNumberOfTuples(len(self.mesh.lines_connectivity))
+        cell_colors.SetNumberOfTuples(number_of_lines)
 
         coordinates = self.mesh.nodal_coordinates[:, 1:]
         points.SetData(numpy_to_vtk(coordinates))
 
-        if self.mesh.faces_connectivity.shape[1] == 2:
-            for i, line, _, _, *values in self.mesh.faces_connectivity:
-                data.InsertNextCell(VTK_LINE, 2, values)
-                # line_indexes.InsertNextValue(line)
-        else:
-            for i, line, _, _, *values in self.mesh.faces_connectivity:
-                data.InsertNextCell(VTK_QUADRATIC_EDGE, 3, values)
-                # line_indexes.InsertNextValue(line)
-
-        # connect = self.mesh.lines_connectivity[:, 4:]
-        # if len(connect[0, :]) == 2:
-        #     for a, b in connect:
-        #         data.InsertNextCell(VTK_LINE, 2, (a, b))
-        # else:
-        #     for a, b, c in connect:
-        #         data.InsertNextCell(VTK_QUADRATIC_EDGE, 3, (a, b, c))
+        data.Allocate(number_of_lines)
+        cell_type = self.NODES_TO_VTK_CELL[nodes_per_line]
+        for _, line_id, _, _, *values in self.mesh.lines_connectivity:
+            data.InsertNextCell(cell_type, nodes_per_line, values)
+            line_indexes.InsertNextValue(line_id)
 
         data.SetPoints(points)
         data.GetCellData().SetScalars(cell_colors)
-        # data.GetCellData().AddArray(line_indexes)
+        data.GetCellData().AddArray(line_indexes)
 
         mapper.SetInputData(data)
         self.SetMapper(mapper)
-
         self.clear_colors()
 
     def configure_appearance(self):
