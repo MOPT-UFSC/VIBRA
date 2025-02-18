dbf7cd70c9890d26be28f3ec68520d4823c8db9e
André Fernandes <fpf.andre@gmail.com>
2025-02-17 21:54:54 -0300

    optimize geometry selection of surfaces

--- vibra/interface/viewer_3d/selection/geometry_selection.py
+++ vibra/interface/viewer_3d/selection/geometry_selection.py
@@ -3,15 +3,16 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ..render_widgets.geometry_render_widget import GeometryRenderWidget
 
+
 import numpy as np
-from vtkmodules.vtkFiltersGeneral import vtkExtractSelectedFrustum
 from vtkmodules.vtkRenderingCore import (
-    vtkAreaPicker,
     vtkCellPicker,
     vtkCoordinate,
 )
 
 from vibra import app
+from vibra.utils.interface_utils import screen_to_world_coords
+from vibra.utils.math_functions import points_in_between
 
 from .common_selection import pick_actor_cell_info
 
@@ -33,6 +34,7 @@ class GeometrySelection:
         picked_surfaces = self.pick_surface(x, y)
         picked_volumes = self.pick_volume(x, y)
 
+        # TODO: this order is wrong, fix it somehow
         if picked_points:
             picked_lines.clear()
             picked_surfaces.clear()
@@ -54,12 +56,12 @@ class GeometrySelection:
     ]:
         internal_picked_nodes = self._area_pick_node_internal_indexes(x0, y0, x1, y1)
 
-        return (
-            self.area_pick_points(x0, y0, x1, y1, internal_picked_nodes),
-            self.area_pick_lines(x0, y0, x1, y1, internal_picked_nodes),
-            self.area_pick_surfaces(x0, y0, x1, y1, internal_picked_nodes),
-            self.area_pick_volumes(x0, y0, x1, y1, internal_picked_nodes),
-        )
+        points = self.area_pick_points(x0, y0, x1, y1, internal_picked_nodes)
+        lines = self.area_pick_lines(x0, y0, x1, y1, internal_picked_nodes)
+        surfaces = self.area_pick_surfaces(x0, y0, x1, y1, internal_picked_nodes)
+        volumes = self.area_pick_volumes(x0, y0, x1, y1, internal_picked_nodes)
+
+        return points, lines, surfaces, volumes
 
     def pick_point(self, x: int, y: int) -> set[int]:
         mesh = app().project.model.mesh
@@ -160,12 +162,50 @@ class GeometrySelection:
                 x0, y0, x1, y1
             )
 
+        mask_unselected = np.any(
+            np.isin(
+                mesh.lines_connectivity[:, 4:],
+                internal_picked_nodes,
+                invert=True,
+            ),
+            axis=1,
+        )
+
+        line_indexes = mesh.lines_connectivity[:, 1].astype(int)
+        all_lines = np.unique(line_indexes)
+        unselected = np.unique(line_indexes[mask_unselected])
+        return set(all_lines) - set(unselected)
+
         mask_selected_elements = np.any(
             np.isin(mesh.lines_connectivity[:, 4:], internal_picked_nodes),
             axis=1,
         )
 
         return set(mesh.lines_connectivity[mask_selected_elements, 1].astype(int))
+
+    # def area_pick_surfaces(
+    #     self,
+    #     x0: int,
+    #     y0: int,
+    #     x1: int,
+    #     y1: int,
+    #     internal_picked_nodes: list[int] | None = None,
+    # ) -> set[int]:
+    #     mesh = app().project.model.mesh
+    #     if mesh is None:
+    #         return set()
+
+    #     if internal_picked_nodes is None:
+    #         internal_picked_nodes = self._area_pick_node_internal_indexes(
+    #             x0, y0, x1, y1
+    #         )
+
+    #     mask_selected_elements = np.any(
+    #         np.isin(mesh.faces_connectivity[:, 4:], internal_picked_nodes),
+    #         axis=1,
+    #     )
+
+    #     return set(mesh.faces_connectivity[mask_selected_elements, 1].astype(int))
 
     def area_pick_surfaces(
         self,
@@ -184,12 +224,19 @@ class GeometrySelection:
                 x0, y0, x1, y1
             )
 
-        mask_selected_elements = np.any(
-            np.isin(mesh.faces_connectivity[:, 4:], internal_picked_nodes),
+        mask_unselected = np.any(
+            np.isin(
+                mesh.faces_connectivity[:, 4:],
+                internal_picked_nodes,
+                invert=True,
+            ),
             axis=1,
         )
 
-        return set(mesh.faces_connectivity[mask_selected_elements, 1].astype(int))
+        surface_indexes = mesh.faces_connectivity[:, 1].astype(int)
+        all_surfaces = np.unique(surface_indexes)
+        unselected = np.unique(surface_indexes[mask_unselected])
+        return set(all_surfaces) - set(unselected)
 
     def area_pick_volumes(
         self,
@@ -226,21 +273,58 @@ class GeometrySelection:
 
         return mesh.nodal_coordinates[node_indexes]
 
+    def _get_coordinates_inside_area(
+        self,
+        coordinates: np.ndarray,
+        area: list[int, int, int, int],
+    ) -> list[int]:
+        x0, y0, x1, y1 = area
+        renderer = self.geometry_render_widget.renderer
+
+        upper_left_3d = screen_to_world_coords((x0, y0, 0), renderer)
+        upper_right_3d = screen_to_world_coords((x1, y0, 0), renderer)
+        bottom_left_3d = screen_to_world_coords((x0, y1, 0), renderer)
+
+        mask_horizontal = points_in_between(
+            coordinates,
+            upper_left_3d,
+            upper_right_3d,
+        )
+
+        mask_vertical = points_in_between(
+            coordinates,
+            upper_left_3d,
+            bottom_left_3d,
+        )
+
+        return mask_horizontal & mask_vertical
+
     def _area_pick_node_internal_indexes(
         self, x0: int, y0: int, x1: int, y1: int
     ) -> list[int]:
-        picker = vtkAreaPicker()
-        extractor = vtkExtractSelectedFrustum()
-        picker.AreaPick(x0, y0, x1, y1, self.geometry_render_widget.renderer)
-        extractor.SetFrustum(picker.GetFrustum())
-
         mesh = app().project.model.mesh
         if mesh is None:
             return set()
 
-        bounds = mesh.nodal_coordinates[:, (1, 1, 2, 2, 3, 3)]
-        picked_indexes = [
-            i for i, bound in enumerate(bounds) if extractor.OverallBoundsTest(bound)
-        ]
+        mask = self._get_coordinates_inside_area(
+            mesh.nodal_coordinates[:, 1:],
+            (x0, y0, x1, y1),
+        )
 
-        return picked_indexes
+        return np.arange(mesh.nodal_coordinates.shape[0])[mask]
+
+        # picker = vtkAreaPicker()
+        # extractor = vtkExtractSelectedFrustum()
+        # picker.AreaPick(x0, y0, x1, y1, self.geometry_render_widget.renderer)
+        # extractor.SetFrustum(picker.GetFrustum())
+
+        # mesh = app().project.model.mesh
+        # if mesh is None:
+        #     return set()
+
+        # bounds = mesh.nodal_coordinates[:, (1, 1, 2, 2, 3, 3)]
+        # picked_indexes = [
+        #     i for i, bound in enumerate(bounds) if extractor.OverallBoundsTest(bound)
+        # ]
+
+        # return picked_indexes
