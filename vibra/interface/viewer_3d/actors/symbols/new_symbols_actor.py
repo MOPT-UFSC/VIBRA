59d1be75a84784f8385d56149005d4767e16aa6c
André Fernandes <fpf.andre@gmail.com>
2025-02-24 15:50:06 -0300

    rename shapes to "shape names" instead of "symbol names" (does it make sense?)

--- vibra/interface/viewer_3d/actors/symbols/new_symbols_actor.py
+++ vibra/interface/viewer_3d/actors/symbols/new_symbols_actor.py
@@ -7,16 +7,18 @@ from vtkmodules.vtkFiltersSources import (
     vtkConeSource,
     vtkCubeSource,
 )
-from vibra import SYMBOLS_DIR
 
+from vibra import SYMBOLS_DIR
+from pathlib import Path
 from molde.colors import Color, color_names
 from vtkmodules.vtkIOGeometry import vtkOBJReader, vtkSTLReader
 
 from .common_symbols_actor_fixed_size import CommonSymbolsActorFixedSize
 from .common_symbols_actor_variable_size import CommonSymbolsActorVariableSize
+from vtkmodules.vtkCommonDataModel import vtkPolyData
 
 
-class NewSymbolsActor(CommonSymbolsActorVariableSize):
+class NewSymbolsActor(CommonSymbolsActorFixedSize):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configure_appearance()
@@ -39,11 +41,11 @@ class NewSymbolsActor(CommonSymbolsActorVariableSize):
 
     def add_force_symbol(self, position, orientation):
         self.add_symbol(
-            "force",
+            "arrow_1",
             position,
             orientation,
             color=color_names.RED,
-            scale=0.3,
+            scale=1,
         )
 
     def add_spring_symbol(self, position, orientation):
@@ -52,25 +54,25 @@ class NewSymbolsActor(CommonSymbolsActorVariableSize):
             position,
             orientation,
             color=color_names.ORANGE,
-            scale=0.3,
+            scale=1,
         )
 
     def add_prescribed_dof_symbol(self, position, orientation):
         self.add_symbol(
-            "dof_cone",
+            "cone",
             position,
             orientation,
             color=color_names.GREEN,
-            scale=0.3,
+            scale=1,
         )
 
     def add_volume_velocity_symbol(self, position, orientation):
         self.add_symbol(
-            "volume_velocity",
+            "arrow_2",
             position,
             orientation,
             color=color_names.RED,
-            scale=0.3,
+            scale=1,
         )
 
     def add_damper_symbol(self, position, orientation):
@@ -79,7 +81,7 @@ class NewSymbolsActor(CommonSymbolsActorVariableSize):
             position,
             orientation,
             color=color_names.PINK,
-            scale=0.3,
+            scale=1,
         )
 
     def add_mass_symbol(self, position, orientation):
@@ -88,103 +90,113 @@ class NewSymbolsActor(CommonSymbolsActorVariableSize):
             position,
             orientation,
             color=color_names.BLUE,
-            scale=0.3,
+            scale=1,
         )
 
     def add_acoustic_pressure_symbol(self, position, orientation):
         self.add_symbol(
-            "acoustic_pressure",
+            "arrow_3",
             position,
             orientation,
             color=color_names.PURPLE,
-            scale=0.3,
+            scale=1,
         )
 
     def add_impedance_symbol(self, position, orientation):
         self.add_symbol(
-            "impedance",
+            "cube",
             position,
             orientation,
             color=color_names.GREEN,
-            scale=0.3,
+            scale=1,
         )
 
     def _register_shapes(self):
-        self.register_shape("force", self._get_force_source())
-        self.register_shape("dof_cone", self._get_cone_source())
+        self.register_shape("arrow_1", self._get_arrow_1_source())
+        self.register_shape("arrow_2", self._get_arrow_2_source())
+        self.register_shape("arrow_3", self._get_arrow_3_source())
+        self.register_shape("cone", self._get_cone_source())
+        self.register_shape("cube", self._get_cube_source())
         self.register_shape("spring", self._get_spring_source())
-        self.register_shape("volume_velocity", self._get_volume_velocity_source())
         self.register_shape("damper", self._get_damper_source())
         self.register_shape("mass", self._get_mass_source())
-        self.register_shape("acoustic_pressure", self._get_acoustic_pressure_source())
-        self.register_shape("impedance", self._get_impedance_source())
 
-    def _get_force_source(self):
+    def _get_arrow_1_source(self):
         source = vtkArrowSource()
-        source.SetTipLength(.25)
+        source.SetTipLength(0.25)
+        source.Update()
+        return source.GetOutput()
+
+    def _get_arrow_2_source(self):
+        source = vtkArrowSource()
+        source.SetTipLength(0.85)
+        source.Update()
+        return source.GetOutput()
+
+    def _get_arrow_3_source(self):
+        source = vtkArrowSource()
+        source.SetTipLength(0.45)
         source.Update()
         return source.GetOutput()
 
     def _get_cone_source(self):
         source = vtkConeSource()
-        source.SetHeight(.5)
-        source.SetRadius(.7)
+        source.SetHeight(0.4)
+        source.SetRadius(0.2)
+        source.SetResolution(12)
         source.Update()
         return source.GetOutput()
 
-    def _get_spring_source(self):
-        reader = vtkSTLReader()
-        reader.SetFileName(str(SYMBOLS_DIR / "stl_files/spring_symbol.STL"))
-        reader.Update()
-        return reader.GetOutput()
-
-    def _get_volume_velocity_source(self):
-        source = vtkArrowSource()
-        source.SetTipLength(.85)
-        source.Update()
-        return source.GetOutput()
-
-    def _get_damper_source(self):
-        reader = vtkOBJReader()
-        reader.SetFileName(str(SYMBOLS_DIR / "structural/lumped_damper.obj"))
-        reader.Update()
-        return reader.GetOutput()
-
-    def _get_mass_source(self):
-        reader = vtkOBJReader()
-        reader.SetFileName(str(SYMBOLS_DIR / "structural/new_lumped_mass.obj"))
-        reader.Update()
-        transform = vtkTransform()
-        transform.RotateWXYZ(90, 0, 1, 0)
-        transformation = vtkTransformPolyDataFilter()
-        transformation.SetTransform(transform)
-        transformation.SetInputData(reader.GetOutput())
-        transformation.Update()
-        return transformation.GetOutput()
-
-    def _get_acoustic_pressure_source(self):
-        source = vtkArrowSource()
-        source.SetTipLength(.45)
-        source.Update()
-        return source.GetOutput()
-
-    def _get_impedance_source(self):
+    def _get_cube_source(self):
         source = vtkCubeSource()
         source.SetBounds(0, 1, 0, 1, 0, 1)
         source.Update()
         return source.GetOutput()
 
-    def _get_shape_obj_file(self, path: str):
+    def _get_spring_source(self):
+        return self._read_stl_file(SYMBOLS_DIR / "stl_files/spring_symbol.STL")
+
+    def _get_damper_source(self):
+        return self._read_obj_file(SYMBOLS_DIR / "structural/lumped_damper.obj")
+
+    def _get_mass_source(self):
+        return self._transform_source(
+            self._read_obj_file(SYMBOLS_DIR / "structural/new_lumped_mass.obj"),
+            rotation=(0, 90, 0),
+        )
+
+    def _read_obj_file(self, path: str | Path):
         reader = vtkOBJReader()
-        reader.SetFileName(str(SYMBOLS_DIR / "structural/lumped_damper.obj"))
+        reader.SetFileName(str(path))
+        # reader.SetFileName(str(SYMBOLS_DIR / "structural/lumped_damper.obj"))
         reader.Update()
         return reader.GetOutput()
 
-    def _get_shape_stl_file(self, path: str):
+    def _read_stl_file(self, path: str | Path):
         reader = vtkSTLReader()
         reader.SetFileName(str(path))
         reader.Update()
         return reader.GetOutput()
 
+    def _transform_source(
+        self,
+        source: vtkPolyData,
+        position=(0, 0, 0),
+        rotation=(0, 0, 0),
+        scale=(0, 0, 0),
+    ) -> vtkPolyData:
+        transform = vtkTransform()
+        transform.Translate(position)
+        transform.Scale(scale)
+        transform.RotateX(rotation[0])
+        transform.RotateY(rotation[1])
+        transform.RotateZ(rotation[2])
+        transformation = vtkTransformPolyDataFilter()
+        transformation.SetTransform(transform)
+        transformation.SetInputData(source)
+        transformation.Update()
+        return transformation.GetOutput()
+
     def configure_appearance(self):
         self.GetProperty().SetAmbient(0.5)
+        self.PickableOff()
