1f7e328f482226af163427de031b495a81fa04d6
Gustavo Martins <gucmartins@gmail.com>
2025-03-13 21:53:33 -0300

    Include Pardiso as linear operator for eigsh

--- vibra/engine/solvers/acoustic_modal_solver.py
+++ vibra/engine/solvers/acoustic_modal_solver.py
@@ -1,11 +1,31 @@
 import logging
 
 import numpy as np
-from scipy.sparse import lil_matrix, coo_matrix, csr_matrix
+from pypardiso import PyPardisoSolver
+from scipy.sparse import lil_matrix, coo_matrix, csr_matrix, triu
 from scipy.sparse.linalg import LinearOperator, eigs, eigsh, inv, lobpcg
 from scipy.sparse.csgraph import reverse_cuthill_mckee
-
 from vibra.utils.progress_status import ProgressStatus
+
+
+def pardiso_eigsh(matrix, k, M, sigma, **kwargs):
+    """Call sla.eigsh with pardiso support.
+
+    See scipy.sparse.linalg.eigsh for documentation.
+    """
+    class LuInv(LinearOperator):
+        def __init__(self, A):
+            ps = PyPardisoSolver(mtype=6)
+            ps.factorize(triu(A, format="csr"))
+            self.factorized_A = ps.factorized_A
+            self.solve = ps.solve
+            LinearOperator.__init__(self, A.dtype, A.shape)
+
+        def _matvec(self, x):
+            return self.solve(self.factorized_A, x.astype(self.dtype))
+
+    opinv = LuInv(matrix - sigma * M)
+    return eigsh(matrix, k, M=M, sigma=sigma, OPinv=opinv, **kwargs)
 
 
 class AcousticModalSolver:
@@ -46,7 +66,7 @@ class AcousticModalSolver:
 
         # self.plot_graph(K)
         logging.info("Solving the eigenproblem..." + ProgressStatus(10, 100))
-        self.eigen_values, self.eigen_vectors = eigs(K, M=M, k=self.modes, which=which, sigma=self.sigma_factor, tol=1e-2)
+        self.eigen_values, self.eigen_vectors = pardiso_eigsh(K, M=M, k=self.modes, which=which, sigma=self.sigma_factor, tol=1e-2)
 
         logging.info("Post-processing the solution..." + ProgressStatus(95, 100))
         positive_real = np.absolute(np.real(self.eigen_values))
