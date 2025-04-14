1f1a194bf964bde79dc033ae5a69bc88066236d5
Rodrigo Schwartz <rodrigo32003@gmail.com>
2025-04-14 18:51:35 -0300

    Fix super method calls

--- vibra/interface/analysis/structural_harmonic_analysis_mode_superpostion_method_input.py
+++ vibra/interface/analysis/structural_harmonic_analysis_mode_superpostion_method_input.py
@@ -2,9 +2,9 @@ from vibra.interface.analysis.analysis_setup_input import AnalysisSetupInput
 from vibra.interface.ui_generated.analysis.structural.harmonic_analysis_mode_superposition_method_ui import HarmonicAnalysisModeSuperpositionMethod_UI
 
 
-class AcousticHarmonicAnalysisDirectMethodInput(AnalysisSetupInput, HarmonicAnalysisModeSuperpositionMethod_UI):
+class StructuralHarmonicAnalysisModeSuperpositionMethodInput(AnalysisSetupInput, HarmonicAnalysisModeSuperpositionMethod_UI):
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
     
 
