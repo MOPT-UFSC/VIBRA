1d8471e209b44d5d39542a259d7254693d5018b7
Gustavo Martins <gucmartins@gmail.com>
2025-05-07 17:27:12 -0300

    Revert "Fix ShowPorousMaterialModelEquations class"
    This reverts commit 80ff4363256027ac3ed86a0303815f100454d980.

--- vibra/interface/model_inputs/acoustic/set_porous_material_model_inputs.py
+++ vibra/interface/model_inputs/acoustic/set_porous_material_model_inputs.py
@@ -151,7 +151,9 @@ class SetPorousMaterialModelInputs(SetPorousMaterialModel_UI):
             self.doubleSpinBox_flow_resistivity_JCAL.setValue(pm_data["flow_resistivity"])
 
     def show_equations_for_DBM_callback(self):
-        self.auxiliar_dialog = ShowPorousMaterialModelEquations()
+        file_path = "model/auxiliar/equations_for_DBM_porous_material_models.ui"
+        self.auxiliar_dialog = ShowPorousMaterialModelEquations(file_path)
+        self.auxiliar_dialog.exec()
 
     def update_plot_buttons_access(self):
         state = self.selected_fluid is None
