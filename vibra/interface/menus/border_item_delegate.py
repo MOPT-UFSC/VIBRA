1883f0948add998772db641d76fc559008b8c051
vini-ribeiro <vini.ribeiro2003@gmail.com>
2025-05-14 20:32:28 -0300

    change icon size and incorporate super() logic into paint and sizeHint

--- vibra/interface/menus/border_item_delegate.py
+++ vibra/interface/menus/border_item_delegate.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QStyledItemDelegate, QStyleOptionViewItem
+from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QStyledItemDelegate, QStyleOptionViewItem, QStyle
 from PySide6.QtGui import QIcon, QFont, QPixmap, QColor, QLinearGradient, QBrush, QPen, QPainter
 from PySide6.QtCore import Qt, QSize, QRect
+
 
 class BorderItemDelegate(QStyledItemDelegate):
     def __init__(self, parent, borderRole):
@@ -15,12 +16,20 @@ class BorderItemDelegate(QStyledItemDelegate):
     def sizeHint(self, option, index):        
         size = super(BorderItemDelegate, self).sizeHint(option, index)
         pen = index.data(self.borderRole)
+        
+        default_size = super().sizeHint(option, index)
+        tree = index.model().parent()
+        item = tree.itemFromIndex(index)
+        if item and item.parent():
+            return QSize(default_size.width(), 22)
+
         if pen is not None:        
             # Make some room for the border
             # When width is 0, it is a cosmetic pen which
             # will be 1 pixel anyways, so set it to 1
             width = max(pen.width(), 1)            
             size = size + QSize(2 * width, 2 * width)
+            size.setHeight(size.height() + 4)
         return size
     
     def size(self, item):
@@ -31,29 +40,34 @@ class BorderItemDelegate(QStyledItemDelegate):
     def paint(self, painter: QPainter, option, index):
         pen = index.data(self.borderRole)
         rect = QRect(option.rect)
-
-        if pen is not None:
-            width = max(pen.width(), 1)
-            # ...and remove the extra room we added in sizeHint...
-            option.rect.adjust(width, width - 1, -width, -width + 3)
-
-        icon = QIcon()
+        icon = index.data(Qt.DecorationRole)
+        
+        # if configuration not enabled, reduce opacity
+        if not (option.state & QStyle.State_Enabled):
+            painter.setOpacity(0.4)
+        else:
+            painter.setOpacity(1)
+            
+        child = False
         tree = index.model().parent()
         if tree:
             item = tree.itemFromIndex(index)
             if item.parent():
-                icon = item.icon(0)
-                item.setIcon(0, QIcon())
-        
-        super(BorderItemDelegate, self).paint(painter, option, index)
 
-        item.setIcon(0, icon)
+                child = True
+                text = index.data()
+                painter.drawText(rect, option.displayAlignment, text)
+
+                # draw icons
+                if icon is not None:
+                    icon_size = option.decorationSize.width() + 7
+                    spacing = 5
+                    icon_rect = QRect(option.rect.right() - icon_size - spacing, option.rect.top() + (option.rect.height() - icon_size)//2, icon_size, icon_size)
+                    icon.paint(painter, icon_rect)
         
-        icon_size = option.decorationSize
-        pixmap = icon.pixmap(icon_size)
-        x = rect.x() + (rect.width() - pixmap.width()) - 3
-        y = rect.y() + (rect.height() - pixmap.height()) // 2
-        painter.drawPixmap(x, y, pixmap)    
+        # to not draw it twice
+        if not child:
+            super(BorderItemDelegate, self).paint(painter, option, index)
         
         if pen is not None:
             painter.save() # Saves previous status
