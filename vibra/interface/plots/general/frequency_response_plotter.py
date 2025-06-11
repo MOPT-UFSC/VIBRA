64cc26453fdbf2f95efb7f363a7b917d1cccb963
JacsonGilVargas <jgvargasemc@gmail.com>
2025-06-10 22:34:13 -0300

    Close the Frequency Response Plotter QDialog whenever the close application is called

--- vibra/interface/plots/general/frequency_response_plotter.py
+++ vibra/interface/plots/general/frequency_response_plotter.py
@@ -16,6 +16,8 @@ class FrequencyResponsePlotter(FrequencyResponsePlot_UI):
     def __init__(self, *args, **kwargs):
         super().__init__()
 
+        app().main_window.set_input_widget(self)
+
         self._config_window()
         self._initialize()
         self._initialize_canvas()
@@ -89,6 +91,7 @@ class FrequencyResponsePlotter(FrequencyResponsePlot_UI):
         elif self.importer is None:
             self.importer = ImportDataToCompare(self)
             self.importer.exec()
+            app().main_window.set_input_widget(self)
 
     def _initial_config(self):
         self.aux_bool = False
