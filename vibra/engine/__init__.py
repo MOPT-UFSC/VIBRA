439678204e97b315d4960e329e13abd8bce31680
André Fernandes <fpf.andre@gmail.com>
2025-07-22 14:58:11 -0300

    refactor AnaysisID to check groups easily

--- vibra/engine/__init__.py
+++ vibra/engine/__init__.py
@@ -11,3 +11,49 @@ class AnalysisID(IntEnum):
     COUPLED_HARMONIC_DIRECT_METHOD = 5
     COUPLED_HARMONIC_MODE_SUPERPOSITION = 6
     STATIC_ANALYSIS = 7
+
+    def is_modal(self):
+        return self in [
+            self.ACOUSTIC_MODAL,
+            self.STRUCTURAL_MODAL,
+        ]
+
+    def is_harmonic(self):
+        return self in [
+            self.STRUCTURAL_HARMONIC_DIRECT_METHOD,
+            self.STRUCTURAL_HARMONIC_MODE_SUPERPOSITION,
+            self.ACOUSTIC_HARMONIC,
+            self.COUPLED_HARMONIC_DIRECT_METHOD,
+            self.COUPLED_HARMONIC_MODE_SUPERPOSITION,
+        ]
+
+    def is_acoustic(self):
+        return self in [self.ACOUSTIC_HARMONIC, self.ACOUSTIC_MODAL]
+
+    def is_structural(self):
+        return self in [
+            self.STRUCTURAL_HARMONIC_DIRECT_METHOD,
+            self.STRUCTURAL_HARMONIC_MODE_SUPERPOSITION,
+            self.STRUCTURAL_MODAL,
+        ]
+
+    def is_coupled(self):
+        return self in [
+            self.COUPLED_HARMONIC_DIRECT_METHOD,
+            self.COUPLED_HARMONIC_MODE_SUPERPOSITION,
+        ]
+
+    def is_direct_method(self):
+        return self in [
+            self.STRUCTURAL_HARMONIC_DIRECT_METHOD,
+            self.COUPLED_HARMONIC_DIRECT_METHOD,
+        ]
+    
+    def is_mode_superposition(self):
+        return self in [
+            self.STRUCTURAL_HARMONIC_MODE_SUPERPOSITION,
+            self.COUPLED_HARMONIC_MODE_SUPERPOSITION,
+        ]
+
+    def is_static(self):
+        return self in [self.STATIC_ANALYSIS]
