87ef9fba130509515383d6ed5f0ed321d0e34c25
JacsonGilVargas <jgvargasemc@gmail.com>
2025-07-31 01:11:35 -0300

    Address issues detected just after importing tabular data

--- vibra/interface/model_inputs/structural/normal_pressure_load_inputs.py
+++ vibra/interface/model_inputs/structural/normal_pressure_load_inputs.py
@@ -389,7 +389,7 @@ class NormalPressureLoadInputs(NormalPressureLoadInputs_UI):
         for surface_id in surface_ids:
             
             if self.pressure_table_values is not None:
-                self.pressure_table_name, self.pressure_array = self.save_table_files(surface_id, self.pressure_table_values, self.pressure_table_path)
+                self.pressure_table_name, self.pressure_array = self.save_table_files(surface_id, self.pressure_table_values)
                 if self.pressure_array is None:
                     return
 
