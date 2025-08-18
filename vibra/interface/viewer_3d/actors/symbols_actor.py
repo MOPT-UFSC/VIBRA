e0a0ccc60d76998a6cd37f73c6505e50bb589dbb
André Fernandes <fpf.andre@gmail.com>
2025-08-18 17:25:24 -0300

    Fix bug in symbols actor colors

--- vibra/interface/viewer_3d/actors/symbols_actor.py
+++ vibra/interface/viewer_3d/actors/symbols_actor.py
@@ -10,6 +10,7 @@ from vibra.interface.viewer_3d import sources
 
 Triple = tuple[float, float, float]
 
+
 class SymbolsActor(CommonSymbolsActorVariableSize):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -41,31 +42,28 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
             "incident_plane_wave": self._build_incident_plane_wave,
             "mass_source": self._build_mass_source,
         }
-    
+
     def _call_build_functions(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
-        # build a dict to map prop name to fun 
+        # build a dict to map prop name to fun
         if property_name in self.prop_name_to_build_func.keys():
             self.prop_name_to_build_func[property_name](
-                property_name=property_name,
-                surface_id=surface_id,
-                line_id=line_id,
-                point_id=point_id
+                property_name=property_name, surface_id=surface_id, line_id=line_id, point_id=point_id
             )
 
     def build(self):
         self.clear_symbols()
         self._build_nodal_normals()
-        
+
         point_properties = app().project.model.properties.point_properties
-        for (property_name, point_id) in point_properties.keys():
+        for property_name, point_id in point_properties.keys():
             self._call_build_functions(property_name, point_id=point_id)
-        
+
         line_properties = app().project.model.properties.line_properties
-        for (property_name, line_id) in line_properties.keys():
+        for property_name, line_id in line_properties.keys():
             self._call_build_functions(property_name, line_id=line_id)
 
         surface_properties = app().project.model.properties.surface_properties
-        for (property_name, surface_id) in surface_properties.keys():
+        for property_name, surface_id in surface_properties.keys():
             self._call_build_functions(property_name, surface_id=surface_id)
 
         super().build()
@@ -102,22 +100,22 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
         center_coords = np.average(line_coordinates, axis=0)
         dist = np.linalg.norm(line_coordinates - center_coords, axis=1)
         index = np.argmin(dist)
-        
+
         # Returns the 3 points around the center (including it)
         return (line_coordinates[index - 1, :], line_coordinates[index, :], line_coordinates[index + 1, :])
-    
+
     def _get_center_coords_and_normals_point(self, point_id: int) -> tuple[np.ndarray, np.ndarray]:
         mesh = app().project.model.mesh
         point_nodes = mesh.nodes_from_points.get(point_id)
         points_coordinates = mesh.nodal_coordinates[point_nodes, 1:]
-        
+
         return points_coordinates
 
     def _build_surface_velocity(self, surface_id: int = -1, *args, **kwargs):
         # how to display this symbol without normal???
         if surface_id is None:
             return
-        
+
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_surface_velocity_source, coords, normal, color=color_names.RED_6)
 
@@ -130,22 +128,22 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
     def _build_prescribed_dofs(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
         coords = None
         property = None
-        
+
         if surface_id != -1:
             coords, _ = self._get_center_coords_and_normals(surface_id)
             surface_properties = app().project.model.properties.surface_properties
             property = surface_properties[property_name, surface_id]
-        
+
         if line_id != -1:
             coords = self._get_center_coords_and_normals_line(line_id)[1]
             line_properties = app().project.model.properties.line_properties
             property = line_properties[property_name, line_id]
-            
+
         if point_id != -1:
             coords = self._get_center_coords_and_normals_point(point_id)
             point_properties = app().project.model.properties.point_properties
             property = point_properties[property_name, point_id]
-        
+
         if coords is not None and property is not None:
             x, y, z, *_ = property["values"]
 
@@ -169,11 +167,11 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
             surface_properties = app().project.model.properties.surface_properties
             property = surface_properties[property_name, surface_id]
             coords, normal = self._get_center_coords_and_normals(surface_id)
-            
+
             Fx, Fy, Fz, Mx, My, Mz = [(i if i is not None else 0) for i in property["values"]]
             force_orientation = np.real((Fx, Fy, Fz))
             m_orientation = np.real((Mx, My, Mz))
-            
+
             if np.any(force_orientation):
                 is_pointing = np.dot(normal, force_orientation) < 0
                 shape = sources.create_arrow_source if is_pointing else sources.create_outwards_arrow_source
@@ -182,35 +180,35 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
                 is_pointing = np.dot(normal, m_orientation) < 0
                 shape = sources.create_double_arrow_source if is_pointing else sources.create_outwards_arrow_source
                 self.add_symbol(shape, coords, m_orientation, color=color_names.BLUE_3)
-        
+
         property = None
         coord = None
-        
+
         if line_id != -1:
             coord = self._get_center_coords_and_normals_line(line_id)[1]
             line_properties = app().project.model.properties.line_properties
             property = line_properties[property_name, line_id]
-        
+
         if point_id != -1:
             coord = self._get_center_coords_and_normals_point(point_id)
             point_properties = app().project.model.properties.point_properties
             property = point_properties[property_name, point_id]
-        
+
         if property is not None and coord is not None:
             Fx, Fy, Fz, Mx, My, Mz = [(i if i is not None else 0) for i in property["values"]]
             force_orientation = np.real((Fx, Fy, Fz))
             m_orientation = np.real((Mx, My, Mz))
-            
+
             if np.any(force_orientation):
                 self.add_symbol(sources.create_arrow_source, coord, force_orientation, color=color_names.RED_2)
             if np.any(m_orientation):
                 self.add_symbol(sources.create_double_arrow_source, coord, m_orientation, color=color_names.BLUE_3)
-            
+
     def _build_distributed_loads(self, property_name: str, surface_id: int = -1, line_id: int = -1, *args, **kwargs):
         if surface_id != -1:
             surface_properties = app().project.model.properties.surface_properties
             property = surface_properties[property_name, surface_id]
-            
+
             coords, normal = self._get_center_coords_and_normals(surface_id)
             x, y, z, *_ = [(i if i is not None else 0) for i in property["values"]]
 
@@ -221,9 +219,11 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
 
             orientation = np.real((x, y, z))
             is_pointing = np.dot(normal, orientation) < 0
-            shape = sources.create_quadruple_arrow_source if is_pointing else sources.create_outwards_triple_arrow_source
+            shape = (
+                sources.create_quadruple_arrow_source if is_pointing else sources.create_outwards_triple_arrow_source
+            )
             self.add_symbol(shape, coords, orientation, color=color_names.RED_2)
-        
+
         if line_id != -1:
             line_properties = app().project.model.properties.line_properties
             property = line_properties[property_name, line_id]
@@ -240,11 +240,11 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
             self.add_symbol(sources.create_arrow_source, coords[0], orientation, color=color_names.RED_2)
             self.add_symbol(sources.create_arrow_source, coords[1], orientation, color=color_names.RED_2)
             self.add_symbol(sources.create_arrow_source, coords[2], orientation, color=color_names.RED_2)
-    
+
     def _build_normal_pressure_load(self, property_name: str, surface_id: int = -1, *args, **kwargs):
         surface_properties = app().project.model.properties.surface_properties
         property = surface_properties[property_name, surface_id]
-        
+
         coords, normal = self._get_center_coords_and_normals(surface_id)
         x = property["values"][0]
 
@@ -253,19 +253,23 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
 
         shape = sources.create_outwards_normal_pressure_load if np.real(x) > 0 else sources.create_normal_pressure_load
         self.add_symbol(shape, coords, normal, color=color_names.RED_2)
-    
+
     def _build_specific_impedance(self, property_name: str, surface_id: int = -1, *args, **kwargs):
         surface_properties = app().project.model.properties.surface_properties
         property = surface_properties[property_name, surface_id]
-        
+
         coords, normal = self._get_center_coords_and_normals(surface_id)
-        shape = sources.create_anechoic_termination_source if "anechoic_termination" in property.keys() else sources.create_impedance_source
+        shape = (
+            sources.create_anechoic_termination_source
+            if "anechoic_termination" in property.keys()
+            else sources.create_impedance_source
+        )
         self.add_symbol(shape, coords, normal, color=color_names.PURPLE_2)
-   
+
     def _build_transfer_impedance(self, surface_id: int = -1, *args, **kwargs):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_transfer_impedance_source, coords, normal, color=color_names.PURPLE_2)
-    
+
     def _build_perforated_plate_model(self, surface_id: int = -1, *args, **kwargs):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_perforated_plate_source, coords, normal, color=color_names.RED)
@@ -273,29 +277,29 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
     def _build_mass_flow_rate(self, surface_id: int = -1, *args, **kwargs):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_mass_flow_rate_source, coords, normal, color=color_names.PINK)
-    
+
     def _build_dofs_decoupling(self, surface_id: int = -1, *args, **kwargs):
         surface_properties = app().project.model.properties.surface_properties
         if ("perforated_plate_model", surface_id) in surface_properties.keys():
             return
         if ("transfer_impedance", surface_id) in surface_properties.keys():
-            return 
+            return
 
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_degrees_of_freedom_decoupling_source, coords, normal, color=color_names.GREEN)
-    
+
     def _build_absorption_surface(self, surface_id: int = -1, *args, **kwargs):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_absorption_surface_source, coords, normal, color=color_names.GREEN)
-    
+
     def _build_acoustic_pressure(self, surface_id: int = -1, *args, **kwargs):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_acoustic_pressure_source, coords, normal, color=color_names.RED_2)
-    
+
     def _build_reciprocating_compressor(self, property_name: str, surface_id: int = -1, *args, **kwargs):
         surface_properties = app().project.model.properties.surface_properties
         property = surface_properties[property_name, surface_id]
-        
+
         color = None
         if property["connection_type"] == "discharge":
             shape = sources.create_compressor_discharge_source
@@ -304,52 +308,57 @@ class SymbolsActor(CommonSymbolsActorVariableSize):
         elif property["connection_type"] == "suction":
             shape = sources.create_compressor_suction_source
             color = color_names.BLUE_3
-        
+
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(shape, coords, normal, color=color)
-    
+
     def _build_dissipation_model(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_dissipation_model_source, coords, normal, color=color_names.BLUE)
-    
-    def _build_viscous_thermal_loss_model(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
+
+    def _build_viscous_thermal_loss_model(
+        self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1
+    ):
         coords, normal = self._get_center_coords_and_normals(surface_id)
         self.add_symbol(sources.create_dissipation_model_source, coords, normal, color=color_names.ORANGE)
-    
-    def _build_acoustic_transfer_element_data(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
+
+    def _build_acoustic_transfer_element_data(
+        self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1
+    ):
         coords, normal = self._get_center_coords_and_normals(surface_id)
-        self.add_symbol(sources.create_acoustic_transfer_element_data_source, coords, normal, color=color_names.TURQUOISE)
-    
-    def _build_incident_plane_wave(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
+        self.add_symbol(
+            sources.create_acoustic_transfer_element_data_source, coords, normal, color=color_names.TURQUOISE
+        )
+
+    def _build_incident_plane_wave(
+        self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1
+    ):
         surface_properties = app().project.model.properties.surface_properties
         property = surface_properties[property_name, surface_id]
-        
+
         wave_vector = property.get("wave_vector")
         coords, _ = self._get_center_coords_and_normals(surface_id)
-        
+
         self.add_symbol(sources.create_incident_plane_wave_source, coords, wave_vector, color=color_names.BLUE)
-    
+
     def _build_mass_source(self, property_name: str, surface_id: int = -1, line_id: int = -1, point_id: int = -1):
         orientation = (0, 0, 0)
-        
+
         if surface_id != -1:
             coords, _ = self._get_center_coords_and_normals(surface_id)
         if line_id != -1:
             coords = self._get_center_coords_and_normals_line(line_id)[1]
         if point_id != -1:
             coords = self._get_center_coords_and_normals_point(point_id)
-        
+
         color_fir_sphere = color_names.RED.copy()
         self.add_symbol(sources.create_mass_load_first_layer_source, coords, orientation, color=color_fir_sphere)
-        
-        color_sec_sphere = color_names.YELLOW.copy()
-        color_sec_sphere.a = 150
+
+        color_sec_sphere = color_names.YELLOW.with_rgba(a=150)
         self.add_symbol(sources.create_mass_load_second_layer_source, coords, orientation, color=color_sec_sphere)
-        
-        color_third_sphere = color_names.GREEN.copy()
-        color_third_sphere.a = 100
+
+        color_third_sphere = color_names.GREEN.with_rgba(a=100)
         self.add_symbol(sources.create_mass_load_third_layer_source, coords, orientation, color=color_third_sphere)
-        
-        color_fourth_sphere = color_names.BLUE.copy()
-        color_fourth_sphere.a = 50
+
+        color_fourth_sphere = color_names.BLUE.with_rgba(a=50)
         self.add_symbol(sources.create_mass_load_fourth_layer_source, coords, orientation, color=color_fourth_sphere)
