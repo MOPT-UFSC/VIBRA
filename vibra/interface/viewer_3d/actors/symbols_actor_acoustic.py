7752b0373b76d1d19960f5368c0cdd3757942cad
JacsonGilVargas <jgvargasemc@gmail.com>
2025-08-25 13:47:14 -0300

    Fix a bug encountered while processing the incident plane wave symbol

--- vibra/interface/viewer_3d/actors/symbols_actor_acoustic.py
+++ vibra/interface/viewer_3d/actors/symbols_actor_acoustic.py
@@ -217,9 +217,7 @@ class SymbolsActorAcoustic(CommonSymbolsActorVariableSize):
             sources.create_acoustic_transfer_element_data_source, coords, normal, color=color_names.TURQUOISE
         )
 
-    def _build_incident_plane_wave(
-        self, property_name: str, surface_id: int = -1
-    ):
+    def _build_incident_plane_wave(self, property_name: str, surface_id: int = -1, *args, **kwargs):
         if surface_id == -1:
             return
 
