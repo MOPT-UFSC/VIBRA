895e74c89a75314fa85f783b7b4163148b718eab
André Fernandes <fpf.andre@gmail.com>
2025-09-02 18:06:41 -0300

    implement basic methods of visual mesh

--- vibra/engine/mesher/visual_mesh.py
+++ vibra/engine/mesher/visual_mesh.py
@@ -15,6 +15,9 @@ from OCC.Core.GCPnts import GCPnts_QuasiUniformDeflection
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from OCC.Core.TopTools import TopTools_IndexedMapOfShape
 
+from itertools import pairwise
+from typing import Iterator
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -35,29 +38,57 @@ class VisualMesh:
         edge_mapper = map_edges(shape)
         face_mapper = map_faces(shape)
 
+        # For some reason I can not use named args =(
         BRepMesh_IncrementalMesh(
-            theShape=shape,
-            theLinDeflection=deflection,
-            isRelative=False,
-            theAngDeflection=deflection,
-            isInParallel=True,
+            shape,
+            deflection,
+            False,  # is relative
+            deflection,
+            True,  # in parallel
         )
 
         loc = TopLoc_Location()
 
+        coords = list()
+        segments = list()
+        triangles = list()
+
+        visited_faces = set()
+        visited_edges = set()
+
         for face in iterate_faces(shape):
+            face_index = face_mapper.FindIndex(face)
+            if face_index in visited_faces:
+                continue
+            visited_faces.add(face_index)
+
             triangulation = BRep_Tool.Triangulation(face, loc)
 
             for a, b, c in extract_triangle_coords(triangulation):
-                pass
+                index_a = len(coords)
+                index_b = len(coords) + 1
+                index_c = len(coords) + 2
+                triangles.append([index_a, index_b, index_c])
+                coords.append(a)
+                coords.append(b)
+                coords.append(c)
 
             for edge in iterate_edges(face):
-                pass
+                edge_index = edge_mapper.FindIndex(edge)
+                if edge_index in visited_edges:
+                    continue
+                visited_edges.add(edge_index)
+
+                for a, b in pairwise(extract_edge_coords(edge, triangulation)):
+                    index_a = len(coords)
+                    index_b = len(coords) + 1
+                    coords.append(a)
+                    coords.append(b)
 
         return shape
 
 
-def extract_triangle_coords(triangulation):
+def extract_triangle_coords(triangulation) -> Iterator[tuple[np.ndarray, np.ndarray, np.ndarray]]:
     for i in range(1, triangulation.NbTriangles() + 1):
         triangle = triangulation.Triangle(i)
         a, b, c = triangle.Get()
@@ -67,9 +98,9 @@ def extract_triangle_coords(triangulation):
         node_c = triangulation.Node(c)
 
         yield (
-            (node_a.X(), node_a.Y(), node_a.Z()),
-            (node_b.X(), node_b.Y(), node_b.Z()),
-            (node_c.X(), node_c.Y(), node_c.Z()),
+            np.array([node_a.X(), node_a.Y(), node_a.Z()]),
+            np.array([node_b.X(), node_b.Y(), node_b.Z()]),
+            np.array([node_c.X(), node_c.Y(), node_c.Z()]),
         )
 
 
@@ -95,7 +126,7 @@ def _iterate_entities(shape, entity_type):
 
 def _map_repetitions(shape, entity_type):
     indexed_map = TopTools_IndexedMapOfShape()
-    topexp.MapShapes(shape, TopAbs_EDGE, entity_type)
+    topexp.MapShapes(shape, entity_type, indexed_map)
     return indexed_map
 
 
