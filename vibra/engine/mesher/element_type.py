d652f097d30aa3fe424d8be815273679fec577d4
JacsonGilVargas <jgvargasemc@gmail.com>
2025-09-03 13:19:45 -0300

    Adjusts the identations in the element_type file

--- vibra/engine/mesher/element_type.py
+++ vibra/engine/mesher/element_type.py
@@ -18,55 +18,55 @@ class ElementType:
 
 
 TETRAHEDRON_4 = ElementType(
-                            element_type = "tetrahedral",
-                            shape_function = "linear",
-                            algorithm_2d = gmsh_constants.DELAUNAY_2D,
-                            algorithm_3d = gmsh_constants.HXT_3D,
-                            subdivision_algorithm = gmsh_constants.NO_SUBDIVISION,
-                            recombination_algorithm = gmsh_constants.SIMPLE_RECOMBINATION,
-                            recombine_all = False,
-                            second_order_incomplete = False,
-                            element_order = 1,
-                            dimensions = 3,
-                            )
+    element_type = "tetrahedral",
+    shape_function = "linear",
+    algorithm_2d = gmsh_constants.DELAUNAY_2D,
+    algorithm_3d = gmsh_constants.HXT_3D,
+    subdivision_algorithm = gmsh_constants.NO_SUBDIVISION,
+    recombination_algorithm = gmsh_constants.SIMPLE_RECOMBINATION,
+    recombine_all = False,
+    second_order_incomplete = False,
+    element_order = 1,
+    dimensions = 3,
+)
 
 TETRAHEDRON_10 = ElementType(
-                            element_type = "tetrahedral",
-                            shape_function = "quadratic",
-                            algorithm_2d = gmsh_constants.DELAUNAY_2D,
-                            algorithm_3d = gmsh_constants.HXT_3D,
-                            subdivision_algorithm = gmsh_constants.NO_SUBDIVISION,
-                            recombination_algorithm = gmsh_constants.SIMPLE_RECOMBINATION,
-                            recombine_all = False,
-                            second_order_incomplete = False,
-                            element_order = 2,
-                            dimensions = 3,
-                            )
+    element_type = "tetrahedral",
+    shape_function = "quadratic",
+    algorithm_2d = gmsh_constants.DELAUNAY_2D,
+    algorithm_3d = gmsh_constants.HXT_3D,
+    subdivision_algorithm = gmsh_constants.NO_SUBDIVISION,
+    recombination_algorithm = gmsh_constants.SIMPLE_RECOMBINATION,
+    recombine_all = False,
+    second_order_incomplete = False,
+    element_order = 2,
+    dimensions = 3,
+)
 
 HEXAHEDRON_8 = ElementType(
-                            element_type = "hexahedral",
-                            shape_function = "linear",
-                            algorithm_2d = gmsh_constants.QUASI_STRUCTURED_QUADS_2D,
-                            algorithm_3d = gmsh_constants.DELAUNAY_3D,
-                            subdivision_algorithm = gmsh_constants.ALL_HEXAHEDRA_SUBDIVISION,
-                            recombination_algorithm = gmsh_constants.BLOSSOM_FULL_QUAD_RECOMBINATION,
-                            recombine_all = True,
-                            second_order_incomplete = False,
-                            element_order = 1,
-                            dimensions = 3,
-                            )
+    element_type = "hexahedral",
+    shape_function = "linear",
+    algorithm_2d = gmsh_constants.QUASI_STRUCTURED_QUADS_2D,
+    algorithm_3d = gmsh_constants.DELAUNAY_3D,
+    subdivision_algorithm = gmsh_constants.ALL_HEXAHEDRA_SUBDIVISION,
+    recombination_algorithm = gmsh_constants.BLOSSOM_FULL_QUAD_RECOMBINATION,
+    recombine_all = True,
+    second_order_incomplete = False,
+    element_order = 1,
+    dimensions = 3,
+)
 
 HEXAHEDRON_20 = ElementType(
-                            element_type = "hexahedral",
-                            shape_function = "quadratic",
-                            algorithm_2d = gmsh_constants.QUASI_STRUCTURED_QUADS_2D,
-                            algorithm_3d = gmsh_constants.DELAUNAY_3D,
-                            subdivision_algorithm = gmsh_constants.ALL_HEXAHEDRA_SUBDIVISION,
-                            recombination_algorithm = gmsh_constants.BLOSSOM_FULL_QUAD_RECOMBINATION,
-                            recombine_all = True,
-                            second_order_incomplete = True,
-                            element_order = 2,
-                            dimensions = 3,
-                            )
+    element_type = "hexahedral",
+    shape_function = "quadratic",
+    algorithm_2d = gmsh_constants.QUASI_STRUCTURED_QUADS_2D,
+    algorithm_3d = gmsh_constants.DELAUNAY_3D,
+    subdivision_algorithm = gmsh_constants.ALL_HEXAHEDRA_SUBDIVISION,
+    recombination_algorithm = gmsh_constants.BLOSSOM_FULL_QUAD_RECOMBINATION,
+    recombine_all = True,
+    second_order_incomplete = True,
+    element_order = 2,
+    dimensions = 3,
+)
 
 DEFAULT_ELEMENT_TYPE = TETRAHEDRON_4
