ad2b154c0c50e6e5acc22b6924e26a0f18ac483e
Rodrigo Schwartz <rodrigo32003@gmail.com>
2025-09-03 15:15:17 -0300

    Recompile some ui files

--- vibra/interface/ui_generated/model/setup/data_filter/change_frequency_data_range_input_ui.py
+++ vibra/interface/ui_generated/model/setup/data_filter/change_frequency_data_range_input_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'change_frequency_data_range_input.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.9.1
+## Created by: Qt User Interface Compiler version 6.9.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
