f2ee2bae21f1c42830f29bb11d47b79245da0f14
JacsonGilVargas <jgvargasemc@gmail.com>
2025-10-03 19:06:53 -0300

    Update the Nastran-related validation files

---
+++ vibra/engine/solvers/_tests/test_acoustic_harmonic_solver_nastran.py
@@ -0,0 +1,167 @@
+
+
+from vibra import PROJECT_DIR
+from vibra.engine.model import Model
+from vibra.engine.properties.fluid import Fluid
+
+from vibra.engine.assemblers.acoustic_assembler import AcousticAssembler
+from vibra.engine.solvers import HarmonicSolver
+from vibra.project_files.project_file import ProjectFile
+
+import os
+import pytest
+import numpy as np
+
+from pathlib import Path
+from pandas import read_excel
+from openpyxl import load_workbook
+
+def _acoustic_model_nastran(path: str, fluid: Fluid) -> Model:
+    
+    # mesh_setup = dict(minimum_element_size=50, maximum_element_size=50)
+
+    model = Model()
+    model.set_geometry_path(path)
+    model.set_length_unit()
+    model.set_geometry_quality_factor()
+    model.initialize_mesh()
+    # model.set_mesh_setup(mesh_setup)
+    model.process_mesh_data(path)
+
+    for vol_id in [1, 2]:
+        model.properties._set_property("fluid", fluid, volume=vol_id)
+        for surf_id in model.mesh.surfaces_from_volume.get(vol_id):
+            model.properties._set_property("fluid", fluid, surface=surf_id)
+
+    # Normal surface velocity data
+    data_Vn = {
+        "real_values": [1],
+        "imag_values": [0],
+        "nodal_attribution": False,
+        "averaged": False,
+    }
+
+    ## normal surface velocity data
+    data_Pa = { 
+                "real_values" : [1],
+                "imag_values" : [0],
+                }
+
+    ## boundary impedance setup
+    Zo = fluid.impedance
+
+    data_Z = {  
+              "real_values" : [Zo],
+              "imag_values" : [0],
+              }
+
+    # model.properties._set_property("surface_velocity", data_Vn, surface=1)
+    model.properties._set_property("acoustic_pressure", data_Pa, surface=1)
+    model.properties._set_property("specific_impedance", data_Z, surface=11)
+
+    # Define the analysis frequency setup
+    df = 5
+    f_min = 100
+    f_max = 1400
+    frequencies = np.arange(f_min, f_max + df, df, dtype=float)
+    model.process_viscous_thermal_model_properties(frequencies)
+
+    analysis_setup = {
+        "analysis_id": 3,
+        "f_min": f_min,
+        "f_max": f_max,
+        "f_step": df,
+        "frequencies": frequencies,
+    }
+
+    model.set_analysis_setup(analysis_setup)
+
+    return model
+
+
+def get_external_results(path: str) -> dict:
+
+    imported_results = dict()
+
+    if not Path(path).exists():
+        return imported_results
+
+    wb = load_workbook(path)
+
+    skiprows = 0
+    sheetnames = wb.sheetnames
+
+    for sheetname in sheetnames:
+
+        try:
+            sheet_data = read_excel(
+                                    path, 
+                                    sheet_name = sheetname, 
+                                    header = skiprows, 
+                                    usecols = [0,1,2]
+                                    ).to_numpy()
+
+        except:
+            sheet_data = read_excel(
+                                    path, 
+                                    sheet_name = sheetname, 
+                                    header = skiprows, 
+                                    usecols = [0,1]
+                                    ).to_numpy()
+            
+        filtered_data = [row_data for row_data in sheet_data if not isinstance(row_data[0], str)]
+        sheet_data = np.array(filtered_data, dtype=float)
+
+        imported_results[sheetname] = sheet_data
+
+    return imported_results
+
+
+def _solve_harmonic_problem(datadir, model: "Model", path: str):
+
+    assembler = AcousticAssembler(model)
+    assembler.process_assemble()
+    project_file = ProjectFile(str(datadir))
+    harmonic_solver = HarmonicSolver(assembler, project_file)
+
+    frequencies = model.frequencies
+
+    # Solve and store solutions into hdf5 files
+    solution = harmonic_solver.solve_direct(print_log=False)
+
+    output_surface_nodes = model.mesh.get_nodes_from_surface(11)
+    average_solution = np.average(solution[output_surface_nodes, :], axis=0)
+
+    results_path = os.path.dirname(path) + "/acoustic_pressures_reference.xlsx"
+    external_data = get_external_results(results_path)
+
+    output_pressures = external_data.get("output_surface")
+    reference_solution = output_pressures[:, 1] + 1j*output_pressures[:, 2]
+
+    if output_pressures[:, 0].size == frequencies.size:
+        diff_abs = np.abs((average_solution - reference_solution) / reference_solution)
+        assert np.max(diff_abs) < 0.02
+
+@pytest.mark.skip
+def test_solve_model_for_tet4_element(fluid: Fluid, datadir: Path):
+    path = str(PROJECT_DIR / "validation_files/data/Comsol/tet4_linear/rectangular_cavities_tet4_40x30_mm.nas")
+    model = _acoustic_model_nastran(path, fluid)
+    _solve_harmonic_problem(datadir, model, path)
+
+@pytest.mark.skip
+def test_solve_model_for_tet10_element(fluid: Fluid, datadir: Path):
+    path = str(PROJECT_DIR / "validation_files/data/Comsol/tet10_lagrange/rectangular_cavities_tet10_40x30_mm.nas")
+    model = _acoustic_model_nastran(path, fluid)
+    _solve_harmonic_problem(datadir, model, path)
+
+@pytest.mark.skip
+def test_solve_model_for_hex8_element(fluid: Fluid, datadir: Path):
+    path = str(PROJECT_DIR / "validation_files/data/Comsol/hex8_linear/rectangular_cavities_hex8_40x30_mm.nas")
+    model = _acoustic_model_nastran(path, fluid)
+    _solve_harmonic_problem(datadir, model, path)
+
+@pytest.mark.skip
+def test_solve_model_for_hex20_element(fluid: Fluid, datadir: Path):
+    path = str(PROJECT_DIR / "validation_files/data/Comsol/hex20_lagrange/rectangular_cavities_hex20_40x30_mm.nas")
+    model = _acoustic_model_nastran(path, fluid)
+    _solve_harmonic_problem(datadir, model, path)
