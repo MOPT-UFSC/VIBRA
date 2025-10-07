4488e41b5354be03ebaad0a1ec94787a317fb18b
JacsonGilVargas <jgvargasemc@gmail.com>
2025-10-06 23:17:16 -0300

    Update the tooltips

--- vibra/interface/menus/tool_tips_dict.py
+++ vibra/interface/menus/tool_tips_dict.py
@@ -1,59 +1,60 @@
 tool_tips: dict[str, str] = dict()
 
 tool_tips["material"] = (
-    "Use this feature to configure the materials from the materials library or set "
+    "Add/edit the materials from the materials library or set "
     "a material to selected entities."
     )
 
 tool_tips["fluid"] = (
-    "Use this feature to configure the fluids from the fluids library or set "
+    "Add/edit the fluids from the fluids library or set "
     "a fluid to selected entities."
 )
 
 tool_tips["mesh_setup"] = (
-    "Use this feature to configure the mesher to discretize the model domains."
+    "Configure the mesher parameters to discretize the model domains and boundaries "
+    "using solid, face, and line elements."
 )
 
 tool_tips["degrees_of_freedom_decoupling"] = (
-    "Use this feature to disconnect the nodes from connected volumes and modifies "
-    "the connectivity from all geometric entities. Both structural and acoustic "
+    "This feature disconnects the nodes from connected volumes and modifies the "
+    "connectivity from all geometric entities. Both structural and acoustic "
     "and degrees of freedom will be increased and decoupled. "
 )
 
 tool_tips["surface_thickness"] = (
-    "Use this feature to define the wall thickness of selected surfaces."
+    "Define the wall thickness of selected surfaces for face elements."
 )
 
 tool_tips["prescribed_dof"] = (
-    "Use this feature to prescribed the known values for the structural degrees of freedom."
+    "Define the known values for the structural degrees of freedom."
 )
 
 tool_tips["nodal_loads"] = (
-    "Use this feature to assign equally distributed nodal loads."
+    "Set equally distributed structural nodal loads for the selected entities."
     )
 
 tool_tips["distributed_loads"] = (
-    "Use this feature to assign distributed loads over a surface or along a line."
+    "Set distributed loads over selected surfaces or along lines."
     )
 
 tool_tips["normal_pressure_load"] = (
-    "Use this featyre to distribute a normal pressure load over a surface surface."
-    )   
+    "Set distributed normal pressure loads over selected surfaces."
+    )
 
 tool_tips["acoustic_pressure"] = (
-    "Use this feature to prescribed the known values for the acoustic pressure."
+    "Create a boundary condition that prescribe an acoustic pressure at the boundary of domain."
     )
 
 tool_tips["mass_source"] = (
-    "Use this feature to assign a mass source to a point, or over even a line, surface, or volume."
+    "Add a mass source to a point, node, or over a line, surface, or volume."
     )
 
 tool_tips["surface_velocity"] = (
-    "Define the velocity of a oscillating surface in the acoustic model."
+    "Set a normal surface velocity in the selected surfaces."
     )
 
 tool_tips["anechoic_termination"] = (
-    "Define an anechoic termination (non-reflecting) boundary condition for the acoustic model."
+    "Define an anechoic termination (non-reflecting) boundary condition for the selected surface."
 )
 
 tool_tips["absorption_surface"] = (
@@ -61,38 +62,40 @@ tool_tips["absorption_surface"] = (
     )
 
 tool_tips["specific_impedance"] = (
-    "Use this feature to assign a specific impedance to a surface."
+    "Define a specific impedance at a boundary in the domain."
     )
 
 tool_tips["transfer_impedance"] = (
-    "Use this feature to define an internal transfer impedance for the acoustic model."
+    "Set an internal transfer impedance at an interface boundary between two domains."
 )
 
 tool_tips["perforated_plate_model"] = (
-    "Use this feature to configure the internal perforated plate for the acoustic model."
+    "Set an internal perforated plate for the acoustic model to simulate the pressure drop "
+    "across this component."
     )
 
 tool_tips["propotional_damping"] = (
-    "Use this feature to configure the proportional damping for the acoustic model."
+    "Configure the proportional damping factors for the acoustic model."
     )
 
 tool_tips["porous_material_model"] = (
-    "Use this feature to configure the porous material dissipation models for the acoustic model."
+    "Set a domain as a porous material modeled in a homogenized way using a so-called equivalent "
+    "fluid by Delany-Bazley, Delany-Bazley-Miki, JCA and JCAL model."
     )
 
 tool_tips["viscous_thermal_model"] = (
-    "Use this feature to enable the viscous-thermal losses for the acoustic model."
+    "Set an equivalent fluid model for viscous thermal boundary-layer induced loss in circular, "
+    "rectangular, quadrangular or narrow slits ducts of constant cross section."
     )
 
 tool_tips["reciprocating_compressor_excitation"] = (
-    "Use this feature to add an equivalent reciprocating compressor excitation for the acoustic model."
+    "Add an idealized reciprocating compressor excitation in the form of an equivalent surface velocity."
+    )
+
+tool_tips["acoustic_properties_gradient"] = (
+    "Assign a gradient of acoustic properties to model properties variations along one defined axis."
     )
 
 tool_tips["acoustic_transfer_element_setup"] = (
     "Use this feature to configure the surfaces where the acoustic transfer element will be computed."
-    )
-
-tool_tips["acoustic_properties_gradient"] = (
-    "Use this feature to assign the gradient of acoustic properties "
-    "to model properties variations along one defined axis."
     )
