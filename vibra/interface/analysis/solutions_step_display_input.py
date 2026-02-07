d7d98f8900b50c1dcbe615eef3b4563cc56f4f0c
JacsonGilVargas <jgvargasemc@gmail.com>
2026-02-07 01:07:14 -0300

    Add an interface to display the solution steps

---
+++ vibra/interface/analysis/solutions_step_display_input.py
@@ -0,0 +1,77 @@
+from PySide6.QtWidgets import QAbstractItemView, QHeaderView, QTableWidgetItem
+from PySide6.QtGui import Qt
+
+from vibra import app
+from vibra.interface.ui_generated.analysis.solution_steps_display_input_ui import SolutionStepsDisplayInput_UI
+
+import numpy as np
+
+error_title = "Error"
+
+
+class SolutionStepsDisplayInput(SolutionStepsDisplayInput_UI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+
+        self.project = app().project
+        self.model = app().project.model
+        self.analysis_setup = app().project.analysis_setup
+
+        # app().main_window.close_dialogs()
+        # app().main_window.set_input_widget(self)
+
+        self._initialize()
+        self._config_window()
+        self._create_connections()
+
+        self.load_solution_steps()
+
+        while self.keep_window_open:
+            self.exec()
+
+    def _initialize(self):
+        self.setup_defined = False
+        self.solve_analysis = False
+        self.keep_window_open = True
+        self.user_defined_frequencies = list()
+        self.table_exists = self.model.properties.check_if_there_are_tables_at_the_model()
+        self.tabular_frequency_setup = self.model.get_tabular_frequency_setup()
+
+    def _config_window(self):
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setWindowModality(Qt.WindowModal)
+        self.setWindowIcon(app().main_window.vibra_icon)
+        self.setWindowTitle("Analysis setup")
+
+    def _create_connections(self):
+        self.pushButton_exit.clicked.connect(self.close)
+
+    def load_solution_steps(self):
+        self.tableWidget_solution_steps.clearContents()
+        self.tableWidget_solution_steps.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.tableWidget_solution_steps.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tableWidget_solution_steps.setSelectionMode(QAbstractItemView.NoSelection)
+
+        frequencies = self.model.frequencies
+        if frequencies is None:
+            return
+                
+        if isinstance(frequencies, np.ndarray):
+            self.tableWidget_solution_steps.setRowCount(frequencies.size)
+            frequency_spacing = self.model.analysis_setup.get("frequency_spacing", "--")
+
+            for index, freq in enumerate(frequencies):
+                self.tableWidget_solution_steps.setItem(index, 0, QTableWidgetItem(str(index+1)))
+                self.tableWidget_solution_steps.setItem(index, 1, QTableWidgetItem(str(freq)))
+                self.tableWidget_solution_steps.setItem(index, 2, QTableWidgetItem(frequency_spacing))
+
+                for j in range(3):
+                    self.tableWidget_solution_steps.item(index, j).setTextAlignment(Qt.AlignCenter)
+
+    def closeEvent(self, a0):
+        self.keep_window_open = False
+        return super().closeEvent(a0)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return or event.key() == Qt.Key_Escape:
+            self.close()
