c6ffacd810172fcffe10c486452ae595191064bf
JacsonGilVargas <jgvargasemc@gmail.com>
2026-02-10 15:24:41 -0300

    Remove some unused imports and methods

--- vibra/interface/model_inputs/acoustic/excitations/incident_plane_wave_inputs.py
+++ vibra/interface/model_inputs/acoustic/excitations/incident_plane_wave_inputs.py
@@ -4,11 +4,10 @@ from PySide6.QtGui import QCloseEvent
 
 from vibra import app
 from vibra.interface.data.data_manager import get_spectral_data_from_array
-from vibra.interface.ui_generated.model.setup.acoustic.incident_plane_wave_inputs_ui import IncidentPlaneWaveInputs_UI
-from vibra.interface.model_inputs.data_filter.change_frequency_data_handler import ChangeFrequencyDataRangeInput
 from vibra.interface.general.get_user_confirmation_input import GetUserConfirmationInput
 from vibra.interface.general.print_message_input import PrintMessageInput
 from vibra.interface.data_handler.data_importer import DataImporter
+from vibra.interface.ui_generated.model.setup.acoustic.incident_plane_wave_inputs_ui import IncidentPlaneWaveInputs_UI
 
 import numpy as np
 
@@ -662,13 +661,6 @@ class IncidentPlaneWaveInputs(IncidentPlaneWaveInputs_UI):
         app().file.write_imported_table_data_in_file()
         app().main_window.update_info_text()
         app().main_window.update_symbols()
-
-    def change_frequency_setup(self):
-        if self.imported_values is not None:
-            self.hide()
-            obj = ChangeFrequencyDataRangeInput(self.imported_values)
-            if obj.filter_data is not None:
-                self.imported_values = obj.filter_data
 
     def check_model_frequency_controls(self):
 
--- vibra/interface/model_inputs/structural/excitations/distributed_loads_inputs.py
+++ vibra/interface/model_inputs/structural/excitations/distributed_loads_inputs.py
@@ -5,10 +5,9 @@ from PySide6.QtGui import QCloseEvent
 
 from vibra import app
 from vibra.interface.data_handler.data_importer import DataImporter
-from vibra.interface.ui_generated.model.setup.structural.distributed_loads_inputs_ui import DistributedLoadsInputs_UI
 from vibra.interface.general.get_user_confirmation_input import GetUserConfirmationInput
-from vibra.interface.model_inputs.data_filter.change_frequency_data_handler import ChangeFrequencyDataRangeInput
 from vibra.interface.general.print_message_input import PrintMessageInput
+from vibra.interface.ui_generated.model.setup.structural.distributed_loads_inputs_ui import DistributedLoadsInputs_UI
 
 import numpy as np
 from os.path import basename
@@ -790,13 +789,6 @@ class DistributedLoadsInputs(DistributedLoadsInputs_UI):
         app().file.write_model_properties_in_file()
         app().file.write_imported_table_data_in_file()
         app().main_window.update_symbols()
-
-    def change_frequency_setup(self):
-        if self.imported_values is not None:
-            self.hide()
-            obj = ChangeFrequencyDataRangeInput(self.imported_values)
-            if obj.filter_data is not None:
-                self.imported_values = obj.filter_data
 
     def check_model_frequency_controls(self):
 
