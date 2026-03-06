a92e26dfa7c3a30e3fa8feb9d1facd83f3a69837
André Fernandes <fpf.andre@gmail.com>
2026-03-06 12:30:48 -0300

    Fix bugs and add an error message if HarmonicAnalysisSetup is being instantiated

--- vibra/engine/analysis_info/harmonic_analysis_setup.py
+++ vibra/engine/analysis_info/harmonic_analysis_setup.py
@@ -1,14 +1,19 @@
 from dataclasses import KW_ONLY, dataclass, replace
-from typing import Literal, Self
+from typing import Literal, Optional, Self
 
 import numpy as np
-from pyparsing import Optional
 
 
 class HarmonicAnalysisSetup:
     analysis_method: Literal["direct", "mode_superposition"] = "direct"
     global_damping: tuple[float, float, float] = (0.0, 0.0, 0.0)
     modes_number: None | int = None
+
+    def __init__(self, *args, **kwargs):
+        subclass_names = ", ".join([f"{cls.__module__}.{cls.__name__}" for cls in self.__class__.__subclasses__()])
+        msg = "HarmonicAnalysisSetup can not be intantiated.\n"
+        msg += f"Use one of the following classes instead: {subclass_names}"
+        raise ValueError(msg)
 
     def replace(self, **changes) -> Self:
         return replace(self, **changes)
