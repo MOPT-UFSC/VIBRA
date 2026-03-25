f390ef7e363962a7a05368894b9222d232153156
André Fernandes <fpf.andre@gmail.com>
2026-03-25 15:21:54 -0300

    Add structural and acoustic tests

---
+++ vibra/engine/_tests/test_harmonic_acoustic.py
@@ -0,0 +1,78 @@
+import pytest
+
+from vibra import PROJECT_DIR, errors
+from vibra.engine.analysis_info import AnalysisID, HarmonicAnalysisSetupRange, ModalAnalysisSetup
+from vibra.engine.project import Project
+
+
+def test_harmonic_acoustic():
+    mesh_path = PROJECT_DIR / "data/examples/mesh_files/branch_cylinders.msh"
+    reference_project = PROJECT_DIR / "validation_files/test_projects/branch_cylinders_harmonic_acoustic.vibra"
+
+    project = Project()
+    project.import_mesh(mesh_path)
+
+    with pytest.raises(errors.IncompleteSetupError):
+        project.run_analysis()
+
+    with pytest.raises(errors.InvalidModelSetupError):
+        # Incompatible AnalysisID and AnalysisSetup
+        project.configure_analysis(AnalysisID.ACOUSTIC_HARMONIC, ModalAnalysisSetup(10, 0.01))
+        project.run_analysis()
+
+    project.configure_analysis(
+        AnalysisID.ACOUSTIC_HARMONIC,
+        HarmonicAnalysisSetupRange(
+            f_min=1_000,
+            f_max=10_000,
+            f_step=1_000,
+        ),
+    )
+
+    with pytest.raises(errors.InvalidModelSetupError):
+        # Fluid not configured
+        project.run_analysis()
+
+    fluid = project.fluid_library.find_by_name("Methane")
+    assert fluid is not None
+
+    project.model.properties._set_property("fluid", fluid, volume=1)
+    project.model.properties._set_property("fluid", fluid, surface=1)
+    project.model.properties._set_property("fluid", fluid, surface=2)
+    project.model.properties._set_property("fluid", fluid, surface=3)
+    project.model.properties._set_property("fluid", fluid, surface=4)
+    project.model.properties._set_property("fluid", fluid, surface=5)
+
+    project.model.properties._set_property(
+        "surface_velocity",
+        {
+            "real_values": [1],
+            "imag_values": [0],
+            "nodal_attribution": False,
+            "averaged": False,
+        },
+        surface=2,
+    )
+
+    project.model.properties._set_property(
+        "absorption_surface",
+        {
+            "real_values": [0.2],
+            "imag_values": [0],
+        },
+        surface=3,
+    )
+
+    project.model.properties._set_property(
+        "specific_impedance",
+        {
+            "anechoic_termination": True,
+            "volume_id": 1,
+        },
+        surface=5,
+    )
+
+    solution = project.run_analysis()
+    reference_solution = Project().load_project(reference_project).run_analysis()
+
+    assert solution == reference_solution
---
+++ vibra/engine/_tests/test_harmonic_structural.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+import pytest
+
+from vibra import PROJECT_DIR, errors
+from vibra.engine.analysis_info import AnalysisID, HarmonicAnalysisSetupRange, ModalAnalysisSetup
+from vibra.engine.mesher.mesh_setup import MeshSetup
+from vibra.engine.project import Project
+
+
+def test_harmonic_structural():
+    mesh_path = PROJECT_DIR / "data/examples/mesh_files/branch_cylinders.msh"
+    reference_project = PROJECT_DIR / "validation_files/test_projects/branch_cylinders_harmonic_structural.vibra"
+
+    project = Project()
+    project.import_mesh(mesh_path)
+
+    with pytest.raises(errors.IncompleteSetupError):
+        project.run_analysis()
+
+    with pytest.raises(errors.InvalidModelSetupError):
+        # Incompatible AnalysisID and AnalysisSetup
+        project.configure_analysis(AnalysisID.STRUCTURAL_HARMONIC, ModalAnalysisSetup(10, 0.01))
+        project.run_analysis()
+
+    project.configure_analysis(
+        AnalysisID.STRUCTURAL_HARMONIC,
+        HarmonicAnalysisSetupRange(
+            f_min=100,
+            f_max=50_000,
+            f_step=5_000,
+        ),
+    )
+
+    with pytest.raises(errors.InvalidModelSetupError):
+        # Material not configured
+        project.run_analysis()
+
+    material = project.material_library.find_by_name("Ni-Co-Cr_Alloy")
+    assert material is not None
+
+    project.model.properties._set_property("material", material, volume=1)
+    project.model.properties._set_property("material", material, surface=1)
+    project.model.properties._set_property("material", material, surface=2)
+    project.model.properties._set_property("material", material, surface=3)
+    project.model.properties._set_property("material", material, surface=4)
+    project.model.properties._set_property("material", material, surface=5)
+
+    project.model.properties._set_property(
+        "prescribed_dof",
+        {
+            "element_type": "3d_element",
+            "real_values": [0, 0, 0],
+            "imag_values": [0, 0, 0],
+        },
+        surface=3,
+    )
+
+    project.model.properties._set_property(
+        "nodal_loads",
+        {
+            "element_type": "3d_element",
+            "real_values": [1, 0, 2],
+            "imag_values": [0, 0, 0],
+            "nodal_attribution": True,
+            "averaged": True,
+        },
+        surface=5,
+    )
+
+    solution = project.run_analysis()
+    reference_solution = Project().load_project(reference_project).run_analysis()
+
+    assert solution == reference_solution
