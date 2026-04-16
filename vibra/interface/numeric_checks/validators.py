0ae56fd6b8d01327ab61a024c22c20a2357c0d63
André Fernandes <fpf.andre@gmail.com>
2026-04-16 18:07:57 -0300

    Improve StrictDoubleValidator implementation

--- vibra/interface/numeric_checks/validators.py
+++ vibra/interface/numeric_checks/validators.py
@@ -4,21 +4,44 @@ from PySide6.QtGui import QDoubleValidator
 class StrictDoubleValidator(QDoubleValidator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._last_acceptable = None
+        self.setNotation(QDoubleValidator.Notation.StandardNotation)
+
+    def fixup(self, string: str):
+        if self._last_acceptable is None:
+            return str(self.bottom())
+
+        if is_numeric(string):
+            value = float(string)
+            if value < self.bottom():
+                return str(self.bottom())
+            elif value > self.top():
+                return str(self.top())
+
+        return self._last_acceptable
 
     def validate(self, string: str, pos: int):
         string = string.replace(",", ".")
 
+        if not string:
+            return QDoubleValidator.State.Intermediate, string, pos
+
+        if string == "-":
+            return QDoubleValidator.State.Intermediate, string, pos
+
         if string.count(".") > 1:
             return QDoubleValidator.State.Invalid, string, pos
 
-        if not is_numeric(string):
-            return super().validate(string, pos)
+        if is_numeric(string):
+            value = float(string)
+            if value < self.bottom() or value > self.top():
+                return QDoubleValidator.State.Intermediate, string, pos
 
-        value = float(string)
-        if value < self.bottom() or value > self.top():
-            return QDoubleValidator.State.Intermediate, string, pos
+        return_value = super().validate(string, pos)
+        if return_value[0] == QDoubleValidator.State.Acceptable:
+            self._last_acceptable = string
 
-        return super().validate(string, pos)
+        return return_value
 
 
 def is_numeric(value: str):
