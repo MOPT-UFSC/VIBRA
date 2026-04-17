379f8e2b0ca1a281130acc86d8ccb099b2f01646
André Fernandes <fpf.andre@gmail.com>
2026-04-17 10:45:11 -0300

    Fix int_list_validator

--- vibra/interface/numeric_checks/int_list_validator.py
+++ vibra/interface/numeric_checks/int_list_validator.py
@@ -6,13 +6,19 @@ from PySide6.QtGui import QValidator
 class IntListValidator(QValidator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._re = re.compile(r"^\d+(\s*,\s*\d+)*,?$")
+        self._re = re.compile(r"\s*\d+\s*(,\s*\d+\s*)*,?\s*")
 
     def validate(self, string: str, pos: int):
         if string == "":
             return QValidator.State.Intermediate, string, pos
 
-        if self._re.fullmatch(string):
-            return QValidator.State.Acceptable, string, pos
+        if not self._re.fullmatch(string):
+            return QValidator.State.Invalid, string, pos
 
-        return QValidator.State.Invalid, string, pos
+        for i in string.strip(", ").split(","):
+            try:
+                val = int(i)
+            except Exception:
+                return QValidator.State.Intermediate, string, pos
+
+        return QValidator.State.Acceptable, string, pos
