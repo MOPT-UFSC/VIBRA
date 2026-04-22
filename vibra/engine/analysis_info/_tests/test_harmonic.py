a2c79613aa5d338ac860cb3d223ff1af76923ce4
André Fernandes <fpf.andre@gmail.com>
2026-04-22 20:14:22 -0300

    fix interval configuration in test harmonic

--- vibra/engine/analysis_info/_tests/test_harmonic.py
+++ vibra/engine/analysis_info/_tests/test_harmonic.py
@@ -1,23 +1,22 @@
 import numpy as np
-import pytest
 
 from vibra.engine.analysis_info import FrequencySpacing, HarmonicAnalysisSetup
 
 
 def test_interval_configuration():
     setup = HarmonicAnalysisSetup(
-        frequency_spacing = FrequencySpacing.EQUALLY_DISTRIBUTED,
-        f_min=10, 
-        f_max=18.2, 
+        frequency_spacing=FrequencySpacing.EQUALLY_DISTRIBUTED,
+        f_min=10,
+        f_max=18.2,
         f_step=1.6,
-        )
+    )
     assert setup.f_min == 10
     assert setup.f_max == 18.2
     assert setup.f_step == 1.6
-    assert setup.f_size == len(setup.frequencies()) == 6
+    assert setup.f_size == len(setup.get_frequencies()) == 6
 
     expected_frequencies = [10.0, 11.6, 13.2, 14.8, 16.4, 18.0]
-    assert np.allclose(expected_frequencies, setup.frequencies())
+    assert np.allclose(expected_frequencies, setup.get_frequencies())
 
     assert 18 in setup
     assert 0 not in setup
@@ -29,13 +28,13 @@ def test_interval_configuration():
 def test_unmasked_frequency_configuration():
     expected_frequencies = [9, 18, 126, 127]
     setup = HarmonicAnalysisSetup(
-        frequency_spacing = FrequencySpacing.USER_DEFINED,
-        frequencies = expected_frequencies,
-        )
+        frequency_spacing=FrequencySpacing.USER_DEFINED,
+        frequencies=expected_frequencies,
+    )
     assert setup.f_min == 9 == min(setup)
     assert setup.f_max == 127 == max(setup)
     assert setup.f_size == 4 == len(setup)
-    assert np.allclose(expected_frequencies, setup.frequencies())
+    assert np.allclose(expected_frequencies, setup.get_frequencies())
 
     assert 9 in setup
     assert 0 not in setup
@@ -50,14 +49,14 @@ def test_masked_frequency_configuration():
     expected_frequencies = [18, 126]
 
     setup = HarmonicAnalysisSetup(
-        frequency_spacing = FrequencySpacing.USER_DEFINED,
-        frequencies = frequencies,
-        solution_steps_mask = mask,
-        )
+        frequency_spacing=FrequencySpacing.USER_DEFINED,
+        frequencies=frequencies,
+        solution_steps_mask=mask,
+    )
     assert setup.f_min == 18 == min(setup)
     assert setup.f_max == 126 == max(setup)
     assert setup.f_size == 2 == len(setup)
-    assert np.allclose(expected_frequencies, setup.frequencies())
+    assert np.allclose(expected_frequencies, setup.get_frequencies())
 
     assert 18 in setup
     assert 9 not in setup
