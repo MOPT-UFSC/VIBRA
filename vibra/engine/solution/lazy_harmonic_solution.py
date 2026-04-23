41e8c6241f86fe9e5856e896ab33ea2aeb3aa83b
André Fernandes <fpf.andre@gmail.com>
2026-04-23 15:48:54 -0300

    Implement a solution class based on LazyArrays to reduce memory consumption

---
+++ vibra/engine/solution/lazy_harmonic_solution.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+import h5py
+
+from vibra.engine import AnalysisID
+from vibra.engine.serialization.project_paths import ProjectPaths
+from vibra.engine.serialization.project_reader import ProjectReader
+from vibra.utils.lazy_array import LazyArray
+
+from .harmonic_solution import HarmonicSolution
+
+
+class LazyHarmonicSolution(HarmonicSolution):
+    def __init__(self, project_paths: ProjectPaths):
+        self.project_paths = project_paths
+
+        hs = self.project_paths.harmonic_solution_filepath
+        if not hs.exists():
+            msg = "LazyHarmonicSolution can not be created without a harmonic solution file"
+            raise FileExistsError(msg)
+
+        self.frequencies = LazyArray(hs, "frequencies")
+        self.nodal_solution = LazyArray(hs, "solution")
+        self.status = LazyArray(hs, "solution_status")
+        self.displacement_dof = LazyArray(hs, "displacement_dof")
+
+    @property
+    def analysis_id(self) -> AnalysisID:
+        reader = ProjectReader(self.project_paths)
+        return reader.read_current_analysis_id()
+
+    @property
+    def displacement_dof(self) -> Optional[LazyArray]:
+        hs = self.project_paths.harmonic_solution_filepath
+
+        with h5py.File(hs, "r") as f:
+            if "displacement_dof" not in f:
+                return None
+
+        return LazyArray(hs, "displacement_dof")
