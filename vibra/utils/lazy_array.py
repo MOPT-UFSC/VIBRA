c387512663178efd3b93eece84d590a8fcc61e49
André Fernandes <fpf.andre@gmail.com>
2026-04-23 15:43:48 -0300

    Creates a LazyArray that works just like an array, but retrieves information from a hdf5 file when needed

---
+++ vibra/utils/lazy_array.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import h5py
+import numpy as np
+
+if TYPE_CHECKING:
+    LazyArrayTypeHints = np.ndarray
+else:
+    LazyArrayTypeHints = object
+
+
+class LazyArray(LazyArrayTypeHints):
+    """
+    This class is meant to access the content of an HDF5 array
+    without storing everyting in memory at once, while behaving
+    just like a np.ndarray.
+
+    Although the h5py.File already implements the lazy approach, it
+    does not have all the methods and attributes to be used seamlessly
+    as a numpy array.
+
+    This approach converts the data to np.ndarray if necessary to allow
+    full compatibility.
+    """
+
+    def __init__(self, file_path: Path, internal_name: str):
+        self.file_path = file_path
+        self.internal_name = internal_name
+
+    def __getattr__(self, attribute: str):
+        with h5py.File(self.file_path, "r") as f:
+            dataset = f[self.internal_name]
+
+            if hasattr(dataset, attribute):
+                return getattr(dataset, attribute)
+            else:
+                array = np.array(dataset)
+                return getattr(array, attribute)
+
+    def __getitem__(self, *args, **kwargs):
+        with h5py.File(self.file_path, "r") as f:
+            dataset = f[self.internal_name]
+            return dataset.__getitem__(*args, **kwargs)
