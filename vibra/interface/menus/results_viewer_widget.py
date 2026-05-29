fbe1be95c1017028f2b93d1e32ce61277744df16
JacsonGilVargas <jgvargasemc@gmail.com>
2026-05-29 00:43:53 -0300

    Refactor the ResultsViewerWidget class

--- vibra/interface/menus/results_viewer_widget.py
+++ vibra/interface/menus/results_viewer_widget.py
@@ -1,12 +1,13 @@
-from PySide6.QtWidgets import QFrame, QWidget
+from PySide6.QtWidgets import QFrame, QGridLayout, QWidget
 
 from vibra import app
-from vibra.interface.ui_generated.menu.left_menu_widget_ui import LeftMenuWidget_UI
 from vibra.interface.menus.results_viewer_items import ResultsViewerItems
 from vibra.interface.plots.acoustic.acoustic_mode_shape_inputs import AcousticModeShapeInputs
 from vibra.interface.plots.acoustic.acoustic_pressure_field_inputs import AcousticPressureFieldInputs
+from vibra.interface.plots.general.animation_widget import AnimationWidget
 from vibra.interface.plots.structural.displacement_field_inputs import PlotDisplacementFieldInputs
 from vibra.interface.plots.structural.structural_mode_shape_inputs import PlotStructuralModeShapeInputs
+from vibra.interface.ui_generated.menu.left_menu_widget_ui import LeftMenuWidget_UI
 
 
 class ResultsViewerWidget(LeftMenuWidget_UI):
@@ -45,10 +46,14 @@ class ResultsViewerWidget(LeftMenuWidget_UI):
     def _define_qt_variables(self):
         self.main_frame = QFrame()
         self.results_viewer_items = ResultsViewerItems()
-        
-        self.layout().replaceWidget(self.top_widget, self.results_viewer_items)
-        self.adjustSize()
-    
+
+        self.grid_layout = QGridLayout()
+        self.grid_layout.setContentsMargins(0, 0, 0, 0)
+        self.top_widget.setLayout(self.grid_layout)
+
+        self.grid_layout.addWidget(self.results_viewer_items)
+        self.top_widget.adjustSize()
+
     def _create_connections(self):
 
         # Structural
@@ -76,34 +81,38 @@ class ResultsViewerWidget(LeftMenuWidget_UI):
         self.results_viewer_items._update_items()
         self.results_viewer_items.update_tree_visibility_after_solution()
 
-    def get_animation_widget(self):
+    def get_animation_widget(self) -> AnimationWidget | None:
         if self.current_widget_is_animatable():
             return self.current_widget.animation_widget
-        
+
         return None
 
     def add_structural_modal_widget(self):
+        self.top_widget.setFixedHeight(120)
+        self.current_widget = self.plot_structural_modal
         self.plot_structural_modal.load_natural_frequencies()
         self.plot_structural_modal.load_user_preference_colormap()
-        self.current_widget = self.plot_structural_modal
         self.add_widget(self.plot_structural_modal)
 
     def add_structural_harmonic_widget(self):
+        self.top_widget.setFixedHeight(120)
+        self.current_widget = self.plot_structural_harmonic
         self.plot_structural_harmonic.load_frequencies()
         self.plot_structural_harmonic.load_user_preference_colormap()
-        self.current_widget = self.plot_structural_harmonic
         self.add_widget(self.plot_structural_harmonic)
 
     def add_acoustic_modal_widget(self):
+        self.top_widget.setFixedHeight(220)
+        self.current_widget = self.plot_acoustic_modal
         self.plot_acoustic_modal.load_natural_frequencies()
         self.plot_acoustic_modal.load_user_preference_colormap()
-        self.current_widget = self.plot_acoustic_modal
         self.add_widget(self.plot_acoustic_modal)
 
     def add_acoustic_harmonic_widget(self):
+        self.top_widget.setFixedHeight(220)
+        self.current_widget = self.plot_acoustic_harmonic
         self.plot_acoustic_harmonic.load_frequencies()
         self.plot_acoustic_harmonic.load_user_preference_colormap()
-        self.current_widget = self.plot_acoustic_harmonic
         self.add_widget(self.plot_acoustic_harmonic)
 
     def add_structural_frequency_response_widget(self):
