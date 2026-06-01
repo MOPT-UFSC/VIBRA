aae8bbc8c7a32f0f518b4c1396f16e7fe5bb97dc
André Fernandes <fpf.andre@gmail.com>
2026-06-01 17:55:14 -0300

    Remove unnecessary QSignal from application

--- vibra/interface/application.py
+++ vibra/interface/application.py
@@ -1,4 +1,3 @@
-from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QApplication
 
 from vibra import TEMP_PROJECT_DIR
@@ -6,8 +5,6 @@ from vibra.interface.splash_screen import SplashScreen
 
 
 class Application(QApplication):
-    selection_changed = Signal()
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
