b6059d903eb4a3cca831b7d5358488ccda3565cb
vini-ribeiro <vini.ribeiro2003@gmail.com>
2026-06-04 19:43:58 -0300

    add gmsh multithread

--- vibra/engine/mesher/mesh.py
+++ vibra/engine/mesher/mesh.py
@@ -210,11 +210,11 @@ class Mesh:
             gmsh.option.set_number("General.Verbosity", 0)
             gmsh.option.set_number("Geometry.Tolerance", mesh_setup.geometry_tolerance)
 
-            # n_thread = 14
-            # gmsh.option.set_number("General.NumThreads", n_thread)
-            # gmsh.option.set_number("Mesh.MaxNumThreads1D", n_thread)
-            # gmsh.option.set_number("Mesh.MaxNumThreads2D", n_thread)
-            # gmsh.option.set_number("Mesh.MaxNumThreads3D", n_thread)
+            n_thread = 0
+            gmsh.option.set_number("General.NumThreads", n_thread)
+            gmsh.option.set_number("Mesh.MaxNumThreads1D", n_thread)
+            gmsh.option.set_number("Mesh.MaxNumThreads2D", n_thread)
+            gmsh.option.set_number("Mesh.MaxNumThreads3D", n_thread)
 
             logging.info("Loading geometry... [10/100]")
             gmsh.open(str(path))
