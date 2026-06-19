9fbb84c4d5bf94fd79e34bb52d2297cf8c68ba73
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-06-19 14:24:49 -0300

    Add save_icon method in paint_icons.py script

--- scripts/paint_icons.py
+++ scripts/paint_icons.py
@@ -29,24 +29,33 @@ def get_icons_to_paint() -> list[Path]:
     
     return icons_to_paint
 
+def save_icon(icon_path: Path, icon_image: Image, theme: str) -> None:
+    parts = icon_path.parts
+    index = parts.index("icons") + 1
+    new_path = Path(*parts[:index], theme, *parts[index:])
+
+    new_path.parent.mkdir(parents=True, exist_ok=True)
+    icon_image.save(new_path)
+
 def paint_icons():
     icons_to_paint = get_icons_to_paint()
+    colors_map = {
+        "dark": DARK_ICON_COLOR,
+        "light": LIGHT_ICON_COLOR
+    }
 
     for icon in icons_to_paint:
-
         img = Image.open(icon).convert("RGBA")
         img_data = np.array(img)
-        
-        for i in range(len(img_data)):
-            for j in range(len(img_data[i])):
-                rgba_color = img_data[i][j]
 
-                if rgba_color[-1] == 0:
-                    continue
+        mask = img_data[:,:, 3] != 0
 
-                img_data[i][j] = DARK_ICON_COLOR.to_rgba()
+        for theme, color in colors_map.items():
+            img_data[mask] = color.to_rgba()
 
-        new_img = Image.fromarray(img_data)
+            painted_icon = Image.fromarray(img_data)
+            save_icon(icon, painted_icon, theme)
+
 
 if __name__ == "__main__":
     paint_icons()
