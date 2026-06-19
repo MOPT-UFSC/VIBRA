4655cbdde3e2c8187b168cd7d27acff31b00bb05
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-06-19 15:04:09 -0300

    Refactors paint_icons script to work correctly with the dark and light folders

--- scripts/paint_icons.py
+++ scripts/paint_icons.py
@@ -9,54 +9,49 @@ from vibra import DARK_ICON_COLOR, DARK_ICONS_DIR, LIGHT_ICON_COLOR, LIGHT_ICONS
 DIR_EXCEPTIONS = ["cursors", "figures", "logos", "model_setup_items", "__pycache__", "warnings"]
 ICONS_EXTENSIONS = [".png"]
 ICONS_DIRS = [DARK_ICONS_DIR, LIGHT_ICONS_DIR]
+COLORS = [DARK_ICON_COLOR, LIGHT_ICON_COLOR]
 
 
-def get_icons_to_paint() -> list[Path]:
+def get_icons_to_paint(dir: Path) -> list[Path]:
     icons_to_paint = list()
 
-    for dir in ICONS_DIRS:
-        for dir_path, _, files in os.walk(os.path.abspath(dir)):
-            last_folder = Path(dir_path).name
-        
-            if last_folder in DIR_EXCEPTIONS:
+    for dir_path, _, files in os.walk(os.path.abspath(dir)):
+        last_folder = Path(dir_path).name
+
+        if last_folder in DIR_EXCEPTIONS:
+            continue
+
+        for file in files:
+            icon_path = Path(os.path.join(dir_path, file))
+
+            if icon_path.suffix.lower() not in ICONS_EXTENSIONS:
                 continue
 
-            for file in files:
-                icon_path = Path(os.path.join(dir_path, file))
+            icons_to_paint.append(icon_path)
 
-                if icon_path.suffix.lower() not in ICONS_EXTENSIONS:
-                    continue
-                
-                icons_to_paint.append(icon_path)
-        
     return icons_to_paint
 
-def save_icon(icon_path: Path, icon_image: Image, theme: str) -> None:
-    parts = icon_path.parts
-    index = parts.index("icons") + 1
-    new_path = Path(*parts[:index], theme, *parts[index:])
 
-    new_path.parent.mkdir(parents=True, exist_ok=True)
-    icon_image.save(new_path)
+def save_icon(icon_path: Path, icon_image: Image) -> None:
+    icon_path.parent.mkdir(parents=True, exist_ok=True)
+    icon_image.save(icon_path)
+
 
 def paint_icons():
-    icons_to_paint = get_icons_to_paint()
-    colors_map = {
-        "dark_theme": DARK_ICON_COLOR,
-        "light_theme": LIGHT_ICON_COLOR
-    }
+    for dir, color in zip(ICONS_DIRS, COLORS):
+        print(dir, color)
 
-    for icon in icons_to_paint:
-        img = Image.open(icon).convert("RGBA")
-        img_data = np.array(img)
+        icons_to_paint = get_icons_to_paint(dir)
 
-        mask = img_data[:,:, 3] != 0
+        for icon in icons_to_paint:
+            img = Image.open(icon).convert("RGBA")
+            img_data = np.array(img)
 
-        for theme, color in colors_map.items():
+            mask = img_data[:, :, 3] != 0
             img_data[mask] = color.to_rgba()
 
             painted_icon = Image.fromarray(img_data)
-            save_icon(icon, painted_icon, theme)
+            save_icon(icon, painted_icon)
 
 
 if __name__ == "__main__":
