5136910fff3cd8d759fd8ce5fba9e19950250b19
André Fernandes <fpf.andre@gmail.com>
2026-06-22 18:47:47 -0300

    Add useful volume methods

--- vibra/interface/general/entity_visibility_handler.py
+++ vibra/interface/general/entity_visibility_handler.py
@@ -22,7 +22,7 @@ class EntityVisibilityHandler(QObject):
         _visible_surfaces = set()
         for volume, surfaces in mesh.surfaces_from_volume.items():
             if volume not in self._volumes_to_hide:
-                _visible_surfaces |= surfaces
+                _visible_surfaces |= set(surfaces)
 
         _visible_surfaces -= self._surfaces_to_hide
         return _visible_surfaces
@@ -34,18 +34,32 @@ class EntityVisibilityHandler(QObject):
 
         all_surfaces = mesh.all_surface_ids()
         return all_surfaces - self.get_visible_surfaces()
+    
+    def get_hidden_volumes(self) -> set[int]:
+        return set(self._volumes_to_hide)
+    
+    def get_visible_volumes(self) -> set[int]:
+        mesh = self.project.mesh
+        if mesh is None:
+            return set()
+
+        all_volumes = mesh.all_solid_ids()
+        return all_volumes - self.get_hidden_volumes()
 
     def hide_surfaces(self, surfaces: Sequence[int]):
         self._surfaces_to_hide |= set(surfaces)
-        if surfaces:
-            self.changed.emit()
+        # if surfaces:
+        #     self.changed.emit()
 
     def hide_volumes(self, volumes: Sequence[int]):
         self._volumes_to_hide |= set(volumes)
-        if volumes:
-            self.changed.emit()
+        # if volumes:
+        #     self.changed.emit()
+
+    def has_hidden_entity(self) -> bool:
+        return bool(self._surfaces_to_hide) or bool(self._volumes_to_hide)
 
     def unhide_all(self):
         self._surfaces_to_hide.clear()
         self._volumes_to_hide.clear()
-        self.changed.emit()
+        # self.changed.emit()
