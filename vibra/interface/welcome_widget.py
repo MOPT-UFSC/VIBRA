49b669c500f587e5e6c85dab83baaa2c8a46e0c8
GuilhermeSPierri <guilhermepierri8@gmail.com>
2026-06-30 15:38:15 -0300

    Update MainWindow and WelcomeWidget dynamic swapping with new logos

--- vibra/interface/welcome_widget.py
+++ vibra/interface/welcome_widget.py
@@ -27,16 +27,22 @@ class WelcomeWidget(QWidget):
         self.setup_example_projects(self.widget_layout)
 
     def setup_image(self, layout):
-        image_label = QLabel(self)
-        image_label.setAlignment(Qt.AlignCenter)
-        pixmap = QPixmap(str(LOGO_DIR / "vibra_colored_dark_background.png")).scaled(
-            500, 210, 
-            Qt.KeepAspectRatio, 
-            Qt.SmoothTransformation
+        self.logo_label = QLabel(self)
+        self.logo_label.setAlignment(Qt.AlignCenter)
+        if app().config.user_preferences.interface_theme == "light":
+            logo = "vibra_colored_light_background.png"
+        else:
+            logo = "vibra_colored_dark_background.png"
+
+        pixmap = QPixmap(str(LOGO_DIR / logo)).scaled(
+            500,
+            210,
+            Qt.KeepAspectRatio,
+            Qt.SmoothTransformation,
         )
-        image_label.setPixmap(pixmap)
-        image_label.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(image_label)
+        self.logo_label.setPixmap(pixmap)
+        self.logo_label.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.logo_label)
 
         layout.addStretch()
 
@@ -55,6 +61,17 @@ class WelcomeWidget(QWidget):
 
         layout.addLayout(labels_layout)
         layout.addStretch()
+
+    def update_logo(self, path: str):
+        logo_path = LOGO_DIR / path
+
+        pixmap = QPixmap(str(logo_path)).scaled(
+            500, 210,
+            Qt.KeepAspectRatio,
+            Qt.SmoothTransformation
+        )
+
+        self.logo_label.setPixmap(pixmap)
 
     def update_recent_projects(self):
         if not self.recents_layout.isEmpty():
