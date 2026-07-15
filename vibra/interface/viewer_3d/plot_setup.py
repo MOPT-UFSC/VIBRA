5f12dd2636b2e114bd5ae98be4dc199afa4d645c
André Fernandes <fpf.andre@gmail.com>
2026-07-15 12:52:40 -0300

    create PlotSetup enumerator

---
+++ vibra/interface/viewer_3d/plot_setup.py
@@ -0,0 +1,37 @@
+from enum import StrEnum, auto
+
+from vibra.utils.struct_enum import StructEnum
+
+
+class PressurePlotType(StrEnum):
+    ABSOLUTE_ANIMATION = auto()
+    NON_ABSOLUTE_ANIMATION = auto()
+    ABSOLUTE_VALUES = auto()
+    REAL_VALUES = auto()
+    IMAG_VALUES = auto()
+
+
+class DisplacementPlotType(StrEnum):
+    U_SUM = auto()
+    U_X = auto()
+    U_Y = auto()
+    U_Z = auto()
+
+
+# This is not just nestled classes, it is a StructEnum
+class PlotSetup(StructEnum):
+    class NoPlotSetup: ...
+
+    class FrequencyDisplacementPlotSetup:
+        phase: float
+        index: int
+        plot_type: PressurePlotType
+
+    class FrequencyPressurePlotSetup:
+        phase: float
+        index: int
+        magnification_factor: float
+        plot_type: DisplacementPlotType
+
+    class TransientPlot:
+        frame: int
