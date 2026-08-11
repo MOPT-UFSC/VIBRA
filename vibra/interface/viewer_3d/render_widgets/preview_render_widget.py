40c82d850fd4000a0e94993f950fedcd5f4f21df
André Fernandes <fpf.andre@gmail.com>
2026-08-11 15:54:44 -0300

    Add example selection method

--- vibra/interface/viewer_3d/render_widgets/preview_render_widget.py
+++ vibra/interface/viewer_3d/render_widgets/preview_render_widget.py
@@ -1,4 +1,8 @@
+from molde.colors.color import Color
 from molde.render_widgets import CommonRenderWidget
+from vtkmodules.util.numpy_support import vtk_to_numpy
+from vtkmodules.vtkCommonDataModel import vtkSelectionNode
+from vtkmodules.vtkRenderingCore import vtkHardwarePicker, vtkHardwareSelector
 
 from vibra.engine.model import Model
 from vibra.interface.viewer_3d.actors.mesh_actor import MeshActor
@@ -10,6 +14,11 @@ class PreviewRenderWidget(CommonRenderWidget):
     def __init__(self):
         super().__init__()
         self.create_axes()
+
+        self.picker = vtkHardwarePicker()
+        self.picker.SnapToMeshPointOff()
+
+        self.left_released.connect(self.click)
 
         self.model = None
         self.section_plane = None
@@ -38,3 +47,28 @@ class PreviewRenderWidget(CommonRenderWidget):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         self.renderer.ResetCamera()
+
+    @function_timer
+    def click(self, x, y):
+        self.picker.Pick(x, y, 0, self.renderer)
+
+        actor = self.picker.GetActor()
+        cell_id = self.picker.GetCellId()
+
+        if cell_id < 0:
+            print("no cell")
+            return
+
+        match actor:
+            case self.mesh_actor.surface_actor:
+                tag = self.mesh_actor.mesh.faces_connectivity[cell_id][1]
+                self.mesh_actor.set_color(Color(0, 0, 0))
+                self.mesh_actor.paint_surfaces(color=Color(255, 0, 0), surfaces=[tag])
+
+            case self.mesh_actor.section_actor:
+                print("solid")
+
+            case _:
+                return
+
+        self.update()
