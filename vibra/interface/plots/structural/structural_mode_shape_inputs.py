4a9d24d52e8f91cf3dd31b49bb3e4d46bde4c9bc
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-08-12 19:01:38 -0300

    Updates structural analysis inputs to use the results display widget

--- vibra/interface/plots/structural/structural_mode_shape_inputs.py
+++ vibra/interface/plots/structural/structural_mode_shape_inputs.py
@@ -8,8 +8,8 @@ from vibra.engine.solution import ModalSolution
 from vibra.interface.common.common_interface import export_modal_analysis_results
 from vibra.interface.loading_window import LoadingWindow
 from vibra.interface.plots.general.animation_widget import AnimationWidget
+from vibra.interface.plots.general.results_display_widget import ResultsDisplayWidget
 from vibra.interface.ui_generated.plots.structural.structural_mode_shape_inputs_ui import StructuralModeShapeInputs_UI
-from vibra.interface.viewer_3d.coloring.color_palettes import COLORMAP_NAMES
 from vibra.interface.viewer_3d.plot_setup import DisplacementPlotType, FrequencyDisplacementPlotSetup
 
 
@@ -18,8 +18,9 @@ class PlotStructuralModeShapeInputs(StructuralModeShapeInputs_UI):
         super().__init__(*args, **kwargs)
 
         self._initialize()
-        self._create_connections()
         self.add_animation_widget()
+        self.add_color_widget()
+        self._create_connections()
 
     def showEvent(self, event):
         super().showEvent(event)
@@ -34,17 +35,15 @@ class PlotStructuralModeShapeInputs(StructuralModeShapeInputs_UI):
 
     def _create_connections(self):
         #
-        self.comboBox_colormaps.currentIndexChanged.connect(self.update_colormap_type)
         self.comboBox_plot_type.currentIndexChanged.connect(self.update_plot)
         #
         self.pushButton_export_results.clicked.connect(self.export_results_callback)
-        #
-        self.slider_transparency.valueChanged.connect(self.update_transparency_callback)
-        #
+        #        
         self.treeWidget_frequencies.itemClicked.connect(self.on_click_item)
         self.treeWidget_frequencies.itemDoubleClicked.connect(self.on_click_item)
         #
-        self.load_user_preference_colormap()
+        self.results_display_widget.colormap_changed.connect(self.animation_widget.update_color_and_deformation)
+        self.results_display_widget.pressure_value_changed.connect(self.animation_widget.update_color_and_deformation)
 
     def add_animation_widget(self):
         self.grid_layout = QGridLayout()
@@ -54,6 +53,15 @@ class PlotStructuralModeShapeInputs(StructuralModeShapeInputs_UI):
         self.animation_widget = AnimationWidget()
         self.grid_layout.addWidget(self.animation_widget)
         self.frame_animation.adjustSize()
+
+    def add_color_widget(self):
+        grid_layout = QGridLayout()
+        grid_layout.setContentsMargins(0, 0, 0, 0)
+        self.frame_color.setLayout(grid_layout)
+
+        self.results_display_widget = ResultsDisplayWidget()
+        grid_layout.addWidget(self.results_display_widget)
+        self.frame_color.adjustSize()
 
     def _configure_qt_variables(self):
         self.lineEdit_natural_frequency.setDisabled(True)
@@ -88,29 +96,6 @@ class PlotStructuralModeShapeInputs(StructuralModeShapeInputs_UI):
         else:
             self.animation_widget.setDisabled(False)
 
-    def load_user_preference_colormap(self):
-        try:
-            colormap = app().config.user_preferences.color_map
-            if colormap in COLORMAP_NAMES:
-                index = COLORMAP_NAMES.index(colormap)
-                self.comboBox_colormaps.setCurrentIndex(index)
-        except Exception:
-            self.comboBox_colormaps.setCurrentIndex(0)
-
-    def update_colormap_type(self):
-        app().config.user_preferences.color_map = self.get_colormap()
-        app().config.update_config_file()
-        try:
-            self.animation_widget.update_color_and_deformation()
-        except AttributeError:
-            pass
-
-    def get_colormap(self) -> str:
-        index = self.comboBox_colormaps.currentIndex()
-        if not (0 <= index < len(COLORMAP_NAMES)):
-            return "jet"
-        return COLORMAP_NAMES[index]
-
     def export_results_callback(self):
         export_modal_analysis_results(self, self.modes_to_frequencies, "structural")
 
@@ -135,10 +120,6 @@ class PlotStructuralModeShapeInputs(StructuralModeShapeInputs_UI):
 
     def update_displacements(self):
         pass
-
-    def update_transparency_callback(self):
-        transparency = self.slider_transparency.value() / 100
-        app().main_window.results_widget.set_analysis_actors_transparency(transparency)
 
     def get_plot_type(self) -> DisplacementPlotType:
         plot_types = [
