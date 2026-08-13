4ecd41206cf91c0e012503a298bff37f3744af33
JacsonGilVargas <jgvargasemc@gmail.com>
2026-08-13 17:51:26 -0300

    Recompile the ui files

--- vibra/interface/ui_generated/plots/general/results_display_widget_ui.py
+++ vibra/interface/ui_generated/plots/general/results_display_widget_ui.py
@@ -23,162 +23,78 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(400, 242)
+        Form.resize(368, 158)
         self.gridLayout = QGridLayout(Form)
+        self.gridLayout.setSpacing(4)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.frame_2 = QFrame(Form)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setMinimumSize(QSize(0, 40))
-        self.frame_2.setMaximumSize(QSize(16777215, 40))
-        self.frame_2.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
-        self.gridLayout_3 = QGridLayout(self.frame_2)
-        self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_3.addItem(self.horizontalSpacer_5, 0, 0, 1, 1)
-
-        self.lineEdit_min_color_value = QLineEdit(self.frame_2)
-        self.lineEdit_min_color_value.setObjectName(u"lineEdit_min_color_value")
-        self.lineEdit_min_color_value.setEnabled(False)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineEdit_min_color_value.sizePolicy().hasHeightForWidth())
-        self.lineEdit_min_color_value.setSizePolicy(sizePolicy)
-        self.lineEdit_min_color_value.setMinimumSize(QSize(145, 0))
-        self.lineEdit_min_color_value.setMaximumSize(QSize(200, 16777215))
-        font = QFont()
-        font.setPointSize(10)
-        self.lineEdit_min_color_value.setFont(font)
-
-        self.gridLayout_3.addWidget(self.lineEdit_min_color_value, 0, 2, 1, 1)
-
-        self.min_color_check_box = QCheckBox(self.frame_2)
-        self.min_color_check_box.setObjectName(u"min_color_check_box")
-
-        self.gridLayout_3.addWidget(self.min_color_check_box, 0, 3, 1, 1)
-
-        self.label_2 = QLabel(self.frame_2)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setMinimumSize(QSize(90, 26))
-        self.label_2.setMaximumSize(QSize(90, 26))
-        self.label_2.setFont(font)
-        self.label_2.setLineWidth(0)
-        self.label_2.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
-
-        self.gridLayout_3.addWidget(self.label_2, 0, 1, 1, 1)
-
-        self.horizontalSpacer_10 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_3.addItem(self.horizontalSpacer_10, 0, 4, 1, 1)
-
-
-        self.gridLayout.addWidget(self.frame_2, 3, 0, 1, 1)
-
-        self.frame = QFrame(Form)
-        self.frame.setObjectName(u"frame")
-        self.frame.setMinimumSize(QSize(0, 40))
-        self.frame.setMaximumSize(QSize(16777215, 40))
-        self.frame.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame.setFrameShadow(QFrame.Shadow.Raised)
-        self.gridLayout_2 = QGridLayout(self.frame)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_2.addItem(self.horizontalSpacer_4, 0, 4, 1, 1)
-
-        self.max_color_check_box = QCheckBox(self.frame)
-        self.max_color_check_box.setObjectName(u"max_color_check_box")
-
-        self.gridLayout_2.addWidget(self.max_color_check_box, 0, 3, 1, 1)
-
-        self.lineEdit_max_color_value = QLineEdit(self.frame)
-        self.lineEdit_max_color_value.setObjectName(u"lineEdit_max_color_value")
-        self.lineEdit_max_color_value.setEnabled(False)
-        sizePolicy.setHeightForWidth(self.lineEdit_max_color_value.sizePolicy().hasHeightForWidth())
-        self.lineEdit_max_color_value.setSizePolicy(sizePolicy)
-        self.lineEdit_max_color_value.setMinimumSize(QSize(145, 0))
-        self.lineEdit_max_color_value.setMaximumSize(QSize(200, 16777215))
-        self.lineEdit_max_color_value.setFont(font)
-
-        self.gridLayout_2.addWidget(self.lineEdit_max_color_value, 0, 2, 1, 1)
-
-        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_2.addItem(self.horizontalSpacer_3, 0, 0, 1, 1)
-
-        self.label = QLabel(self.frame)
-        self.label.setObjectName(u"label")
-        self.label.setMinimumSize(QSize(90, 26))
-        self.label.setMaximumSize(QSize(90, 26))
-        self.label.setFont(font)
-        self.label.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
-
-        self.gridLayout_2.addWidget(self.label, 0, 1, 1, 1)
-
-
-        self.gridLayout.addWidget(self.frame, 2, 0, 1, 1)
-
-        self.frame_transparency = QFrame(Form)
-        self.frame_transparency.setObjectName(u"frame_transparency")
-        self.frame_transparency.setMinimumSize(QSize(0, 40))
-        self.frame_transparency.setMaximumSize(QSize(16777215, 40))
-        self.frame_transparency.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_transparency.setFrameShadow(QFrame.Shadow.Raised)
-        self.gridLayout_10 = QGridLayout(self.frame_transparency)
-        self.gridLayout_10.setObjectName(u"gridLayout_10")
-        self.gridLayout_10.setContentsMargins(0, 0, 0, 0)
-        self.label_4 = QLabel(self.frame_transparency)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setMinimumSize(QSize(90, 26))
-        self.label_4.setMaximumSize(QSize(90, 26))
-        self.label_4.setFont(font)
-        self.label_4.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
-
-        self.gridLayout_10.addWidget(self.label_4, 0, 1, 1, 1)
-
-        self.slider_transparency = QSlider(self.frame_transparency)
-        self.slider_transparency.setObjectName(u"slider_transparency")
-        self.slider_transparency.setMinimumSize(QSize(176, 0))
-        self.slider_transparency.setMaximumSize(QSize(200, 16777215))
-        self.slider_transparency.setOrientation(Qt.Orientation.Horizontal)
-
-        self.gridLayout_10.addWidget(self.slider_transparency, 0, 2, 1, 1)
-
-        self.horizontalSpacer_8 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_10.addItem(self.horizontalSpacer_8, 0, 3, 1, 1)
-
-        self.horizontalSpacer_9 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_10.addItem(self.horizontalSpacer_9, 0, 0, 1, 1)
-
-
-        self.gridLayout.addWidget(self.frame_transparency, 1, 0, 1, 1)
-
+        self.gridLayout.setContentsMargins(4, 4, 4, 4)
         self.frame_4 = QFrame(Form)
         self.frame_4.setObjectName(u"frame_4")
         self.frame_4.setMinimumSize(QSize(0, 40))
-        self.frame_4.setMaximumSize(QSize(16777215, 40))
+        self.frame_4.setMaximumSize(QSize(16777215, 16777215))
         self.frame_4.setFrameShape(QFrame.Shape.NoFrame)
         self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout_15 = QGridLayout(self.frame_4)
         self.gridLayout_15.setObjectName(u"gridLayout_15")
         self.gridLayout_15.setContentsMargins(0, 0, 0, 0)
-        self.frame_8 = QFrame(self.frame_4)
-        self.frame_8.setObjectName(u"frame_8")
-        self.frame_8.setMinimumSize(QSize(176, 0))
-        self.frame_8.setMaximumSize(QSize(176, 16777215))
-        self.frame_8.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_8.setFrameShadow(QFrame.Shadow.Raised)
-        self.gridLayout_16 = QGridLayout(self.frame_8)
-        self.gridLayout_16.setSpacing(0)
-        self.gridLayout_16.setObjectName(u"gridLayout_16")
-        self.gridLayout_16.setContentsMargins(0, 0, 0, 0)
-        self.comboBox_colormaps = QComboBox(self.frame_8)
+        self.min_color_check_box = QCheckBox(self.frame_4)
+        self.min_color_check_box.setObjectName(u"min_color_check_box")
+
+        self.gridLayout_15.addWidget(self.min_color_check_box, 3, 4, 1, 1)
+
+        self.label_2 = QLabel(self.frame_4)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setMinimumSize(QSize(120, 26))
+        self.label_2.setMaximumSize(QSize(120, 26))
+        font = QFont()
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setLineWidth(0)
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayout_15.addWidget(self.label_2, 3, 1, 1, 1)
+
+        self.horizontalSpacer_14 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout_15.addItem(self.horizontalSpacer_14, 2, 5, 1, 1)
+
+        self.label_4 = QLabel(self.frame_4)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMinimumSize(QSize(120, 26))
+        self.label_4.setMaximumSize(QSize(120, 26))
+        self.label_4.setFont(font)
+        self.label_4.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayout_15.addWidget(self.label_4, 1, 1, 1, 1)
+
+        self.horizontalSpacer_15 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout_15.addItem(self.horizontalSpacer_15, 2, 0, 1, 1)
+
+        self.label = QLabel(self.frame_4)
+        self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(120, 26))
+        self.label.setMaximumSize(QSize(120, 26))
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayout_15.addWidget(self.label, 2, 1, 1, 1)
+
+        self.label_3 = QLabel(self.frame_4)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMinimumSize(QSize(120, 26))
+        self.label_3.setMaximumSize(QSize(120, 26))
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayout_15.addWidget(self.label_3, 0, 1, 1, 1)
+
+        self.max_color_check_box = QCheckBox(self.frame_4)
+        self.max_color_check_box.setObjectName(u"max_color_check_box")
+
+        self.gridLayout_15.addWidget(self.max_color_check_box, 2, 4, 1, 1)
+
+        self.comboBox_colormaps = QComboBox(self.frame_4)
         self.comboBox_colormaps.addItem("")
         self.comboBox_colormaps.addItem("")
         self.comboBox_colormaps.addItem("")
@@ -191,31 +107,44 @@ class Ui_Form(object):
         self.comboBox_colormaps.addItem("")
         self.comboBox_colormaps.addItem("")
         self.comboBox_colormaps.setObjectName(u"comboBox_colormaps")
-        self.comboBox_colormaps.setMinimumSize(QSize(120, 26))
-        self.comboBox_colormaps.setMaximumSize(QSize(200, 26))
+        self.comboBox_colormaps.setMinimumSize(QSize(160, 26))
+        self.comboBox_colormaps.setMaximumSize(QSize(160, 26))
         self.comboBox_colormaps.setFont(font)
 
-        self.gridLayout_16.addWidget(self.comboBox_colormaps, 0, 0, 1, 1)
+        self.gridLayout_15.addWidget(self.comboBox_colormaps, 0, 2, 1, 1)
 
+        self.slider_transparency = QSlider(self.frame_4)
+        self.slider_transparency.setObjectName(u"slider_transparency")
+        self.slider_transparency.setMinimumSize(QSize(160, 0))
+        self.slider_transparency.setMaximumSize(QSize(160, 16777215))
+        self.slider_transparency.setOrientation(Qt.Orientation.Horizontal)
 
-        self.gridLayout_15.addWidget(self.frame_8, 0, 2, 1, 1)
+        self.gridLayout_15.addWidget(self.slider_transparency, 1, 2, 1, 1)
 
-        self.horizontalSpacer_14 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.lineEdit_max_color_value = QLineEdit(self.frame_4)
+        self.lineEdit_max_color_value.setObjectName(u"lineEdit_max_color_value")
+        self.lineEdit_max_color_value.setEnabled(False)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit_max_color_value.sizePolicy().hasHeightForWidth())
+        self.lineEdit_max_color_value.setSizePolicy(sizePolicy)
+        self.lineEdit_max_color_value.setMinimumSize(QSize(160, 0))
+        self.lineEdit_max_color_value.setMaximumSize(QSize(160, 16777215))
+        self.lineEdit_max_color_value.setFont(font)
 
-        self.gridLayout_15.addItem(self.horizontalSpacer_14, 0, 3, 1, 1)
+        self.gridLayout_15.addWidget(self.lineEdit_max_color_value, 2, 2, 1, 1)
 
-        self.label_3 = QLabel(self.frame_4)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setMinimumSize(QSize(90, 26))
-        self.label_3.setMaximumSize(QSize(90, 26))
-        self.label_3.setFont(font)
-        self.label_3.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.lineEdit_min_color_value = QLineEdit(self.frame_4)
+        self.lineEdit_min_color_value.setObjectName(u"lineEdit_min_color_value")
+        self.lineEdit_min_color_value.setEnabled(False)
+        sizePolicy.setHeightForWidth(self.lineEdit_min_color_value.sizePolicy().hasHeightForWidth())
+        self.lineEdit_min_color_value.setSizePolicy(sizePolicy)
+        self.lineEdit_min_color_value.setMinimumSize(QSize(160, 0))
+        self.lineEdit_min_color_value.setMaximumSize(QSize(160, 16777215))
+        self.lineEdit_min_color_value.setFont(font)
 
-        self.gridLayout_15.addWidget(self.label_3, 0, 1, 1, 1)
-
-        self.horizontalSpacer_15 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_15.addItem(self.horizontalSpacer_15, 0, 0, 1, 1)
+        self.gridLayout_15.addWidget(self.lineEdit_min_color_value, 3, 2, 1, 1)
 
 
         self.gridLayout.addWidget(self.frame_4, 0, 0, 1, 1)
@@ -232,13 +161,14 @@ class Ui_Form(object):
 #if QT_CONFIG(tooltip)
         self.label_2.setToolTip(QCoreApplication.translate("Form", u"Minimum colorbar value", None))
 #endif // QT_CONFIG(tooltip)
-        self.label_2.setText(QCoreApplication.translate("Form", u"Min. Color:", None))
-        self.max_color_check_box.setText("")
+        self.label_2.setText(QCoreApplication.translate("Form", u"Min. scale value:", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"Transparency:", None))
 #if QT_CONFIG(tooltip)
         self.label.setToolTip(QCoreApplication.translate("Form", u"Maximum colorbar value", None))
 #endif // QT_CONFIG(tooltip)
-        self.label.setText(QCoreApplication.translate("Form", u"Max. Color:", None))
-        self.label_4.setText(QCoreApplication.translate("Form", u"Transparency:", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Max. scale value:", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"Colormaps:", None))
+        self.max_color_check_box.setText("")
         self.comboBox_colormaps.setItemText(0, QCoreApplication.translate("Form", u" Jet scale", None))
         self.comboBox_colormaps.setItemText(1, QCoreApplication.translate("Form", u" Viridis scale", None))
         self.comboBox_colormaps.setItemText(2, QCoreApplication.translate("Form", u" Inferno scale", None))
@@ -251,7 +181,6 @@ class Ui_Form(object):
         self.comboBox_colormaps.setItemText(9, QCoreApplication.translate("Form", u"PuOr diverging scale", None))
         self.comboBox_colormaps.setItemText(10, QCoreApplication.translate("Form", u" Grayscale", None))
 
-        self.label_3.setText(QCoreApplication.translate("Form", u"Colormaps:", None))
     # retranslateUi
 
 
@@ -261,26 +190,18 @@ class ResultsDisplayWidget_UI(QWidget, Ui_Form):
     Component Hierarchy:
     - Form: QWidget
         - (Layout): QGridLayout
-                - frame_2: QFrame
-                    - (Layout): QGridLayout
-                            - lineEdit_min_color_value: QLineEdit
-                            - min_color_check_box: QCheckBox
-                            - label_2: QLabel
-                - frame: QFrame
-                    - (Layout): QGridLayout
-                            - max_color_check_box: QCheckBox
-                            - lineEdit_max_color_value: QLineEdit
-                            - label: QLabel
-                - frame_transparency: QFrame
-                    - (Layout): QGridLayout
-                            - label_4: QLabel
-                            - slider_transparency: QSlider
                 - frame_4: QFrame
                     - (Layout): QGridLayout
-                            - frame_8: QFrame
-                                - (Layout): QGridLayout
-                                        - comboBox_colormaps: QComboBox
+                            - min_color_check_box: QCheckBox
+                            - label_2: QLabel
+                            - label_4: QLabel
+                            - label: QLabel
                             - label_3: QLabel
+                            - max_color_check_box: QCheckBox
+                            - comboBox_colormaps: QComboBox
+                            - slider_transparency: QSlider
+                            - lineEdit_max_color_value: QLineEdit
+                            - lineEdit_min_color_value: QLineEdit
     """
 
     def __init__(self, *args, **kwargs):
