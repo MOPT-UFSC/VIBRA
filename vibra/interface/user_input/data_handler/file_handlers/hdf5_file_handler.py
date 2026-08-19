9aa3cb489a4af21847cfe7f27b03ed68e042cc64
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-08-19 10:53:00 -0300

    Replaces the code in the hdf5_file_handler.py file

--- vibra/interface/user_input/data_handler/file_handlers/hdf5_file_handler.py
+++ vibra/interface/user_input/data_handler/file_handlers/hdf5_file_handler.py
@@ -1,66 +1,52 @@
+from vibra.interface.user_input.data_handler.file_handlers.io_handler import IOHandler
+from vibra.interface.user_input.data_handler.imported_data import SimulationData
+
 from pathlib import Path
 
 import numpy as np
-from polars import DataFrame as PolarsDataFrame
-
-from vibra.interface.user_input.data_handler.file_handlers.hdf5_file_handler import HDF5FileHandler
-from vibra.interface.user_input.data_handler.file_handlers.spreadsheet_file_handler import SpreadsheetFileHandler
-from vibra.interface.user_input.data_handler.file_handlers.text_file_handler import TextFileHandler
-from vibra.interface.user_input.data_handler.imported_data import SimulationData, SpreadsheetData, TextData
+import h5py
 
 
-class FileHandler:
+class HDF5FileHandler(IOHandler):
+
+    EXTENSIONS = [".h5", ".hdf5"]
+
     def __init__(self):
-        self._hdf5_file_handler = HDF5FileHandler()
-        self._text_file_handler = TextFileHandler()
-        self._spreadsheet_file_handler = SpreadsheetFileHandler()
+        super().__init__()
 
-    def read(self, file_path: str | Path) -> TextData | SimulationData | SpreadsheetData:
-        file_path = Path(file_path)
+    def read(self, file_path: str | Path) -> SimulationData:
+        simulation_data = SimulationData(
+            file_path.stem,
+            file_path.suffix,
+            str(file_path)
+        )
 
-        if file_path.suffix in TextFileHandler.EXTENSIONS:
-            return self._text_file_handler.read(file_path)
-        elif file_path.suffix in HDF5FileHandler.EXTENSIONS:
-            return self._hdf5_file_handler.read(file_path)
-        elif file_path.suffix in SpreadsheetFileHandler.EXTENSIONS:
-            return self._spreadsheet_file_handler.read(file_path)
-        else:
-            all_extensions = TextFileHandler.EXTENSIONS + HDF5FileHandler.EXTENSIONS + SpreadsheetFileHandler.EXTENSIONS
-            raise self.raise_extensions_error(file_path, all_extensions)
+        with h5py.File(file_path, 'r') as hf:
 
-    def save_text_file(self, file_path: str, data: np.array, delimiter=",", header=""):
-        file_path = Path(file_path)
+            # Read key data
+            nodal_data = hf.get("nodal_data")
+            variables = hf.get("variables")
+            metadata = hf.get("metadata")
 
-        if not file_path.parent.exists():
-            raise FileNotFoundError(f"The path {file_path.parent} does not exist")
+            # Save the nodal coordinates matrix
+            simulation_data.nodal_area = np.array(nodal_data.get("nodal_area"), dtype=float)
+            simulation_data.nodal_coordinates = np.array(nodal_data.get("coords"), dtype=float)
 
-        if file_path.suffix not in self._text_file_handler.EXTENSIONS:
-            raise self.raise_extensions_error(file_path, self._text_file_handler.EXTENSIONS)
+            # Save other attributes
+            for attr_key in metadata.attrs:
+                setattr(simulation_data, attr_key, metadata.attrs[attr_key])
 
-        self._text_file_handler.save(file_path, data, delimiter=delimiter, header=header)
+            # Calculate the start point from last revolution
+            delta_theta = metadata.attrs["delta_theta"]
+            steps_per_rev = int(360 / delta_theta)
+            start = steps_per_rev + 1
 
-    def save_spreadsheet_file(self, file_path: str, sheetname: str, data: PolarsDataFrame, index_rows: bool = False):
-        file_path = Path(file_path)
+            # filter the last revolution data for metadata
+            for key, values in metadata.items():
+                setattr(simulation_data, key, values[-start:])
 
-        if not file_path.parent.exists():
-            raise FileNotFoundError(f"The path {file_path.parent} does not exist")
+            # filter the last revolution data for variables
+            for key, values in variables.items():
+                setattr(simulation_data, key, values[:, -start:])
 
-        if file_path.suffix not in self._spreadsheet_file_handler.EXTENSIONS:
-            raise self.raise_extensions_error(file_path, self._spreadsheet_file_handler.EXTENSIONS)
-
-        self._spreadsheet_file_handler.save(file_path, sheetname, data, index_rows)
-
-    def generate_extensions_string_for_error_message(self, extensions: list[str]) -> str:
-        if not extensions:
-            return ""
-
-        if len(extensions) == 1:
-            return extensions[0]
-
-        return ", ".join(extensions[:-1]) + " or " + extensions[-1]
-
-    def raise_extensions_error(self, file_path: str | Path, extensions: list[str]) -> ValueError:
-        extensions_text = self.generate_extensions_string_for_error_message(extensions)
-        file_path = Path(file_path)
-
-        raise ValueError(f"Invalid suffix {file_path.suffix}. Use {extensions_text}")
+        return simulation_data
