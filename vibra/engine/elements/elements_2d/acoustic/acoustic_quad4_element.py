f1f74eae1b9f5948ebcf4ea3f1fae2ce5140dc3a
JacsonGilVargas <jgvargasemc@gmail.com>
2026-08-20 17:47:25 -0300

    Implement the 2D element integration for structural 'QUAD' elements

--- vibra/engine/elements/elements_2d/acoustic/acoustic_quad4_element.py
+++ vibra/engine/elements/elements_2d/acoustic/acoustic_quad4_element.py
@@ -1,293 +1,24 @@
 
-from vibra.engine.elements.surface_elements import Element2D
-
 from typing import TYPE_CHECKING
+
+from vibra.engine.elements.elements_2d.quad4_element import QUADRANGLE_4, get_local_coordinates
+
 if TYPE_CHECKING:
     from vibra.engine.model import Model
 
 import numpy as np
 
 
-def get_local_coordinates(coords: np.ndarray) -> np.ndarray:
-    """
-    This funtion computes the local coordinates from global coordinates.
+class ACT_QUADRANGLE_4(QUADRANGLE_4):
 
-    Parameter
-    ---------
-    coords: np.ndarray
-        An array containing the global coordinates to be converted.
-
-    Returns
-    -------
-    coord_loc: np.ndarray
-        The array of coordinates in the local coordinate system.
-    """
-    
-    X1, X2, X3, X4 = coords[:, 1]
-    Y1, Y2, Y3, Y4 = coords[:, 2]
-    Z1, Z2, Z3, Z4 = coords[:, 3]
-
-    vec_12 = np.array([X2-X1, Y2-Y1, Z2-Z1]).T
-    vec_13 = np.array([X3-X1, Y3-Y1, Z3-Z1]).T
-    vec_14 = np.array([X4-X1, Y4-Y1, Z4-Z1]).T
-
-    loc_x_axis = vec_12.copy()
-    loc_z_axis = np.cross(loc_x_axis, vec_14)
-    loc_y_axis = np.cross(loc_z_axis, loc_x_axis)
-
-    unit_x_axis = loc_x_axis / np.linalg.norm(loc_x_axis)
-    unit_y_axis = loc_y_axis / np.linalg.norm(loc_y_axis)
-
-    x1 = np.dot(vec_13, unit_x_axis)
-    x2 = np.dot(vec_14, unit_x_axis)
-    x3 = 0.
-    x4 = np.dot(vec_12, unit_x_axis)
-
-    y1 = np.dot(vec_13, unit_y_axis)
-    y2 = np.dot(vec_14, unit_y_axis)
-    y3 = 0.
-    y4 = np.dot(vec_12, unit_y_axis)
-
-    # x1 = 0.
-    # x2 = np.dot(vec_12, unit_x_axis)
-    # x3 = np.dot(vec_13, unit_x_axis)
-    # x4 = np.dot(vec_14, unit_x_axis)
-
-    # y1 = 0.
-    # y2 = np.dot(vec_12, unit_y_axis)
-    # y3 = np.dot(vec_13, unit_y_axis)
-    # y4 = np.dot(vec_14, unit_y_axis)
-
-    coord_loc = np.array([[x1, y1],
-                          [x2, y2],
-                          [x3, y3],
-                          [x4, y4]], dtype=float)
-
-    return coord_loc
-
-
-class ACT_QUADRANGLE_4(Element2D):
-
-    NODES_PER_ELEMENT = 4
-    DOF_PER_NODE = 1
-    DOF_PER_ELEMENT = NODES_PER_ELEMENT * DOF_PER_NODE
-
-    def __init__(self, model: "Model"):
+    def __init__(self, model: "Model", dof_per_node: int = 1):
+        super().__init__(model, dof_per_node)
 
         self.model = model
 
         self.connectivities = None
         self.element_label = "acoustic_quadrangular_4"
         self.nodal_coordinates = self.model.mesh.nodal_coordinates
-
-        self.define_integration_points()
-        self.process_shape_functions_and_derivatives()
-
-
-    def define_integration_points(self, integration_points: int = 4):
-        """ 
-        Defines the integration points and their respective weights
-        for the numerical integration processing.
-        """
-        self.nint = integration_points
-        self.num_int_data = self.integration_points_data_for_quadrangles(integration_points)
-        self.wps = self.num_int_data[:, -1].reshape(-1, 1, 1)
-
-
-    def process_shape_functions_and_derivatives(self):
-        """
-        This method processes the shape functions and their
-        derivatives for all integration points.
-        """
-
-        ##NOTE: Atalla, Noureddine.; Sgard Franck. Finite Element and Boundary Methods in Structural Acoustics and Vibration. 1st Ed. 2015
-
-        ## coordinates from integration points
-        xi_1 = self.num_int_data[:, 0]
-        xi_2 = self.num_int_data[:, 1]
-
-        ## shape functions (Atalla and Sgard, 2015, pg. 174)
-        phi = np.zeros((self.nint, 1, self.NODES_PER_ELEMENT), dtype=float)
-        phi[:, 0, 0] = (1 - xi_1)*(1 - xi_2) / 4      # ->      (-1.0, -1.0)   Node 1
-        phi[:, 0, 1] = (1 + xi_1)*(1 - xi_2) / 4      # ->      ( 1.0, -1.0)   Node 2
-        phi[:, 0, 2] = (1 + xi_1)*(1 + xi_2) / 4      # ->      ( 1.0,  1.0)   Node 3
-        phi[:, 0, 3] = (1 - xi_1)*(1 + xi_2) / 4      # ->      (-1.0,  1.0)   Node 4
-
-        ## derivatives of shape functions (obtained from the Atalla and Sgard proposed shape functions)
-        dphi = np.zeros((self.nint, 2, self.NODES_PER_ELEMENT), dtype=float)
-        dphi[:, 0, 0] = -(1 - xi_2) / 4 
-        dphi[:, 0, 1] =  (1 - xi_2) / 4
-        dphi[:, 0, 2] =  (1 + xi_2) / 4
-        dphi[:, 0, 3] = -(1 + xi_2) / 4
-
-        dphi[:, 1, 0] = -(1 - xi_1) / 4
-        dphi[:, 1, 1] = -(1 + xi_1) / 4
-        dphi[:, 1, 2] =  (1 + xi_1) / 4
-        dphi[:, 1, 3] =  (1 - xi_1) / 4
-
-        self.phi = phi
-        self.dphi = dphi
-
-
-    def get_stacked_local_coordinates(self) -> np.ndarray:
-        """
-        This funtion computes the local coordinates from global coordinates.
-
-        Parameter
-        ---------
-        coords: np.ndarray
-            An array containing the global coordinates to be converted.
-
-        Returns
-        -------
-        coord_loc: np.ndarray
-            The array of the stacked coordinates in the local coordinate system.
-        """
-
-        X1 = self.nodal_coordinates[self.connectivities[:, 0], 1]
-        Y1 = self.nodal_coordinates[self.connectivities[:, 0], 2]
-        Z1 = self.nodal_coordinates[self.connectivities[:, 0], 3]
-
-        X2 = self.nodal_coordinates[self.connectivities[:, 1], 1]
-        Y2 = self.nodal_coordinates[self.connectivities[:, 1], 2]
-        Z2 = self.nodal_coordinates[self.connectivities[:, 1], 3]
-        
-        X3 = self.nodal_coordinates[self.connectivities[:, 2], 1]
-        Y3 = self.nodal_coordinates[self.connectivities[:, 2], 2]
-        Z3 = self.nodal_coordinates[self.connectivities[:, 2], 3]
-
-        X4 = self.nodal_coordinates[self.connectivities[:, 3], 1]
-        Y4 = self.nodal_coordinates[self.connectivities[:, 3], 2]
-        Z4 = self.nodal_coordinates[self.connectivities[:, 3], 3]
-
-        vec_12 = np.array([X2-X1, Y2-Y1, Z2-Z1]).T
-        vec_13 = np.array([X3-X1, Y3-Y1, Z3-Z1]).T
-        vec_14 = np.array([X4-X1, Y4-Y1, Z4-Z1]).T
-
-        loc_x_axis = vec_12.copy()
-        loc_z_axis = np.cross(loc_x_axis, vec_14, axis=1)
-        loc_y_axis = np.cross(loc_z_axis, loc_x_axis, axis=1)
-
-        nx = np.linalg.norm(loc_x_axis, axis=1).reshape(-1, 1, 1)
-        ny = np.linalg.norm(loc_y_axis, axis=1).reshape(-1, 1, 1)
-
-        unit_x_axis = loc_x_axis.reshape(-1, 1, 3) / nx
-        unit_y_axis = loc_y_axis.reshape(-1, 1, 3) / ny
-
-        unit_x_axis = unit_x_axis.reshape(-1, 3)
-        unit_y_axis = unit_y_axis.reshape(-1, 3)
-
-        x1 = np.sum(vec_13 * unit_x_axis, axis=1)
-        x2 = np.sum(vec_14 * unit_x_axis, axis=1)
-        x3 = 0.
-        x4 = np.sum(vec_12 * unit_x_axis, axis=1)
-
-        y1 = np.sum(vec_13 * unit_y_axis, axis=1)
-        y2 = np.sum(vec_14 * unit_y_axis, axis=1)
-        y3 = 0.
-        y4 = np.sum(vec_12 * unit_y_axis, axis=1)
-
-        # x1 = 0.
-        # x2 = np.sum(vec_12 * unit_x_axis, axis=1)
-        # x3 = np.sum(vec_13 * unit_x_axis, axis=1)
-        # x4 = np.sum(vec_14 * unit_x_axis, axis=1)
-
-        # y1 = 0.
-        # y2 = np.sum(vec_12 * unit_y_axis, axis=1)
-        # y3 = np.sum(vec_13 * unit_y_axis, axis=1)
-        # y4 = np.sum(vec_14 * unit_y_axis, axis=1)
-
-        nel = self.connectivities.shape[0]
-        coord_loc = np.zeros((nel, self.DOF_PER_ELEMENT, 2), dtype=float)
-
-        coord_loc[:, 0, 0] = x1
-        coord_loc[:, 0, 1] = y1
-        coord_loc[:, 1, 0] = x2
-        coord_loc[:, 1, 1] = y2
-        coord_loc[:, 2, 0] = x3
-        coord_loc[:, 2, 1] = y3
-        coord_loc[:, 3, 0] = x4
-        coord_loc[:, 3, 1] = y4
-
-        return coord_loc
-
-
-    def stacked_matrices_NtN(self) -> np.ndarray:
-        """
-        This method processes all elementary matrices and returns them
-        in the stacked array form.
-
-        Returns
-        -------
-        int2d_NtN: np.ndarray
-            The array containing the stacked elementary matrices.
-        """
-
-        # compute local coordinates for all elements
-        local_coords = self.get_stacked_local_coordinates()
-
-        # initialize variable
-        int2d_NtN = 0.
-
-        # integration loop
-        for i in range(self.nint):
-
-            # Jacobian matrices of all elements
-            JAC_stacked = self.dphi[i, :, :] @ local_coords
-
-            # Jacobian determinants and inverses of all elements
-            det_jacs = self.get_detJAC(JAC_stacked)
-
-            # shape functions
-            N = self.phi[i, :, :]
-
-            int2d_NtN += N.T @ N * (det_jacs * self.wps[i])
-
-        return int2d_NtN
-
-
-    def stacked_matrices_NtN_and_BtB(self) -> np.ndarray:
-        """
-        This method processes all elementary matrices for mass source
-        and returns them in the stacked array form.
-
-        Returns
-        -------
-        Nt_N_stacked: np.ndarray
-            The array containing the elementary stacked matrices int(Nt @ N, gamma_s).
-
-        Bt_B_stacked: np.ndarray
-            The array containing the elementary stacked matrices int(Bt @ B, gamma_s).
-        """
-
-        # compute local coordinates for all elements
-        local_coords = self.get_stacked_local_coordinates()
-
-        # initialize variables
-        int2d_NtN = 0.
-        int2d_BtB = 0.
-
-        # integration loop
-        for i in range(self.nint):
-
-            # Jacobian matrices of all elements
-            JAC_stacked = self.dphi[i, :, :] @ local_coords
-
-            # Jacobian determinants and inverses of all elements
-            det_jacs, inv_jacs = self.get_detJAC_and_invJAC(JAC_stacked)
-
-            # shape functions
-            N = self.phi[i, :, :]
-            N_t = N.T
-
-            # derivative of shape functions
-            B = inv_jacs @ self.dphi[i, :, :]
-            B_t = np.transpose(B, axes=(0, 2, 1))
-
-            int2d_NtN += N_t @ N * (det_jacs * self.wps[i])
-            int2d_BtB += B_t @ B * (det_jacs * self.wps[i])
-
-        return int2d_NtN, int2d_BtB
 
 
     def load_vector(self, el_index: int, load: float = 1.0) -> np.ndarray:
@@ -428,19 +159,15 @@ class ACT_QUADRANGLE_4(Element2D):
         return acoustic_load
 
 
-    def reorder_connect(self, connect_face: np.ndarray):
-        """Reordering connectivity matrix to adequate the GMSH connectivity to the FE model"""
-        self.connectivities = connect_face[:, [0, 1, 2, 3]]
-
-
-    def generate_ind_rows_cols(self, connect_face):
+    def generate_ind_rows_cols(self, connectivities: np.ndarray):
         """ This method processess the dof indices (rows and columns) for assembly"""
 
-        self.reorder_connect(connect_face)
-        dof, edof = self.DOF_PER_NODE, self.DOF_PER_ELEMENT
-        ind_dof = dof * self.connectivities[:, :]
+        self.reorder_connect(connectivities)
+        dof, edof = self.dof_per_node, self.dof_per_element
 
+        ind_dof = dof * self.connectivities[:, :]
         vect_indices = ind_dof.flatten()
+
         ind_rows_face = ((np.tile(vect_indices, (edof,1))).T).flatten()
         ind_cols_face = (np.tile(ind_dof, edof)).flatten()
 
---
+++ vibra/engine/elements/elements_2d/structural/structural_quad8_element.py
@@ -0,0 +1,285 @@
+
+from typing import TYPE_CHECKING
+
+from vibra.engine.elements.elements_2d.quad8_element import QUADRANGLE_8, get_local_coordinates
+
+if TYPE_CHECKING:
+    from vibra.engine.model import Model
+
+import numpy as np
+
+
+class STRUCT_QUADRANGLE_8(QUADRANGLE_8):
+
+    def __init__(self, model: "Model", dof_per_node: int = 3):
+        super().__init__(model, dof_per_node)
+
+        self.model = model
+
+        self.connectivities = None
+        self.element_label = "structural_quadrangle_8"
+        self.nodal_coordinates = self.model.mesh.nodal_coordinates
+
+
+    def integrate_area(self, connectivities: np.ndarray):
+
+        self.connectivities = connectivities
+        
+        # compute local coordinates for all elements
+        local_coords = self.get_stacked_local_coordinates()
+
+        # initialize variable
+        dA = 0.
+
+        # integration loop
+        for i in range(self.nint):
+
+            # Jacobian matrices of all elements
+            JAC_stacked = self.dphi[i, :, :] @ local_coords
+
+            # Jacobian determinants and inverses of all elements
+            det_jacs, _ = self.get_detJAC_and_invJAC(JAC_stacked)
+
+            dA +=  det_jacs * self.wps[i]
+
+        return np.sum(dA)
+
+
+    def normal_pressure_load(self, e_normals: np.ndarray, normal_pressures: np.ndarray) -> np.ndarray:
+        """ 
+        This method computes the acoustic pressure loads over a surface.
+
+        Parameters
+        ----------
+        e_normals: np.ndarray
+            The stacked surface elements normals vectors.
+
+        nodal_solution: np.ndarray
+            The acoustic nodal_solution array.
+
+        Returns
+        -------
+        acoustic_load: np.ndarray
+            The acoustic presure loads integrated over a surface.
+        """
+
+        # # stack all elements nodal pressures 
+        # pressures = np.array([normal_pressures @ np.ones() for node_ids in self.connectivities.T], dtype=complex)
+
+        # stack the element normal pressures in format [n_el, DOFS_PER_ELEMENT, n_freq]
+        # Pe = pressures.transpose(1, 0, 2)
+        # Pe = normal_pressures.reshape(-1, 1, len(self.model.frequencies))
+        Pe = normal_pressures.transpose(1, 0, 2)
+
+        # compute local coordinates for all elements
+        local_coords = self.get_stacked_local_coordinates()
+
+        # initialize variable
+        element_loads = 0.
+
+        # Pn = Pe @ e_normals
+
+        # integration loop
+        for i in range(self.nint):
+
+            # Jacobian matrices of all elements
+            JAC_stacked = self.dphi[i, :, :] @ local_coords
+
+            # Jacobian determinants and inverses of all elements
+            det_jacs, _ = self.get_detJAC_and_invJAC(JAC_stacked)
+
+            # shape functions
+            N = np.zeros((3, self.dof_per_element), dtype=float)
+            N[0, 0::3] = self.phi[i, :, :]
+            N[1, 1::3] = self.phi[i, :, :]
+            N[2, 2::3] = self.phi[i, :, :]
+
+            element_loads +=  (N.T @ (e_normals @ Pe)) * (det_jacs * self.wps[i])
+
+        total_dof = self.dof_per_node * len(self.nodal_coordinates)
+        nodal_loads = np.zeros(total_dof, len(self.model.frequencies), dtype=complex)
+
+        for elem_id, node_ids in enumerate(self.connectivities):
+            nodal_loads[node_ids, :] += element_loads[elem_id, :, :]
+
+        return nodal_loads
+    
+
+    def integrate_normal_pressure_load(self, el_index: int, e_normal: np.ndarray,  normal_pressure: np.ndarray) -> np.ndarray:
+        """ 
+        This method computes the elementary load vector.
+
+        Parameters
+        ----------
+        el_index: int
+            The element index.
+
+        load: float, optional
+            The load vector.
+
+        Returns
+        -------
+        Fe: np.ndarray
+            The elementary load vector.
+        """
+
+        # element nodes
+        e_nodes = self.connectivities[el_index, :]
+
+        # element nodal coordinates
+        coords = self.nodal_coordinates[e_nodes, :]
+
+        # nodal coordinates in the local CS
+        coord_lcs = get_local_coordinates(coords)
+
+        # initialize the shape functions matrix
+        N = np.zeros((3, self.dof_per_element), dtype=float)
+
+        # initialize the variable Fe
+        Fe = 0.
+
+        # integration loop
+        for i in range(self.nint):
+
+            # Jacobian matrix
+            JAC = self.dphi[i, :, :] @ coord_lcs
+
+            # determinant of Jacobia matrix
+            det_JAC = self.get_detJAC(JAC)
+
+            # populate the shape functions matrix
+            N[0, 0::3] = self.phi[i, :, :]
+            N[1, 1::3] = self.phi[i, :, :]
+            N[2, 2::3] = self.phi[i, :, :]
+
+            # the negative value of the normal is used to point the normal toward the interior of the domain.
+            Fe += (N.T @ (-e_normal @ normal_pressure)) * (det_JAC * self.wps[i])
+
+        return Fe
+
+    def integrate_distributed_load(self, el_index: int, distributed_load: np.ndarray) -> np.ndarray:
+        """ 
+        This method computes the elementary load vector.
+
+        Parameters
+        ----------
+        el_index: int
+            The element index.
+
+        load: float, optional
+            The load vector.
+
+        Returns
+        -------
+        Fe: np.ndarray
+            The elementary load vector.
+        """
+
+        # element nodes
+        e_nodes = self.connectivities[el_index, :]
+
+        # element nodal coordinates
+        coords = self.nodal_coordinates[e_nodes, :]
+
+        # nodal coordinates in the local CS
+        coord_lcs = get_local_coordinates(coords)
+
+        # initialize the shape functions matrix
+        N = np.zeros((3, self.dof_per_element), dtype=float)
+
+        # initialize the variable Fe
+        Fe = 0.
+
+        # integration loop
+        for i in range(self.nint):
+
+            # Jacobian matrix
+            JAC = self.dphi[i, :, :] @ coord_lcs
+
+            # determinant of Jacobia matrix
+            det_JAC = self.get_detJAC(JAC)
+
+            # populate the shape functions matrix
+            N[0, 0::3] = self.phi[i, :, :]
+            N[1, 1::3] = self.phi[i, :, :]
+            N[2, 2::3] = self.phi[i, :, :]
+
+            Fe += (N.T @ distributed_load) * (det_JAC * self.wps[i])
+
+        return Fe
+
+
+    def integrate_distributed_mass(self, el_index: int, distributed_mass: float) -> np.ndarray:
+        """ 
+        This method computes the elementary load vector.
+
+        Parameters
+        ----------
+        el_index: int
+            The element index.
+
+        load: float, optional
+            The load vector.
+
+        Returns
+        -------
+        Fe: np.ndarray
+            The elementary load vector.
+        """
+
+        # element nodes
+        e_nodes = self.connectivities[el_index, :]
+
+        # element nodal coordinates
+        coords = self.nodal_coordinates[e_nodes, :]
+
+        # nodal coordinates in the local CS
+        coord_lcs = get_local_coordinates(coords)
+
+        # initialize the shape functions matrix
+        N = np.zeros((3, self.dof_per_element), dtype=float)
+
+        # initialize the variable Fe
+        Me = 0.
+
+        # integration loop
+        for i in range(self.nint):
+
+            # Jacobian matrix
+            JAC = self.dphi[i, :, :] @ coord_lcs
+
+            # determinant of Jacobia matrix
+            det_JAC = self.get_detJAC(JAC)
+
+            # populate the shape functions matrix
+            N[0, 0::3] = self.phi[i, :, :]
+            N[1, 1::3] = self.phi[i, :, :]
+            N[2, 2::3] = self.phi[i, :, :]
+
+            Me += (N.T @ N) * distributed_mass * (det_JAC * self.wps[i])
+
+        return Me
+
+
+    def element_indexes_vector(self, index: int):
+        node_ids = self.connectivities[index, :]
+        element_dofs = self.dof_per_node * node_ids.reshape(-1, 1) + np.arange(self.dof_per_node, dtype=int)
+        return element_dofs.flatten()
+
+
+    def elements_indexes_matrix(self):
+
+        dof, edof = self.dof_per_node, self.dof_per_element
+        n_el = len(self.connectivities)
+
+        local_dof = np.arange(dof, dtype=int)
+        ind_dof = np.zeros((n_el, edof), dtype=int)
+
+        for j in range(self.NODES_PER_ELEMENT):
+            ind_dof[:, j*dof : (1 + j)*dof] = dof * self.connectivities[:, j+1].reshape(-1, 1) + local_dof
+
+        vect_indices = ind_dof.flatten()
+        self.ind_rows = ((np.tile(vect_indices, (edof, 1))).T).flatten()
+        self.ind_cols = (np.tile(ind_dof, edof)).flatten()
+
+        return self.ind_rows, self.ind_cols
