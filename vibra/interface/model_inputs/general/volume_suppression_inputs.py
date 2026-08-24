3ba41b8178e1bb79d67e6b260d04c2283866dfb1
vitorslongo <v.v.slongo@gmail.com>
2026-08-24 16:19:51 -0300

    update BC message
    Co-authored-by: André Fernandes <fpf.andre@gmail.com>

--- vibra/interface/model_inputs/general/volume_suppression_inputs.py
+++ vibra/interface/model_inputs/general/volume_suppression_inputs.py
@@ -201,8 +201,7 @@ class VolumeSuppressionInputs(VolumeSuppressionDialog_UI):
         message = (
             f"The following properties are assigned to surfaces of the selected volumes:\n\n"
             f"  {', '.join(prop_names)}\n\n"
-            "Suppressing these volumes will remove all associated mesh data. "
-            "These properties will become invalid and must be reassigned later.\n\n"
+            "Suppressing these volumes will remove all associated mesh data while the volume is suppressed\n\n"
             "Do you want to proceed?"
         )
 
