c5780e94ed7c0afa7c6ce2ad8a9c7f555567a373
JacsonGilVargas <jgvargasemc@gmail.com>
2026-09-24 15:02:33 -0300

    Refactor the pyramid elements

--- vibra/engine/elements/elements_3d/pyram5_element.py
+++ vibra/engine/elements/elements_3d/pyram5_element.py
@@ -1,7 +1,7 @@
 
 from typing import TYPE_CHECKING
 
-from vibra.engine.elements.elements_3d.solid_elements import Element3D
+from vibra.engine.elements.elements_3d.hex8_element import Hexahedron8
 
 if TYPE_CHECKING:
     from vibra.engine.model import Model
@@ -9,17 +9,11 @@ if TYPE_CHECKING:
 import numpy as np
 
 
-class Pyramid5(Element3D):
+class Pyramid5(Hexahedron8):
 
     def __init__(self, model: "Model", dof_per_node: int, nodes_per_element: int):
-
-        self.model = model
-        self.dof_per_node = dof_per_node
-        self.nodes_per_element = nodes_per_element
-
-        self.connectivities = None
-        self.element_label = ""
-
+        super().__init__(model, dof_per_node, nodes_per_element)
+        
         self.define_integration_points()
         self.process_shape_functions_and_derivatives()
 
@@ -29,133 +23,9 @@ class Pyramid5(Element3D):
         return np.arange(5, dtype=int)
 
 
-    def define_integration_points(self, integration_points: int=11):
-        """ 
-        This method defines the integration points and their
-        weights for numerical integration.
-        """
-        self.nint = integration_points
-        self.num_int_data = self.integration_points_data_for_tetrahedrons(integration_points)
-        self.wps = self.num_int_data[:, -1].reshape(-1, 1, 1)
-
-
-    def process_shape_functions_and_derivatives(self):
-        """
-        This method returns the shape functions and its derivatives
-        for all integration points.
-
-        Returns
-        -------
-        phi: np.ndarray
-            The shape functions evaluated in the integration points.
-
-        dphi: np.ndarray
-            The shape functions derivatives.
-        """
-
-        ## coordinates from integration points
-        xi_1 = self.num_int_data[:, 0]
-        xi_2 = self.num_int_data[:, 1]
-        xi_3 = self.num_int_data[:, 2]
-
-        self.phi, self.dphi = self.get_shape_functions_and_derivatives(xi_1, xi_2, xi_3)
-        self.phi_inv = self.inverse_of_trilinear_shape_functions()
-
-
-    def inverse_of_trilinear_shape_functions(self):
-        """
-        This method returns the inverse of shape functions matrix N applied
-        at integration points (Gauss-Legendre quadrature points).
-        """
-        N = self.phi
-        n_intp, n_nodes = N.shape
-
-        if n_intp == n_nodes:
-            # print("N_int = N_nodes")
-            return np.linalg.inv(N)
-
-        elif n_intp > n_nodes:
-            # print("N_int > N_nodes")
-            return np.linalg.inv(N.T @ N) @ N.T
-
-        else:
-            print("Not implemented stress extrapolation for N_int < N_nodes")
-            return None
-
-    def get_shape_functions_and_derivatives(self, xi: np.ndarray | float, eta: np.ndarray | float, zeta: np.ndarray | float):
-
-        """
-        This method returns the shape functions and its derivatives.
-        
-        Parameters
-        ----------
-        xi: np.ndarray
-            The x coordinates of the integration points.
-        
-        eta: np.ndarray
-            The y coordinates of the integration points.
-
-        zeta: np.ndarray
-            The z coordinates of the integration points.
-
-        Returns
-        -------
-        phi: np.ndarray
-            The shape functions evaluated in the integration points.
-
-        dphi: np.ndarray
-            The shape functions derivatives.
-        """
-
-        if isinstance(xi, np.ndarray):
-            Nz = xi.size
-        else:
-            Nz = 1
-
-        ##NOTE: Atalla, Noureddine.; Sgard Franck. Finite Element and Boundary Methods in Structural Acoustics and Vibration. 1st Ed. 2015
-
-        # define the shape functions (Atalla and Sgard, 2015, pg. 170)
-        phi = np.zeros((Nz, self.nodes_per_element), dtype=float)
-
-        phi[:, 0] = (1 / 4) * ((1 - xi) * (1 - eta) - (1 + zeta) / 2 + ((xi * eta) * (1 + zeta) / (2 * (1 - zeta))))      # ->      (-1.0, -1.0, -1.0)   Node 1
-        phi[:, 1] = (1 / 4) * ((1 + xi) * (1 - eta) - (1 + zeta) / 2 - ((xi * eta) * (1 + zeta) / (2 * (1 - zeta))))      # ->      ( 1.0, -1.0, -1.0)   Node 2
-        phi[:, 2] = (1 / 4) * ((1 + xi) * (1 + eta) - (1 + zeta) / 2 + ((xi * eta) * (1 + zeta) / (2 * (1 - zeta))))      # ->      ( 1.0,  1.0, -1.0)   Node 3
-        phi[:, 3] = (1 / 4) * ((1 - xi) * (1 + eta) - (1 + zeta) / 2 - ((xi * eta) * (1 + zeta) / (2 * (1 - zeta))))      # ->      (-1.0,  1.0, -1.0)   Node 4
-        phi[:, 4] = (1 / 2) * (1 + zeta)                                                                                  # ->      ( 0.0,  0.0,  1.0)   Node 5
-
-        ## derivatives of shape functions (obtained from the Atalla and Sgard proposed shape functions)
-        dphi = np.zeros((Nz, 3, self.nodes_per_element), dtype=float)
-
-        # dN / dxi
-        dphi[:, 0, 0] = (1 / 4) * (-(1 - eta) + eta * (1 + zeta) / (2 * (1 - zeta))) 
-        dphi[:, 0, 1] = (1 / 4) * (+(1 - eta) - eta * (1 + zeta) / (2 * (1 - zeta)))
-        dphi[:, 0, 2] = (1 / 4) * (+(1 - eta) + eta * (1 + zeta) / (2 * (1 - zeta)))
-        dphi[:, 0, 3] = (1 / 4) * (-(1 - eta) - eta * (1 + zeta) / (2 * (1 - zeta)))
-        dphi[:, 0, 4] = 0
-
-        # dN / deta
-        dphi[:, 1, 0] = (1 / 4) * (-(1 - xi) + (xi * (1 + zeta) / (2 * (1 - zeta))))
-        dphi[:, 1, 1] = (1 / 4) * (-(1 - xi) - (xi * (1 + zeta) / (2 * (1 - zeta))))
-        dphi[:, 1, 2] = (1 / 4) * (+(1 - xi) + (xi * (1 + zeta) / (2 * (1 - zeta))))
-        dphi[:, 1, 3] = (1 / 4) * (+(1 - xi) - (xi * (1 + zeta) / (2 * (1 - zeta))))
-        dphi[:, 1, 4] =  0
-
-        # dN / dzeta
-        dphi[:, 2, 0] = (1 / 4) * (-(1 / 2) + (xi * eta) / ((1 - zeta)**2))
-        dphi[:, 2, 1] = (1 / 4) * (-(1 / 2) - (xi * eta) / ((1 - zeta)**2))
-        dphi[:, 2, 2] = (1 / 4) * (-(1 / 2) + (xi * eta) / ((1 - zeta)**2))
-        dphi[:, 2, 3] = (1 / 4) * (-(1 / 2) - (xi * eta) / ((1 - zeta)**2))
-        dphi[:, 2, 4] = (1 / 2) * zeta
-
-        if Nz == 1:
-            return phi[0, :], dphi[0, :, :]
-
-        return phi, dphi
-
-
     def reorder_connect(self):
         """
         Reordering connectivity matrix to adequate 
         the GMSH connectivity to the FE model
         """
-        self.connectivities = self.model.mesh.solids_connectivity[:, [4, 5, 6, 7, 8]]
+        self.connectivities = self.model.mesh.solids_connectivity[:, [4, 5, 6, 7, 8, 8, 8, 8]]
